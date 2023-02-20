62737153135dc7a6653958abde55f32bc30c8b35
kayaba <fluctlight-kayaba@gmail.com>
2023-02-20 15:53:22 +0700

    bring back Coming Soon! welcome screen for Home page.

--- apps/home/src/screens/Home/index.tsx
+++ apps/home/src/screens/Home/index.tsx
@@ -1,17 +1,34 @@
 import { FC } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text } from '@metacraft/ui';
-
-const styles = StyleSheet.create({
-	container: {},
-});
+import { StyleSheet, Text, View } from 'react-native';
+import { Hyperlink } from '@metacraft/ui';
 
 export const HomeScreen: FC = () => {
 	return (
 		<View style={styles.container}>
-			<Text>HomeScreen</Text>
+			<Text style={styles.heading}>COMING SOON!</Text>
+			<Text>
+				<Text style={styles.sub}>Created with ❤️ by </Text>
+				<Hyperlink href="https://stormgate.io" title="Stormgate.io" />
+			</Text>
 		</View>
 	);
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#001825',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	heading: {
+		color: '#ffffff',
+		fontSize: 42,
+		marginBottom: 8,
+	},
+	sub: {
+		color: '#888888',
+	},
+});

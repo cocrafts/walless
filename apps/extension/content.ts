48b993e89e0f08ad68ee8af9ab49b1e24dde0d5d
kayaba <fluctlight-kayaba@gmail.com>
2023-02-20 16:43:23 +0700

    bare convention on forwarding Browser Tab's message

--- apps/extension/content.ts
+++ apps/extension/content.ts
@@ -3,12 +3,11 @@ console.log('content.js: loaded');
 const port = chrome.runtime.connect();
 window.addEventListener(
 	'message',
-	(event) => {
-		if (event.source !== window) return;
+	({ source, data }) => {
+		if (source !== window) return;
 
-		if (event.data.type && event.data.type === 'FROM_PAGE') {
-			console.log('content.js: received message from page:', event.data.text);
-			chrome.runtime.sendMessage({ hello: 1 });
+		if (data.from?.startsWith('walless@')) {
+			chrome.runtime.sendMessage(data);
 		}
 	},
 	false,

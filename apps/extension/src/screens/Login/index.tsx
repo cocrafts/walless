3cde5bb2441e2c1f202a7b1c5af778a4ee8f003a
kayaba <fluctlight-kayaba@gmail.com>
2023-02-22 02:11:08 +0700

    offload signIn function to utility module, use deprecate Stylesheet with Nativewind in LoginScreen

--- apps/extension/src/screens/Login/index.tsx
+++ apps/extension/src/screens/Login/index.tsx
@@ -1,65 +1,39 @@
-import { FC, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {
-	AGGREGATE_VERIFIER,
-	TorusAggregateLoginResponse,
-} from '@toruslabs/customauth';
+import { FC, Fragment, useState } from 'react';
+import { Text, View } from 'react-native';
+import { TorusAggregateLoginResponse } from '@toruslabs/customauth';
 import { useW3a } from 'utils/hook';
-import { customAuth } from 'utils/w3a';
+import { googleSignIn } from 'utils/w3a';
 
 export const LoginScreen: FC = () => {
 	const [login, setLogin] = useState<TorusAggregateLoginResponse>();
 	useW3a();
 
 	const toggleLogin = async () => {
-		const loginResponse = await customAuth.triggerAggregateLogin({
-			aggregateVerifierType: AGGREGATE_VERIFIER.SINGLE_VERIFIER_ID,
-			verifierIdentifier: 'stormgate-testnet',
-			subVerifierDetailsArray: [
-				{
-					typeOfLogin: 'google',
-					verifier: 'google',
-					clientId: GOOGLE_CLIENT_ID,
-				},
-			],
-		});
-
-		setLogin(loginResponse);
-		console.log(loginResponse, '<--');
+		const response = await googleSignIn();
+		setLogin(response);
+		console.log(response, '<--');
 	};
 
 	return (
-		<View style={styles.container}>
-			<Text onPress={toggleLogin} style={styles.heading}>
+		<View className="flex-1 items-center justify-center">
+			<Text className="text-white text-3xl" onPress={toggleLogin}>
 				Google Sign-In
 			</Text>
-			<Text>
-				<Text style={styles.sub}>Created with ❤️ by Metacraft</Text>
+			<Text className="text-white mt-3">
+				<Text>Created with ❤️ by Metacraft</Text>
 			</Text>
 			{login?.pubKey && (
-				<Text style={{ color: '#ADADAD', marginTop: 24 }}>
-					{login?.userInfo[0]?.email} {login?.publicAddress}
-				</Text>
+				<Fragment>
+					<Text className="text-white text-center mt-5">
+						{login?.userInfo[0]?.email}
+					</Text>
+					<Text className="text-white text-xs text-center mt-2">
+						{login?.publicAddress}
+					</Text>
+				</Fragment>
 			)}
 		</View>
 	);
 };
 
 export default LoginScreen;
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#001825',
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	heading: {
-		color: '#ffffff',
-		fontSize: 42,
-		marginBottom: 8,
-	},
-	sub: {
-		color: '#888888',
-	},
-});
--- apps/extension/src/utils/w3a.ts
+++ apps/extension/src/utils/w3a.ts
@@ -1,4 +1,7 @@
-import CustomAuth, { CustomAuthArgs } from '@toruslabs/customauth';
+import CustomAuth, {
+	AGGREGATE_VERIFIER,
+	CustomAuthArgs,
+} from '@toruslabs/customauth';
 
 const customAuthArgs: CustomAuthArgs = {
 	network: 'testnet',
@@ -10,3 +13,17 @@ const customAuthArgs: CustomAuthArgs = {
 };
 
 export const customAuth = new CustomAuth(customAuthArgs);
+
+export const googleSignIn = () => {
+	return customAuth.triggerAggregateLogin({
+		aggregateVerifierType: AGGREGATE_VERIFIER.SINGLE_VERIFIER_ID,
+		verifierIdentifier: 'stormgate-testnet',
+		subVerifierDetailsArray: [
+			{
+				typeOfLogin: 'google',
+				verifier: 'google',
+				clientId: GOOGLE_CLIENT_ID,
+			},
+		],
+	});
+};

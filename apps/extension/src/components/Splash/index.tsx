fa991bc0606aff0340f47c4ca7c68690b528dcaf
kayaba <fluctlight-kayaba@gmail.com>
2023-02-22 01:08:48 +0700

    splash screen with official logo and animation transition

--- apps/extension/src/components/Splash/index.tsx
+++ apps/extension/src/components/Splash/index.tsx
@@ -1,21 +1,61 @@
 import { FC, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, View } from 'react-native';
+import Animated, {
+	useAnimatedStyle,
+	useSharedValue,
+	withSpring,
+	withTiming,
+} from 'react-native-reanimated';
 import { appActions } from 'utils/state/app';
 
-const styles = StyleSheet.create({
-	container: {},
-});
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 export const SplashScreen: FC = () => {
+	const opacity = useSharedValue(0);
+	const raise = useSharedValue(50);
+
+	const logoStyle = useAnimatedStyle(() => ({
+		width: logoSize,
+		height: logoSize,
+		opacity: opacity.value,
+		transform: [{ translateY: raise.value }],
+	}));
+
 	useEffect(() => {
-		appActions.setLoading(false);
+		const fadeAnimation = async (): Promise<void> => {
+			return new Promise((resolve) => {
+				opacity.value = withTiming(1, { duration: 1000 }, () => {
+					resolve();
+				});
+			});
+		};
+
+		const raiseAnimation = async (): Promise<void> => {
+			return new Promise((resolve) => {
+				raise.value = withSpring(0, {}, () => {
+					resolve();
+				});
+			});
+		};
+
+		Promise.all([fadeAnimation(), raiseAnimation()]).then(() => {
+			opacity.value = withTiming(0, { duration: 400 }, () => {
+				appActions.setLoading(false);
+			});
+		});
 	}, []);
 
 	return (
-		<View style={styles.container}>
-			<Text>SplashScreen</Text>
+		<View className="flex-1 items-center justify-center">
+			<AnimatedImage
+				style={logoStyle}
+				source={{ uri: 'icon.png' }}
+				resizeMode="contain"
+			/>
 		</View>
 	);
 };
 
 export default SplashScreen;
+
+const logoSize = 150;

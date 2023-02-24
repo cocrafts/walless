f16c61e51dfe62f5692bff6e1a1a5f394ef022c0
Tan Le <tanlethanh8266@gmail.com>
2023-02-24 09:01:58 +0700

    Init network package, basic query

---
+++ packages/network/connection.ts
@@ -0,0 +1,30 @@
+import { Connection, clusterApiUrl } from '@solana/web3.js'
+
+
+export default class SolanaConnection {
+
+    private static instance: SolanaConnection
+    private connection: Connection
+    private static countGetConnection: number = 0
+
+    private constructor() {
+        this.connection = new Connection(clusterApiUrl('devnet'))
+    }
+
+    public static getConnection(): Connection {
+
+        if (!SolanaConnection.instance) {
+            SolanaConnection.instance = new SolanaConnection()
+        }
+
+        SolanaConnection.countGetConnection++
+        return SolanaConnection.instance.connection
+    } 
+
+    public static getConnectionLog() {
+        return {
+            countConnection: SolanaConnection.countGetConnection
+        }
+    }
+
+}

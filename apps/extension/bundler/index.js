eeb76d4f4d52f03cc45b6288b1ec12eccf6e8740
kayaba <fluctlight-kayaba@gmail.com>
2023-02-25 00:10:47 +0700

    zip Chrome and Firebox zips to Home build

--- apps/extension/bundler/index.js
+++ apps/extension/bundler/index.js
@@ -1,12 +1,33 @@
 const fs = require('fs-extra');
+const archiver = require('archiver');
 const manifest = require('./manifest.json');
 
-const cloneExtensionBuild = (platform, override = {}) => {
+const zipDir = (platform) => {
+	const homeBuildDir = '../home/metacraft';
+	const archive = archiver('zip', { zlib: { level: 9 } });
+	const stream = fs.createWriteStream(`${homeBuildDir}/${platform}.zip`);
+
+	fs.ensureDir(homeBuildDir);
+
+	return new Promise((resolve, reject) => {
+		archive
+			.directory(`./builds/${platform}`, false)
+			.on('error', (err) => reject(err))
+			.pipe(stream);
+
+		stream.on('close', () => resolve());
+		archive.finalize();
+	});
+};
+
+const cloneExtensionBuild = async (platform, override = {}) => {
 	const manifestUri = `./builds/${platform}/manifest.json`;
 	const mergedManifest = { ...manifest, ...override };
 
 	fs.copySync(`./metacraft`, `./builds/${platform}`);
 	fs.writeJsonSync(manifestUri, mergedManifest, { spaces: 2 });
+
+	await zipDir(platform);
 };
 
 cloneExtensionBuild('chrome');

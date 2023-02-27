ef97e3f08fdf36de94994241c682e974ca1b5a14
FME849 <1414439@gmail.com>
2023-02-27 15:06:05 +0700

    minor UI update to DashboardLayout

--- apps/extension/src/screens/Dashboard/Layout.tsx
+++ apps/extension/src/screens/Dashboard/Layout.tsx
@@ -12,7 +12,8 @@ export const DashboardLayout: FC<Props> = ({
 }) => {
 	return (
 		<View className="flex-1 flex-row">
-			<View className="w-16" />
+			<View className="w-16 bg-color-7 px-2">
+			</View>
 			<View className={contentContainerClass}>{children}</View>
 		</View>
 	);

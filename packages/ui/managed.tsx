10bafcc8a47e9911f18a35bea01f2e3ca0b09e46
FME849 <1414439@gmail.com>
2023-02-28 10:19:55 +0700

    add Svg component to styled native wind

--- packages/ui/managed.tsx
+++ packages/ui/managed.tsx
@@ -5,12 +5,14 @@ import {
 	View as BareView,
 } from 'react-native';
 import Animated from 'react-native-reanimated';
+import { Svg as BareSvg } from 'react-native-svg';
 import { styled } from 'nativewind';
 
 export const View = styled(BareView);
 export const Text = styled(BareText, 'font-poppins text-white');
 export const Image = styled(BareImage);
 export const TouchableOpacity = styled(BareTouchableOpacity);
+export const Svg = styled(BareSvg);
 
 export const AnimatedView = Animated.createAnimatedComponent(View);
 export const AnimatedTouch = Animated.createAnimatedComponent(TouchableOpacity);

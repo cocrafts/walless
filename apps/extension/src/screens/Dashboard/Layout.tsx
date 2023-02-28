8b3970fc3897f8a4f6a255e509595181b5cb1b3f
Tuan Tran <1414439@gmail.com>
2023-02-28 16:45:47 +0700

    update UI Dashboard layout (#5)
    * add Svg component to styled native wind
    
    * add HomeIcon to use in DashboardLayout
    
    * add Avatar to use in DashboardLayout
    
    * update UI in DashboardLayout
    
    ---------
    
    Co-authored-by: Kayaba Akihiko <kayaba.fluctlight@gmail.com>

--- apps/extension/src/screens/Dashboard/Layout.tsx
+++ apps/extension/src/screens/Dashboard/Layout.tsx
@@ -1,5 +1,7 @@
 import { FC, ReactNode } from 'react';
-import { View } from '@walless/ui';
+import { TouchableOpacity, View } from '@walless/ui';
+import Avatar from '@walless/ui/components/Avatar';
+import HomeIcon from '@walless/ui/components/icons/Home';
 
 interface Props {
 	children?: ReactNode;
@@ -12,7 +14,18 @@ export const DashboardLayout: FC<Props> = ({
 }) => {
 	return (
 		<View className="flex-1 flex-row">
-			<View className="w-16 bg-color-7 px-2" />
+			<View className="w-16 bg-color-7 px-2">
+				<View className="border-b border-color-3">
+					<TouchableOpacity className="items-center py-4">
+						<HomeIcon size={28} />
+					</TouchableOpacity>
+				</View>
+				<View className="flex-1 justify-end pb-5">
+					<TouchableOpacity className="w-full h-12">
+						<Avatar />
+					</TouchableOpacity>
+				</View>
+			</View>
 			<View className={contentContainerClass}>{children}</View>
 		</View>
 	);
--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,5 +1,7 @@
+export * from './components/Avatar';
 export * from './components/Button';
 export * from './components/IconButton';
+export * from './components/icons';
 export * from './components/IconSearch';
 export * from './managed';
 export * from './utils/state/env';

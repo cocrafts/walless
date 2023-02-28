e4afe11caf4729a84cf94c882d143565a1cf2fec
FME849 <1414439@gmail.com>
2023-02-28 17:01:37 +0700

    setting nested route in Dashboard

--- apps/extension/src/router.tsx
+++ apps/extension/src/router.tsx
@@ -1,5 +1,7 @@
 import { createHashRouter } from 'react-router-dom';
 import DashboardScreen from 'screens/Dashboard';
+import ChooseLayout from 'screens/Dashboard/ChooseLayout';
+import Profile from 'screens/Dashboard/Profile';
 import LoginScreen from 'screens/Login';
 import PasscodeScreen from 'screens/Passcode';
 
@@ -7,6 +9,16 @@ export const router = createHashRouter([
 	{
 		path: '/',
 		element: <DashboardScreen />,
+		children: [
+			{
+				path: '/profile',
+				element: <Profile />,
+			},
+			{
+				path: '/explore',
+				element: <ChooseLayout />,
+			},
+		],
 	},
 	{
 		path: '/login',

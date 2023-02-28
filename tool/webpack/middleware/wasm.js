f79eecd611289ee1d8dde993bf01097b08ac2c2e
kayaba <fluctlight-kayaba@gmail.com>
2023-02-28 10:38:35 +0700

    bring back env fallback helper

--- tool/webpack/middleware/wasm.js
+++ tool/webpack/middleware/wasm.js
@@ -2,6 +2,7 @@ const { resolve } = require('path');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
 const wasmBundler = (outDir) => (config) => {
+	config.entry.app.import.unshift('env.js');
 	config.plugins.push(
 		new WasmPackPlugin({
 			crateDirectory: resolve(__dirname, '../../../wasm'),

dfa447e2198f945d47804fb164fc1e2bfa5cb77f
Tuan Tran <1414439@gmail.com>
2023-02-28 17:11:19 +0700

    Setting nested route (#7)
    * setting nested route in Dashboard
    
    * link to profile and explore layout screens

--- apps/extension/src/screens/Dashboard/index.tsx
+++ apps/extension/src/screens/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
 
-import ChooseLayout from './ChooseLayout';
 import DashboardLayout from './Layout';
 
 export const HomeScreen: FC = () => {
 	return (
 		<DashboardLayout contentContainerClass="bg-gradient-to-b from-color-5 to-color-6 flex-1">
-			<ChooseLayout />
+			<Outlet />
 		</DashboardLayout>
 	);
 };

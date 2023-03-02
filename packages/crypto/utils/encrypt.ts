4b6d290a70f7d2ed13796e9a9d7f7e90576dfd7f
kayaba <fluctlight-kayaba@gmail.com>
2023-03-02 18:14:38 +0700

    finalize encryption interface

--- packages/crypto/utils/encrypt.ts
+++ packages/crypto/utils/encrypt.ts
@@ -1,49 +1,90 @@
-interface KeyIVOptions {
-	algorithm: string;
-	keyLength: number;
-	ivLength: number;
+import { storage } from 'webextension-polyfill';
+
+export interface HydratedKey {
+	jwk: JsonWebKey;
+	keyParams: AesKeyGenParams;
+	keyUsages: ReadonlyArray<KeyUsage>;
 }
 
-export interface KeyIV {
-	options: KeyIVOptions;
-	key: CryptoKey;
-	iv: Uint8Array;
-}
-
+export type AesAlgorithm = 'AES-GCM' | 'AES-CBC' | 'AES-CTR';
 export type AesKeyLength = 128 | 192 | 256;
 
-export const generateKeyIV = async (
-	keyLength: AesKeyLength = 256,
-	ivLength = 12,
-): Promise<KeyIV> => {
-	const algorithm: AesKeyGenParams = { name: 'AES-GCM', length: keyLength };
-	const keyUsages: ReadonlyArray<KeyUsage> = ['encrypt', 'decrypt'];
-	const key = await crypto.subtle.generateKey(algorithm, true, keyUsages);
-	const iv = crypto.getRandomValues(new Uint8Array(12));
+export const createCryptoKey = async (
+	name: AesAlgorithm,
+	length: AesKeyLength,
+	keyUsages: ReadonlyArray<KeyUsage>,
+): Promise<CryptoKey> => {
+	const keyParams: AesKeyGenParams = { name, length };
 
-	return {
-		options: { algorithm: algorithm.name, ivLength, keyLength },
-		key,
-		iv,
-	};
+	return await crypto.subtle.generateKey(keyParams, true, keyUsages);
 };
 
-export const encryptAes = async (
+export const createAndHydrateCryptoKey = async (
+	id: string,
+	name: AesAlgorithm = 'AES-GCM',
+	length: AesKeyLength = 256,
+	keyUsages: ReadonlyArray<KeyUsage> = ['encrypt', 'decrypt'],
+): Promise<CryptoKey> => {
+	const keyParams: AesKeyGenParams = { name, length };
+	const key = await crypto.subtle.generateKey(keyParams, true, keyUsages);
+	const jwk = await crypto.subtle.exportKey('jwk', key);
+	const hydrated: HydratedKey = { jwk, keyParams, keyUsages };
+
+	await storage.local.set({ [id]: JSON.stringify(hydrated) });
+	return key;
+};
+
+export const restructCryptoKey = async (id: string): Promise<CryptoKey> => {
+	const result = await storage.local.get([id]);
+	const hydrated: HydratedKey = JSON.parse(result[id]);
+
+	return await crypto.subtle.importKey(
+		'jwk',
+		hydrated.jwk,
+		hydrated.keyParams,
+		true,
+		hydrated.keyUsages,
+	);
+};
+
+export const encrypt = async (
 	data: string,
-	{ options, key, iv }: KeyIV,
+	key: CryptoKey,
+	iv: Uint8Array,
 ): Promise<ArrayBuffer> => {
 	const plaintext = new TextEncoder().encode(data);
-	const algorithm = { name: options.algorithm, iv };
+	const algorithm = { name: key.algorithm.name, iv };
 
 	return await crypto.subtle.encrypt(algorithm, key, plaintext);
 };
 
-export const decryptAes = async (
-	ciphered: ArrayBuffer,
-	{ options, key, iv }: KeyIV,
+export const encryptToString = async (
+	data: string,
+	key: CryptoKey,
+	iv: Uint8Array,
 ): Promise<string> => {
-	const algorithm = { name: options.algorithm, iv };
+	const cipheredBytes = await encrypt(data, key, iv);
+
+	return new Buffer(cipheredBytes).toString('base64');
+};
+
+export const decrypt = async (
+	ciphered: ArrayBuffer,
+	key: CryptoKey,
+	iv: Uint8Array,
+): Promise<string> => {
+	const algorithm = { name: key.algorithm.name, iv };
 	const deciphered = await crypto.subtle.decrypt(algorithm, key, ciphered);
 
 	return new TextDecoder().decode(deciphered);
+};
+
+export const decryptFromString = async (
+	encrypted: string,
+	key: CryptoKey,
+	iv: Uint8Array,
+): Promise<string> => {
+	const bytes = new Uint8Array(Buffer.from(encrypted, 'base64'));
+
+	return await decrypt(bytes, key, iv);
 };

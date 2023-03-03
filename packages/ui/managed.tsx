4a05e5b113a0f5dcb1a9566dbbe312a1a40c792a
FME849 <1414439@gmail.com>
2023-03-03 15:54:07 +0700

    add TextInput to native wind component

--- packages/ui/managed.tsx
+++ packages/ui/managed.tsx
@@ -1,6 +1,7 @@
 import {
 	Image as BareImage,
 	Text as BareText,
+	TextInput as BareTextInput,
 	TouchableOpacity as BareTouchableOpacity,
 	View as BareView,
 } from 'react-native';
@@ -13,6 +14,7 @@ export const Text = styled(BareText, 'font-poppins text-white');
 export const Image = styled(BareImage);
 export const TouchableOpacity = styled(BareTouchableOpacity);
 export const Svg = styled(BareSvg);
+export const TextInput = styled(BareTextInput);
 
 export const AnimatedView = Animated.createAnimatedComponent(View);
 export const AnimatedTouch = Animated.createAnimatedComponent(TouchableOpacity);

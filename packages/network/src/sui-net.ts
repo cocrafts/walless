57bc29e2a0385aa08cbc4da565a6420582361e25
Tan Le <tanlethanh8266@gmail.com>
2023-03-05 01:07:55 +0700

    Add SUI net with basic query

---
+++ packages/network/src/sui-net.ts
@@ -0,0 +1,35 @@
+import { Connection, JsonRpcProvider } from '@mysten/sui.js';
+
+import { Token, TokenType } from './network-interface';
+
+const connection = new Connection({
+	fullnode: 'https://fullnode.devnet.sui.io',
+	faucet: 'https://faucet.devnet.sui.io/gas',
+});
+
+// connect to a custom RPC server
+export const provider = new JsonRpcProvider(connection);
+
+class SuiNet {
+	static queryAllByAddress = async (address: string) => {
+		const coins = await provider.getAllCoins(address);
+		console.log(typeof coins.data[0].balance);
+		const balance: number = coins.data.reduce((accumulator, coin) => {
+			return accumulator + coin.balance;
+		}, 0);
+		const SUI_COIN_STRING = '0x2::sui::SUI';
+		const metadata = await provider.getCoinMetadata(SUI_COIN_STRING);
+		console.log(metadata);
+
+		const result: Token = {
+			name: metadata.name,
+			balance: (balance / 10 ** metadata.decimals).toString(),
+			symbol: metadata.symbol,
+			type: TokenType.SUI_TOKEN,
+		};
+
+		return result;
+	};
+}
+
+export default SuiNet;

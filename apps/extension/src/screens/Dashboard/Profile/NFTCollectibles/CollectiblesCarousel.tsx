d2f4cb349ba355f17cb025af3a4289da03663a0e
Hieu Nguyen <dev.ngtronghieu@gmail.com>
2023-03-04 16:57:49 +0700

    scaffold collectibles layout

---
+++ apps/extension/src/screens/Dashboard/Profile/NFTCollectibles/CollectiblesCarousel.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { Image, Text, View } from '@walless/ui';
+
+import { CollectibleProps } from '.';
+
+interface CollectiblesCarouselProps {
+	className?: string;
+	collectibles: CollectibleProps[];
+}
+
+const CollectiblesCarousel: FC<CollectiblesCarouselProps> = ({
+	className,
+	collectibles,
+}) => {
+	return (
+		<View className={`flex flex-row gap-3 justify-between ${className}`}>
+			{collectibles.map((collectible) => (
+				<View key={collectible.name} className="w-[84px]">
+					<Link to={collectible.link}>
+						<Image
+							source={{ uri: collectible.thumbnail }}
+							className="h-[90px] w-[84px]"
+						/>
+					</Link>
+
+					<View className="flex flex-row gap-1">
+						<Link to={collectible.link}>
+							<Image
+								source={{ uri: collectible.icon }}
+								className="h-5 w-5 ml-2 -mt-2"
+							/>
+						</Link>
+						<Link
+							className="h-3 text-[8px] mt-1 overflow-hidden"
+							to={collectible.link}
+						>
+							{collectible.name}
+						</Link>
+					</View>
+				</View>
+			))}
+		</View>
+	);
+};
+
+export default CollectiblesCarousel;

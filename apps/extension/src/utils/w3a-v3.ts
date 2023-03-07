f9554b0e96cdcfa89e2688840f4d084832dcaa22
Tan Le <tanlethanh8266@gmail.com>
2023-03-07 18:00:17 +0700

    Fix login flow function

--- apps/extension/src/utils/w3a-v3.ts
+++ apps/extension/src/utils/w3a-v3.ts
@@ -1,16 +1,23 @@
 import { BNString } from '@tkey/common-types';
-import { TorusLoginResponse } from '@toruslabs/customauth';
+import {
+	AGGREGATE_VERIFIER,
+	TorusAggregateLoginResponse,
+	TorusLoginResponse,
+} from '@toruslabs/customauth';
+import { assignWith } from 'lodash';
+import { storage } from 'webextension-polyfill';
 
 import { key } from './tkey';
 
-export enum tKeySignal {
-	REQUIRE_INIT_PASSCODE,
-	REQUIRE_INPUT_PASSCODE,
-	WRONG_PASSCODE,
-	INPUT_PASSCODE_SUCCESS,
-	INIT_PASSCODE_SUCCESS,
-	INIT_PASSCODE_FAIL,
-	RECONSTRUCT_KEY_SUCCESS,
+export enum w3aSignal {
+	REQUIRE_INIT_PASSCODE = 'REQUIRE_INIT_PASSCODE',
+	REQUIRE_INPUT_PASSCODE = 'REQUIRE_INPUT_PASSCODE',
+	WRONG_PASSCODE = 'WRONG_PASSCODE',
+	INPUT_PASSCODE_SUCCESS = 'INPUT_PASSCODE_SUCCESS',
+	INIT_PASSCODE_SUCCESS = 'INIT_PASSCODE_SUCCESS',
+	INIT_PASSCODE_FAIL = 'INIT_PASSCODE_FAIL',
+	RECONSTRUCT_KEY_SUCCESS = 'RECONSTRUCT_KEY_SUCCESS',
+	TRIGGER_LOGIN_FAIL = 'TRIGGER_LOGIN_FAIL',
 }
 
 /**
@@ -45,23 +52,37 @@ export const inputPasscode = async (passcode: string) => {
 		);
 	} catch (e) {
 		console.log(e);
-		return tKeySignal.WRONG_PASSCODE;
+		return w3aSignal.WRONG_PASSCODE;
 	}
-	return tKeySignal.INPUT_PASSCODE_SUCCESS;
+	return w3aSignal.INPUT_PASSCODE_SUCCESS;
 };
 
-export const googleSignIn = async (): Promise<TorusLoginResponse> => {
+export const googleSignIn = async (): Promise<
+	TorusLoginResponse | w3aSignal
+> => {
 	const { serviceProvider } = key;
 
 	if (!serviceProvider.directWeb.isInitialized) {
 		await serviceProvider.init({ skipSw: true });
 	}
 
-	const response = await serviceProvider.triggerLogin({
-		typeOfLogin: 'google',
-		verifier: 'walless-gc',
-		clientId: GOOGLE_CLIENT_ID,
-	});
+	let response = null;
+	console.log('GOOGLE_CLIENT_ID: ', GOOGLE_CLIENT_ID);
+
+	try {
+		response = await serviceProvider.triggerLogin({
+			typeOfLogin: 'google',
+			verifier: 'stormgate-google',
+			clientId: GOOGLE_CLIENT_ID,
+		});
+	} catch (e) {
+		console.log('TRIGGER LOGIN ERROR: ');
+		console.log(e);
+		return w3aSignal.TRIGGER_LOGIN_FAIL;
+	}
+
+	console.log('Init key');
+	await key.initialize();
 
 	return response;
 };
@@ -71,22 +92,32 @@ export const googleSignIn = async (): Promise<TorusLoginResponse> => {
  * Just call it after trigger login
  * */
 export const initAfterLogin = async () => {
+	// Try to reconstruct key
+
 	// If this is the first time login
-	const { requiredShares } = key.getKeyDetails();
-	if (requiredShares > 0) {
-		return tKeySignal.REQUIRE_INIT_PASSCODE;
+	try {
+		const { totalShares, requiredShares } = key.getKeyDetails();
+		if (totalShares === 2) {
+			if (requiredShares <= 0) {
+				await key.reconstructKey();
+			}
+
+			return w3aSignal.REQUIRE_INIT_PASSCODE;
+		}
+	} catch (e) {
+		console.log(e);
 	}
 	// If storage share was found
 	try {
 		await key.modules.webStorage.inputShareFromWebStorage();
+		console.log('Get from storage success');
 		await key.reconstructKey();
-
-		return tKeySignal.RECONSTRUCT_KEY_SUCCESS;
+		return w3aSignal.RECONSTRUCT_KEY_SUCCESS;
 	} catch (e) {
 		console.log(e);
 	}
 	// If storage share wasn't found (loss or in new browser/ device)
-	return tKeySignal.REQUIRE_INPUT_PASSCODE;
+	return w3aSignal.REQUIRE_INPUT_PASSCODE;
 };
 
 export const initPasscode = async (passcode: string) => {
@@ -101,7 +132,7 @@ export const initPasscode = async (passcode: string) => {
 		);
 
 		// Reconstruct key after fetch 2/3 flow (google share and passcode share)
-		await key.reconstructKey();
+		// await key.reconstructKey();
 
 		// Create new share for current user
 		const shareStore = await key.generateNewShare();
@@ -109,9 +140,13 @@ export const initPasscode = async (passcode: string) => {
 		// Store share into web storage
 		await key.modules.webStorage.storeDeviceShare(shareStore.newShareStores[1]);
 
-		return tKeySignal.INIT_PASSCODE_SUCCESS;
+		return w3aSignal.INIT_PASSCODE_SUCCESS;
 	} catch (e) {
 		console.log(e);
-		return tKeySignal.INIT_PASSCODE_FAIL;
+		return w3aSignal.INIT_PASSCODE_FAIL;
 	}
+};
+
+export const checkLogin = async () => {
+	return null;
 };

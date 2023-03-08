e6b166262e22b71ced5b5bde0a648d2d50821f38
Tan Le <tanlethanh8266@gmail.com>
2023-03-08 15:09:07 +0700

    Add utils and check valid derive key

--- packages/network/tests/index.test.ts
+++ packages/network/tests/index.test.ts
@@ -1,7 +1,9 @@
 import { describe, test } from '@jest/globals';
+import { Keypair } from '@solana/web3.js';
 
 import SolanaNet from '../src/solana-net';
 import SuiNet from '../src/sui-net';
+import { convertStringToUin8Array } from '../src/utils/convert';
 
 import { solKey, suiKey } from './config';
 
@@ -14,5 +16,22 @@ describe('fake test', () => {
 describe('fake test', () => {
 	test('this should be always fine, replace this with real test case once you have it', async () => {
 		console.log(await SolanaNet.queryAllByAddress(solKey));
+	});
+});
+
+describe('fake test', () => {
+	test('this should be always fine, replace this with real test case once you have it', async () => {
+		try {
+			const privateKey =
+				'355e86993ccd81c04e126af03d7cd8a0e899875dd45ae63193de07988f8e669745023f4b28f2461e7b480b65f48a5a42174a4fb471c3d13cd3284900384a745c';
+			const keypair = Keypair.fromSecretKey(
+				convertStringToUin8Array(privateKey),
+			);
+			console.log(
+				await SolanaNet.queryAllByAddress(keypair.publicKey.toString()),
+			);
+		} catch (e) {
+			console.log(e);
+		}
 	});
 });

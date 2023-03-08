3ce6d6d14e33ca139a28e9b29334383883d6ff6f
kayaba <fluctlight-kayaba@gmail.com>
2023-03-08 19:50:42 +0700

    messaging event-engine, wip

---
+++ packages/messaging/utils/engine.ts
@@ -0,0 +1,22 @@
+import { RequestMetadata } from './types';
+
+export const requestEngine: Record<string, RequestMetadata> = {};
+
+export const makeRequestMetadata = (): RequestMetadata => {
+	return {} as never;
+};
+
+export const registerRequest = (meta: RequestMetadata) => {
+	requestEngine[meta.id] = meta;
+};
+
+setInterval(() => {
+	Object.keys(requestEngine).forEach((id) => {
+		const { timestamp, timeout = 10000, reject } = requestEngine[id];
+		const milliseconds = new Date().getTime() - timestamp.getTime();
+
+		if (milliseconds > timeout) {
+			reject(new Error('Request timeout'));
+		}
+	});
+}, 1000);
--- packages/messaging/utils/messenger.ts
+++ packages/messaging/utils/messenger.ts
@@ -1,32 +1,44 @@
+/* eslint-disable */
 import {
 	decryptFromString,
 	encryptToString,
 	restructCryptoKey,
 } from '@walless/crypto';
-import { Runtime } from 'webextension-polyfill';
 
-interface MessagePayload {
-	ciphered: string;
-	iv: string;
-}
+import { registerRequest } from './engine';
+import { EncryptedMessage, MessagePayload, ResponsePayload } from './types';
 
-export const sendEncryptedMessage = async <T>(
+export const sendMessage = async <T extends MessagePayload>(
 	payload: T,
-	from: Runtime.Port,
+	from: chrome.runtime.Port,
 ) => {
+	if (!payload.id) payload.id = crypto.randomUUID();
 	const key = await restructCryptoKey(`key@${from.name}`);
 	const iv = crypto.getRandomValues(new Uint8Array(12));
 	const data = JSON.stringify(payload);
 	const ciphered = await encryptToString(data, key, iv);
-	const ivString = new Buffer(iv).toString('base64');
-	const message: MessagePayload = { ciphered, iv: ivString };
+	const ivString = Buffer.from(iv).toString('base64');
+	const message: EncryptedMessage = { ciphered, iv: ivString };
 
-	return from.postMessage(message);
+	from.postMessage(message);
+	return payload;
 };
 
-export const decryptMessage = async <T>(
-	message: MessagePayload,
-	sender: Runtime.Port,
+export const sendRequest = async <
+	T extends MessagePayload,
+	R extends ResponsePayload,
+>(
+	payload: T,
+	from: chrome.runtime.Port,
+): Promise<R> => {
+	return new Promise((resolve, reject) => {
+		//
+	});
+};
+
+export const decryptMessage = async <T extends MessagePayload>(
+	message: EncryptedMessage,
+	sender: chrome.runtime.Port,
 ): Promise<T> => {
 	const key = await restructCryptoKey(`key@${sender.name}`);
 	const iv = new Uint8Array(Buffer.from(message.iv, 'base64'));

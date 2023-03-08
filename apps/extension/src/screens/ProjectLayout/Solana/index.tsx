f5307ec5feb30e50675985440aecba94e6f39156
FME849 <1414439@gmail.com>
2023-03-08 10:16:32 +0700

    initialize Solana and Under Realm layouts

---
+++ apps/extension/src/screens/ProjectLayout/Solana/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from '@walless/ui';
+
+export const Solana: React.FC = () => {
+	return (
+		<View className="flex-1 bg-red-600">
+			<Text>Solana</Text>
+		</View>
+	);
+};
+
+export default Solana;
---
+++ apps/extension/src/screens/ProjectLayout/UnderRealm/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from '@walless/ui';
+
+export const UnderRealm: React.FC = () => {
+	return (
+		<View className="flex-1 bg-color-2">
+			<Text>UnderRealm</Text>
+		</View>
+	);
+};
+
+export default UnderRealm;

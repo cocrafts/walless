a99a3e37e469b5dbd97577772868b7140764f61f
kayaba <fluctlight-kayaba@gmail.com>
2023-03-09 01:04:57 +0700

    batch @tkey/chrome-storage support web

--- apps/extension/src/utils/tkey.ts
+++ apps/extension/src/utils/tkey.ts
@@ -1,3 +1,5 @@
+import ChromeStorageModule from '@tkey/chrome-storage';
+import { ModuleMap } from '@tkey/common-types';
 import ThresholdKey from '@tkey/default';
 import PrivateKeyModule, {
 	ED25519Format,
@@ -8,14 +10,6 @@ import { TorusServiceProvider } from '@tkey/service-provider-torus';
 import WebStorageModule from '@tkey/web-storage';
 import { CustomAuthArgs } from '@toruslabs/customauth';
 
-const webStorageModule = new WebStorageModule();
-const securityQuestionsModule = new SecurityQuestionsModule();
-
-const privateKeyModule = new PrivateKeyModule([
-	new SECP256K1Format(),
-	new ED25519Format(),
-]);
-
 export const customAuthArgs: CustomAuthArgs = {
 	network: 'testnet',
 	baseUrl: `http://localhost:3002/`,
@@ -25,20 +19,33 @@ export const customAuthArgs: CustomAuthArgs = {
 	popupFeatures: 'width=380,height=600',
 };
 
-export type TypedThresholdKey = ThresholdKey & {
-	serviceProvider: TorusServiceProvider;
-	modules: {
-		webStorage: WebStorageModule;
-		securityQuestions: SecurityQuestionsModule;
-		privateKeyModule: PrivateKeyModule;
-	};
+type InternalModules = ModuleMap & {
+	webStorage?: WebStorageModule;
+	chromeStorage?: ChromeStorageModule;
+	securityQuestions: SecurityQuestionsModule;
+	privateKeyModule: PrivateKeyModule;
 };
 
+export type TypedThresholdKey = ThresholdKey & {
+	serviceProvider: TorusServiceProvider;
+	modules: InternalModules;
+};
+
+const modules: InternalModules = {
+	securityQuestions: new SecurityQuestionsModule(),
+	privateKeyModule: new PrivateKeyModule([
+		SECP256K1Format,
+		ED25519Format,
+	] as never),
+};
+
+if (window.chrome?.runtime) {
+	modules.chromeStorage = new ChromeStorageModule();
+} else {
+	modules.webStorage = new WebStorageModule();
+}
+
 export const key = new ThresholdKey({
-	modules: {
-		webStorage: webStorageModule,
-		securityQuestions: securityQuestionsModule,
-		privateKeyModule: privateKeyModule,
-	},
-	customAuthArgs: customAuthArgs,
+	modules,
+	customAuthArgs,
 }) as TypedThresholdKey;
---
+++ tool/batch/modules/@tkey/chrome-storage/src/ChromeExtensionStorageModule.ts
@@ -0,0 +1,55 @@
+import { DeviceShareDescription, IModule, ITKeyApi, ShareStore, StringifiedType } from "@tkey/common-types";
+
+export const CHROME_EXTENSION_STORAGE_MODULE_NAME = "chromeExtensionStorage";
+
+export default class ChromeExtensionStorageModule implements IModule {
+  moduleName: string;
+
+  tbSDK: ITKeyApi;
+
+  constructor() {
+    this.moduleName = CHROME_EXTENSION_STORAGE_MODULE_NAME;
+  }
+
+  setModuleReferences(tbSDK: ITKeyApi): void {
+    this.tbSDK = tbSDK;
+    this.tbSDK._setDeviceStorage(this.storeDeviceShare.bind(this));
+  }
+
+  // eslint-disable-next-line
+  async initialize(): Promise<void> { }
+
+  async storeDeviceShare(deviceShareStore: ShareStore, customDeviceInfo?: StringifiedType): Promise<void> {
+    await this.storeShareOnChromeExtensionStorage(deviceShareStore);
+    const shareDescription: DeviceShareDescription = {
+      module: this.moduleName,
+      userAgent: window.navigator.userAgent,
+      dateAdded: Date.now(),
+    };
+    if (customDeviceInfo) {
+      shareDescription.customDeviceInfo = JSON.stringify(customDeviceInfo);
+    }
+    await this.tbSDK.addShareDescription(deviceShareStore.share.shareIndex.toString("hex"), JSON.stringify(shareDescription), true);
+  }
+
+  async storeShareOnChromeExtensionStorage(share: ShareStore): Promise<void> {
+    const metadata = this.tbSDK.getMetadata();
+    const key = metadata.pubKey.x.toString("hex"); // tbkey public
+    return chrome.storage.sync.set({ [key]: JSON.stringify(share) });
+  }
+
+  async getStoreFromChromeExtensionStorage(): Promise<ShareStore> {
+    const metadata = this.tbSDK.getMetadata();
+    const key = metadata.pubKey.x.toString("hex"); // tbkey public
+    const result = await chrome.storage.sync.get(key);
+    const verifierIdObj: ShareStore = JSON.parse(result[key]);
+    await this.tbSDK.inputShareStoreSafe(verifierIdObj);
+    return verifierIdObj;
+  }
+
+  async inputShareFromChromeExtensionStorage(): Promise<void> {
+    const castedShareStore = await this.getStoreFromChromeExtensionStorage();
+    const latestShareDetails = await this.tbSDK.catchupToLatestShare({ shareStore: castedShareStore, includeLocalMetadataTransitions: true });
+    this.tbSDK.inputShareStore(latestShareDetails.latestShare);
+  }
+}

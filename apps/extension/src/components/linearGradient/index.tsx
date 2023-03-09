5660fed31b2630d2fa0e245fd371b41ef3742600
minhduypham <minhduypham@kms-technology.com>
2023-03-10 00:10:34 +0700

    add note for linear-gradient

--- apps/extension/src/components/linearGradient/index.tsx
+++ apps/extension/src/components/linearGradient/index.tsx
@@ -11,7 +11,15 @@ interface LinearGradientProps {
 	children: JSX.Element | null;
 	style?: object | null;
 }
-
+/**
+ *
+ * @param {string[]} colors array colors like [{"#1","#2"}]
+ * @param {number[] | null} locations width for each color
+ * @param {LinearGradientPoint|null} start start point
+ * @param {LinearGradientPoint | null} end end point
+ * @param {object | null} style style object
+ * @returns {JSX.Element}
+ */
 export const LinearGradient = (props: LinearGradientProps) => {
 	const { colors, locations, start, end, children, style } = props;
 	return (

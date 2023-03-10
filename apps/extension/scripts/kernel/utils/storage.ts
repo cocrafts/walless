f48bd96d18ac64f2e265435ceb98bdb7712cf577
kayaba <fluctlight-kayaba@gmail.com>
2023-03-10 08:51:34 +0700

    kernel itself cover domain-specific-encryption

--- apps/extension/scripts/kernel/utils/storage.ts
+++ apps/extension/scripts/kernel/utils/storage.ts
@@ -1,13 +1,3 @@
 import { createStorage } from '@walless/storage';
 
-import { notifySetting } from './messaging';
-
-const { settings } = createStorage();
-
-settings.hook('updating', (updates, id, previous) => {
-	const united = { ...previous, ...updates };
-
-	if (id === 1) {
-		notifySetting(united);
-	}
-});
+export const db = createStorage();

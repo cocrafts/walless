5273eff95c196828b83ff39f2f914837bc7f4280
Hieu <dev.ngtronghieu@gmail.com>
2023-03-11 16:58:36 +0700

    refactor remove layout

--- apps/extension/src/screens/Dashboard/Layout.tsx
+++ apps/extension/src/screens/Dashboard/Layout.tsx
@@ -1,11 +1,13 @@
 import { FC, ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Text, TouchableOpacity, View } from '@walless/ui';
+import { TouchableOpacity, View } from '@walless/ui';
 import { Avatar, IconButton } from '@walless/ui/components';
-import { CrossIcon, TimesIcon } from '@walless/ui/icons';
+import { CrossIcon } from '@walless/ui/icons';
 import { resources } from 'utils/config';
 import { useSnapshot } from 'utils/hook';
-import { layoutActions, layoutProxy } from 'utils/state/layout';
+import { layoutProxy } from 'utils/state/layout';
+
+import RemoveLayout from './RemoveLayout';
 
 interface Props {
 	children?: ReactNode;
@@ -35,35 +37,12 @@ export const DashboardLayout: FC<Props> = ({
 								onPress={() => navigate(`/layouts/${key}`)}
 							/>
 
-							<View className="absolute left-12 top-0 h-24 w-[200px] bg-[#00080E] rounded-lg px-3 py-2 flex gap-3">
-								<View className="absolute w-3 h-3 bg-[#00080E] rounded-sm rotate-45 top-3 -left-[2px]" />
-
-								<View className="flex flex-row gap-3 items-center">
-									<IconButton
-										size={36}
-										source={resources.icons.solana}
-										className="rounded-lg overflow-hidden"
-									/>
-									<Text>{layouts[key].name}</Text>
-								</View>
-
-								<View className="w-full border border-[#203C4E] rounded-lg" />
-
-								<TouchableOpacity
-									className="w-full flex flex-row justify-between items-center"
-									onPress={() => layoutActions.removeLayout(layouts[key].id)}
-								>
-									<Text className="text-[10px] [color:#587A90]">
-										Remove this layout
-									</Text>
-									<View className="flex flex-row justify-center items-center">
-										<View className="w-[6px] h-[6px] rounded-[1px] translate-x-1/2 bg-[#203C4E] rotate-45" />
-										<View className="w-[12px] h-[10px] rounded-sm bg-[#203C4E] flex justify-center items-center">
-											<TimesIcon size={16} color="#4E758E" />
-										</View>
-									</View>
-								</TouchableOpacity>
-							</View>
+							<RemoveLayout
+								className="absolute left-12 top-0 h-24 w-[200px]"
+								icon={resources.icons.solana}
+								name={layouts[key].name}
+								id={layouts[key].id}
+							/>
 						</View>
 					))}
 					<TouchableOpacity

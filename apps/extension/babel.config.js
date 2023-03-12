1a4810c547cff2c1e8d1fdf7e01f9861db289fa1
kayaba <fluctlight-kayaba@gmail.com>
2023-03-13 05:24:42 +0700

    configure @babel/plugin-proposal-private-methods

--- apps/extension/babel.config.js
+++ apps/extension/babel.config.js
@@ -14,6 +14,7 @@ module.exports = {
 				},
 			},
 		],
+		['@babel/plugin-proposal-private-methods', { loose: true }],
 		'@babel/plugin-proposal-export-namespace-from',
 		'react-native-reanimated/plugin',
 		'react-native-web',

aa36c2717df83f809771dafa2dd7493a772c5999
kayaba <fluctlight-kayaba@gmail.com>
2023-03-14 19:50:47 +0700

    include auth-response function for Home page

--- apps/home/src/stacks/Root.tsx
+++ apps/home/src/stacks/Root.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { themeState } from '@metacraft/ui';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import AuthenticationScreen from 'screens/Authentication';
+import AuthResponseScreen from 'screens/AuthResponse';
 import HomeScreen from 'screens/Home';
 import {
 	linking,
@@ -21,7 +21,7 @@ export const RootStack: FC = () => {
 		<NavigationContainer ref={navigationRef} linking={linking} theme={theme}>
 			<Stack.Navigator screenOptions={screenOptions}>
 				<Stack.Screen name="Home" component={HomeScreen} />
-				<Stack.Screen name="Authentication" component={AuthenticationScreen} />
+				<Stack.Screen name="AuthResponse" component={AuthResponseScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);

67b19d41c328927c1db10d25ec3b39191583f319
FME849 <1414439@gmail.com>
2023-03-14 18:00:47 +0700

    add detectMobile function to use in home website

---
+++ apps/home/src/utils/helper.ts
@@ -0,0 +1,15 @@
+export const detectMobile = (): boolean => {
+	const toMatch = [
+		/Android/i,
+		/webOS/i,
+		/iPhone/i,
+		/iPad/i,
+		/iPod/i,
+		/BlackBerry/i,
+		/Windows Phone/i,
+	];
+
+	return toMatch.some((toMatchItem) => {
+		return navigator.userAgent?.match(toMatchItem);
+	});
+};

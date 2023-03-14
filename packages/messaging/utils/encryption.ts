9606e9f9d50fa191331dd7ae66bfeb2b427a3aec
kayaba <fluctlight-kayaba@gmail.com>
2023-03-14 12:16:02 +0700

    messenger factory in @walless/messenger

--- packages/messaging/utils/encryption.ts
+++ packages/messaging/utils/encryption.ts
@@ -1,7 +1,13 @@
 import { EncryptionKeyRecord, WallessDB } from '@walless/storage';
 
-export const getEncryptionKeyVault = (db: WallessDB) => {
-	const get = async (id: string): Promise<CryptoKey> => {
+import {
+	CreateAndHydrateKeyVault,
+	EncryptionKeyVault,
+	GetKeyVault,
+} from './types';
+
+export const createEncryptionKeyVault = (db: WallessDB): EncryptionKeyVault => {
+	const get: GetKeyVault = async (id) => {
 		const hydrated = await db.encryptionKeys.get(id);
 		if (!hydrated) throw new Error(`Encryption key not found for ${id}`);
 
@@ -14,7 +20,7 @@ export const getEncryptionKeyVault = (db: WallessDB) => {
 		);
 	};
 
-	const createAndHydrate = async (id: string): Promise<CryptoKey> => {
+	const createAndHydrate: CreateAndHydrateKeyVault = async (id) => {
 		const keyParams: AesKeyGenParams = { name: 'AES-GCM', length: 256 };
 		const keyUsages: ReadonlyArray<KeyUsage> = ['encrypt', 'decrypt'];
 		const key = await crypto.subtle.generateKey(keyParams, true, keyUsages);

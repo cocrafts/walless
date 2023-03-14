525ba6190370dbbf47e84e0ee5842dcd08a9fe8f
FME849 <1414439@gmail.com>
2023-03-14 18:01:13 +0700

    update home website UI

--- apps/home/src/components/ExtensionIcon.tsx
+++ apps/home/src/components/ExtensionIcon.tsx
@@ -15,7 +15,7 @@ export const ProductIcon: FC<Props> = ({
 	onPress,
 }) => {
 	return (
-		<View className="px-8 hover:scale-95 cursor-pointer transform-gpu ease-in-out duration-300">
+		<View className="p-8 hover:scale-95 cursor-pointer transform-gpu ease-in-out duration-300">
 			<TouchableOpacity onPress={onPress}>
 				<Image className="w-20 h-20" source={iconSrc} />
 				<Text className="font-extralight text-center bg-white/10 px-3 py-2 rounded-full mt-5">

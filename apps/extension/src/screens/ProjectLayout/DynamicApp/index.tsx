55ede88d836aa2bd7d9baaa35341fb91b1ea98d9
kayaba-akihiko <kayaba.fluctlight@gmail.com>
2023-03-18 16:31:26 +0700

    temporary clone runner game

--- apps/extension/src/screens/ProjectLayout/DynamicApp/index.tsx
+++ apps/extension/src/screens/ProjectLayout/DynamicApp/index.tsx
@@ -1,14 +1,37 @@
-import { FC } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FC, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
-	container: {},
+	container: {
+		flex: 1,
+		backgroundColor: 'rgb(47, 47, 47)',
+	},
 });
 
 export const DynamicAppLayout: FC = () => {
+	useEffect(() => {
+		setTimeout(() => {
+			new global.Runner('.interstitial-wrapper');
+		}, 200);
+	}, []);
+
 	return (
 		<View style={styles.container}>
-			<Text>Index</Text>
+			<img
+				id="offline-resources-1x"
+				className="hidden"
+				src="/runner/100-offline-sprite.png"
+			/>
+			<img
+				id="offline-resources-2x"
+				className="hidden"
+				src="/runner/200-offline-sprite.png"
+			/>
+			<div id="main-frame-error" className="interstitial-wrapper">
+				<div id="main-content">
+					<div className="icon icon-offline" alt=""></div>
+				</div>
+			</div>
 		</View>
 	);
 };

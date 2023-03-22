93a958646543dd49c3795a9303f6018266b6da03
FME849 <1414439@gmail.com>
2023-03-22 10:44:04 +0700

    update fetchAllCollectibles func

--- apps/extension/scripts/kernel/utils/query.ts
+++ apps/extension/scripts/kernel/utils/query.ts
@@ -1,10 +1,29 @@
+import { PublicKey } from '@solana/web3.js';
 import { CollectibleRecord } from '@walless/storage';
 
 import { db } from '../storage';
 
-export const fetchCollectibles = async (): Promise<CollectibleRecord[]> => {
+import { connection } from './connection';
+export const fetchAllCollectibles = async (): Promise<CollectibleRecord[]> => {
 	const keys = await db.publicKeys.toArray();
+	const collectibles = db.collectibles;
 
-	console.log('fetch collectible of all keys', keys);
-	return [];
+	keys.forEach(async (key) => {
+		if (key.network === 'sui') return;
+		const publicKey = new PublicKey(key.id as string);
+		const collectiblesByWallet = await getNftsByOwner(connection, publicKey);
+		collectiblesByWallet.forEach((collectible) => {
+			collectibles.put({
+				id: collectible.address.toString(),
+				collectionId: collectible.collection?.address.toString(),
+				metadata: {
+					name: collectible.name,
+					symbol: collectible.symbol,
+					imageUri: collectible.uri,
+				},
+			});
+		});
+	});
+
+	return await collectibles.toArray();
 };

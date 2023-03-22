80c33e7fd359599dbac853f46a00df9350a8f73e
FME849 <1414439@gmail.com>
2023-03-22 10:54:02 +0700

    update fetchAllCollectibles to query collections metadata

--- apps/extension/scripts/kernel/utils/query.ts
+++ apps/extension/scripts/kernel/utils/query.ts
@@ -1,12 +1,20 @@
 import { PublicKey } from '@solana/web3.js';
-import { CollectibleRecord } from '@walless/storage';
+import { getNftCollections, getNftsByOwner } from '@walless/network';
+import { CollectibleRecord, CollectionRecord } from '@walless/storage';
 
 import { db } from '../storage';
 
 import { connection } from './connection';
-export const fetchAllCollectibles = async (): Promise<CollectibleRecord[]> => {
+
+interface CollectibleResult {
+	collectibles: CollectibleRecord[];
+	collections: CollectionRecord[];
+}
+
+export const fetchAllCollectibles = async (): Promise<CollectibleResult> => {
 	const keys = await db.publicKeys.toArray();
 	const collectibles = db.collectibles;
+	const collections = db.collections;
 
 	keys.forEach(async (key) => {
 		if (key.network === 'sui') return;
@@ -25,5 +33,25 @@ export const fetchAllCollectibles = async (): Promise<CollectibleRecord[]> => {
 		});
 	});
 
-	return await collectibles.toArray();
+	const collectionsKey = (await collectibles
+		.orderBy('collectionId')
+		.uniqueKeys()) as [];
+	collectionsKey.forEach(async (key: string) => {
+		const publicKey = new PublicKey(key);
+		const collectionInfo = await getNftCollections(connection, publicKey);
+
+		collections.put({
+			id: key,
+			metadata: {
+				name: collectionInfo.name,
+				symbol: collectionInfo.symbol,
+				imageUri: collectionInfo.uri,
+			},
+		});
+	});
+
+	return {
+		collectibles: await collectibles.toArray(),
+		collections: await collections.toArray(),
+	};
 };

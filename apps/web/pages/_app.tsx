3258431b4f1b3049dbd9f4e73cfcc7d5109c199d
kayaba <fluctlight-kayaba@gmail.com>
2023-03-26 20:57:59 +0700

    migrate bridge module from legacy implementation

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,6 +1,7 @@
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { TamaguiProvider } from 'tamagui';
+import { runBridge } from 'utils/bridge';
 
 import 'raf';
 import 'setimmediate';
@@ -11,6 +12,10 @@ import '@tamagui/core/reset.css';
 import '../style.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
+	useEffect(() => {
+		runBridge();
+	}, []);
+
 	return (
 		<TamaguiProvider config={config}>
 			<Suspense>

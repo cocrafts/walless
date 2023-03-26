4a3714df75e9bd370de74e2f4637df0f2b9aca02
kayaba <fluctlight-kayaba@gmail.com>
2023-03-26 20:58:59 +0700

    make core utils work with Next.js compiler

--- tool/webpack/middleware/env.js
+++ tool/webpack/middleware/env.js
@@ -2,7 +2,7 @@ const devGoogleClientId =
 	'474398641068-f7mbjjtstm2d7eb0lerhh4en7bp3gdd7.apps.googleusercontent.com';
 
 const setEnvironments =
-	(varables = {}) =>
+	(variables = {}) =>
 	(configs, internal) => {
 		const { webpack } = internal.modules;
 		const { DefinePlugin } = webpack;
@@ -15,7 +15,7 @@ const setEnvironments =
 			GOOGLE_CLIENT_ID: JSON.stringify(
 				process.env.GOOGLE_CLIENT_ID || devGoogleClientId,
 			),
-			...varables,
+			...variables,
 		});
 
 		return configs;

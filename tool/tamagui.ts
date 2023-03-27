b5805381e790313dcfac6abac45d1b74cc48943e
kayaba <fluctlight-kayaba@gmail.com>
2023-03-28 02:36:31 +0700

    share tamagui config between child Apps

---
+++ tool/tamagui.ts
@@ -0,0 +1,37 @@
+import { createAnimations } from '@tamagui/animations-css';
+import { shorthands } from '@tamagui/shorthands';
+import { themes, tokens } from '@tamagui/themes';
+import { createFont, createTamagui, TamaguiInternalConfig } from 'tamagui';
+
+const animations = createAnimations({
+	fast: 'ease-in 150ms',
+	medium: 'ease-in 300ms',
+	slow: 'ease-in 450ms',
+	'modal-mask': 'ease-in 250ms',
+});
+
+const fonts = {
+	body: createFont({
+		family: 'Arial',
+		size: {
+			4: 14,
+		},
+		lineHeight: {
+			4: 16,
+		},
+		letterSpacing: {
+			4: 1,
+		},
+		weight: {
+			4: 400,
+		},
+	}),
+};
+
+export const config: TamaguiInternalConfig = createTamagui({
+	themes,
+	tokens,
+	shorthands,
+	animations,
+	fonts,
+});

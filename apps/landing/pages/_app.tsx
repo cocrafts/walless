8fd35277f7ca2928f6a70070467d530edfb5fdba
kayaba <fluctlight-kayaba@gmail.com>
2023-03-28 02:37:10 +0700

    landing page: replicate app and document config from extension

--- apps/landing/pages/_app.tsx
+++ apps/landing/pages/_app.tsx
@@ -1,21 +1,34 @@
-import { FC, Suspense } from 'react';
+import { FC, Fragment, useMemo } from 'react';
+import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme';
+import { WuiProvider } from '@walless/wui';
 import { AppProps } from 'next/app';
-import { TamaguiProvider } from 'tamagui';
-
-import 'raf';
-import 'setimmediate';
 
 import config from '../tamagui.config';
 
 import '@tamagui/core/reset.css';
+import '../style.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
+	const [theme, setTheme] = useRootTheme();
+
+	const contents = useMemo(() => {
+		return <Component {...pageProps} />;
+	}, [pageProps]);
+
 	return (
-		<TamaguiProvider config={config}>
-			<Suspense>
-				<Component {...pageProps} />
-			</Suspense>
-		</TamaguiProvider>
+		<Fragment>
+			<script
+				key="tamagui-animations-mount"
+				dangerouslySetInnerHTML={{
+					__html: `document.documentElement.classList.add('t_unmounted')`,
+				}}
+			/>
+			<NextThemeProvider onChangeTheme={setTheme}>
+				<WuiProvider config={config} theme={theme}>
+					{contents}
+				</WuiProvider>
+			</NextThemeProvider>
+		</Fragment>
 	);
 };
 
--- apps/landing/pages/_document.tsx
+++ apps/landing/pages/_document.tsx
@@ -1,18 +1,45 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import { Children } from 'react';
+import { AppRegistry } from 'react-native';
+import NextDocument, {
+	DocumentContext,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from 'next/document';
 
-export default function Document() {
-	return (
-		<Html lang="en">
-			<Head>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,300;1,400;1,700&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+import Tamagui from '../tamagui.config';
+
+export default class Document extends NextDocument {
+	static async getInitialProps({ renderPage }: DocumentContext) {
+		AppRegistry.registerComponent('Main', () => Main);
+
+		const page = await renderPage();
+		// eslint-disable-next-line
+		const { getStyleElement } = (AppRegistry as any).getApplication('Main');
+		const styleInner = { __html: Tamagui.getCSS() };
+		const styles = [
+			getStyleElement(),
+			<style key="style" dangerouslySetInnerHTML={styleInner} />,
+		];
+
+		return { ...page, styles: Children.toArray(styles) };
+	}
+
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link href={fontUri} rel="stylesheet" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
+
+const fontUri =
+	'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,300;1,400;1,700&display=swap';

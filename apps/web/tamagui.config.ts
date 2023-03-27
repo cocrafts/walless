dde0ab54c79202b7c8dbb850a550c0569b4270c1
kayaba <fluctlight-kayaba@gmail.com>
2023-03-28 02:14:38 +0700

    use css-engine for animation in Browser

--- apps/web/tamagui.config.ts
+++ apps/web/tamagui.config.ts
@@ -1,46 +1,39 @@
-import { createAnimations } from '@tamagui/animations-reanimated';
+import { createAnimations } from '@tamagui/animations-css';
 import { shorthands } from '@tamagui/shorthands';
 import { themes, tokens } from '@tamagui/themes';
-import { createFont, createTamagui } from 'tamagui';
+import { createFont, createTamagui, TamaguiConfig } from 'tamagui';
 
-export default createTamagui({
+const animations = createAnimations({
+	fast: 'ease-in 150ms',
+	medium: 'ease-in 300ms',
+	slow: 'ease-in 450ms',
+	'modal-mask': 'ease-in 250ms',
+});
+
+const fonts = {
+	body: createFont({
+		family: 'Arial',
+		size: {
+			4: 14,
+		},
+		lineHeight: {
+			4: 16,
+		},
+		letterSpacing: {
+			4: 1,
+		},
+		weight: {
+			4: 400,
+		},
+	}),
+};
+
+const config: TamaguiConfig = createTamagui({
 	themes,
 	tokens,
 	shorthands,
-	animations: createAnimations({
-		fast: {
-			type: 'spring',
-			damping: 20,
-			mass: 1.2,
-			stiffness: 250,
-		},
-		medium: {
-			type: 'spring',
-			damping: 10,
-			mass: 0.9,
-			stiffness: 100,
-		},
-		slow: {
-			type: 'spring',
-			damping: 20,
-			stiffness: 60,
-		},
-	}),
-	fonts: {
-		body: createFont({
-			family: 'Arial',
-			size: {
-				4: 14,
-			},
-			lineHeight: {
-				4: 16,
-			},
-			letterSpacing: {
-				4: 1,
-			},
-			weight: {
-				4: 400,
-			},
-		}),
-	},
+	animations,
+	fonts,
 });
+
+export default config;

66ee86a311de0a463ed031874bea0e7c6ced2ec2
Tan Le <tanlethanh8266@gmail.com>
2023-03-27 10:58:39 +0700

    Add simple show, hide actions

--- packages/ui/utils/state/modal/index.ts
+++ packages/ui/utils/state/modal/index.ts
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 import { proxy } from 'valtio';
 
 import { referenceMap } from './helper';
-import { ModalState } from './type';
+import { ModalState, ShowModalConfigs } from './type';
 
 export const modalState = proxy<ModalState>({
 	count: 0,
@@ -14,11 +14,19 @@ export const modalActions = {
 	setContainerRef: (ref: RefObject<View>): void => {
 		referenceMap.root = ref;
 	},
-	show: (): void => {
+	show: ({ id, bindingRef, ...restConfigs }: ShowModalConfigs): void => {
 		console.log('show modal');
+		console.log(bindingRef);
+		const safeId = id || 'default-modal';
+		// Show
+		modalState.hashmap[safeId] = {
+			id: safeId,
+			...restConfigs,
+		};
 	},
-	hide: (): void => {
-		console.log('hide modal');
+	hide: (id: string): void => {
+		const instance = modalState.hashmap[id];
+		if (instance) instance.hide = true;
 	},
 };
 
--- packages/ui/utils/state/modal/type.ts
+++ packages/ui/utils/state/modal/type.ts
@@ -1,10 +1,16 @@
-import { FC } from 'react';
+import { FC, RefObject } from 'react';
+import { View } from 'react-native';
 
 export interface ModalConfigs {
 	id?: string;
 	component: FC<{ config: ModalConfigs }>;
 	context?: unknown;
+	hide?: boolean;
 }
+
+export type ShowModalConfigs = ModalConfigs & {
+	bindingRef?: RefObject<View>;
+};
 
 export interface ModalState {
 	count: number;

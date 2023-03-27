7f3116ffa8de43b3afc48cd22d96379a999f951c
kayaba <fluctlight-kayaba@gmail.com>
2023-03-27 18:27:08 +0700

    valtio as deps and fork modal state from @metacraft/ui

---
+++ packages/wui/state/modal/index.ts
@@ -0,0 +1,39 @@
+import { RefObject } from 'react';
+import { View } from 'react-native';
+import { proxy } from 'valtio';
+
+import { measureRelative, referenceMap } from './helper';
+import { ModalState, ShowModalConfigs } from './type';
+
+export const modalState = proxy<ModalState>({
+	count: 0,
+	hashmap: {},
+});
+
+export const modalActions = {
+	setContainerRef: (ref: RefObject<View>): void => {
+		referenceMap.root = ref;
+	},
+	show: ({ id, bindingRef, ...restConfigs }: ShowModalConfigs): void => {
+		const safeId = id || 'default-modal';
+
+		measureRelative(bindingRef).then((layout) => {
+			modalState.hashmap[safeId] = {
+				id: safeId,
+				bindingRectangle: layout,
+				...restConfigs,
+			};
+		});
+	},
+	hide: (id: string): void => {
+		const instance = modalState.hashmap[id];
+		if (instance) instance.hide = true;
+	},
+	destroy: (id?: string): void => {
+		const safeId = id || 'default-modal';
+		delete modalState.hashmap[safeId];
+	},
+};
+
+export * from './helper';
+export * from './type';

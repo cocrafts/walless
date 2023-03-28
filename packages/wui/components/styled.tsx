094b734bc9bb44a7f06ffcb05912f400fb9c2fb5
kayaba <fluctlight-kayaba@gmail.com>
2023-03-29 01:19:00 +0700

    instant feedback modal manager hack

--- packages/wui/components/styled.tsx
+++ packages/wui/components/styled.tsx
@@ -9,7 +9,19 @@ import {
 	ZStack as TamaguiZStack,
 } from 'tamagui';
 
-export const Stack = TamaguiStack;
+export const Stack = styled(TamaguiStack, {
+	variants: {
+		fullscreen: {
+			true: {
+				position: 'absolute',
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+			},
+		},
+	} as const,
+});
 export const XStack = TamaguiXStack;
 export const YStack = TamaguiYStack;
 export const ZStack = TamaguiZStack;

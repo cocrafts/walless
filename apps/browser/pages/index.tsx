00aaed941d8e21b90b9cf35fb907036485f48139
FME849 <1414439@gmail.com>
2023-03-30 16:01:22 +0700

    add DashboardLayout to Explore page

--- apps/browser/pages/index.tsx
+++ apps/browser/pages/index.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
-import { modalActions, Stack, Text } from '@walless/wui';
+import { Button, modalActions, Stack, Text } from '@walless/wui';
+import DashboardLayout from 'components/DashboardLayout';
+import { layoutActions } from 'utils/state/layout';
 
 export const Test: FC = () => {
 	return (
@@ -18,15 +20,24 @@ export const Explore = () => {
 		});
 	};
 
+	const addLayout = (id: string) => {
+		layoutActions.add({ id });
+	};
+
 	return (
-		<Stack flex={1} alignItems="center" justifyContent="center">
-			<Stack
-				style={{ backgroundColor: 'red' }}
-				cursor="pointer"
-				onPress={showModal}
-			>
-				<Text>Hmm</Text>
-			</Stack>
+		<Stack flex={1} alignItems="stretch" justifyContent="center">
+			<DashboardLayout>
+				<Stack
+					style={{ backgroundColor: 'red' }}
+					cursor="pointer"
+					onPress={showModal}
+				>
+					<Text>Hmm</Text>
+				</Stack>
+
+				<Button onPress={() => addLayout('solana')}>Solana</Button>
+				<Button onPress={() => addLayout('under realm')}> Under Realm</Button>
+			</DashboardLayout>
 		</Stack>
 	);
 };

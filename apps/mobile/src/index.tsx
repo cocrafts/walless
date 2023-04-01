d038df02ac7ca44bc7c30fa0ec2b2ffe5576b612
kayaba <fluctlight-kayaba@gmail.com>
2023-04-02 03:57:44 +0700

    load SplashScreen in Mobile

--- apps/mobile/src/index.tsx
+++ apps/mobile/src/index.tsx
@@ -1,14 +1,13 @@
 import { FC } from 'react';
-import { GuiProvider, Stack, Text } from '@walless/gui';
+import { SplashScreen } from '@walless/app';
+import { GuiProvider } from '@walless/gui';
 
 import config from '../tamagui.config';
 
 export const Index: FC = () => {
 	return (
 		<GuiProvider config={config}>
-			<Stack flex={1} alignItems="center" justifyContent="center">
-				<Text>Index!!</Text>
-			</Stack>
+			<SplashScreen />
 		</GuiProvider>
 	);
 };

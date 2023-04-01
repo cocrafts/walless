5a48cd5de9727b8b53bd91b381763183dfcb2e2a
kayaba <fluctlight-kayaba@gmail.com>
2023-04-02 03:26:34 +0700

    shared SplashScreen boilerplate

---
+++ packages/app/screens/Splash.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Stack, Text } from '@walless/gui';
+
+export const SplashScreen: FC = () => {
+	return (
+		<Stack>
+			<Text>Splash</Text>
+		</Stack>
+	);
+};
+
+export default SplashScreen;

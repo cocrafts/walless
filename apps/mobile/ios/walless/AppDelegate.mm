ad3872f76c3ebe1b03beeb6f363807d98fdb78a9
kayaba <fluctlight-kayaba@gmail.com>
2023-04-02 03:54:24 +0700

    default background color for IOS, customized LaunchScreen style

--- apps/mobile/ios/walless/AppDelegate.mm
+++ apps/mobile/ios/walless/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 
+#import <React/RCTRootView.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -7,7 +8,14 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"walless";
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  BOOL success = [super application:application didFinishLaunchingWithOptions:launchOptions];
+  
+  if (success) {
+    RCTRootView *rootView = (RCTRootView *)self.window.rootViewController.view;
+    rootView.backgroundColor = [[UIColor alloc] initWithRed:0.01 green:0.0901 blue:0.149 alpha:1];
+  }
+  
+  return success;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge

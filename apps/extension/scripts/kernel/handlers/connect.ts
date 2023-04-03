d6cf3d96e1107f1c7625629d5b8ee31b53d09ffc
Tan Le <tanlethanh8266@gmail.com>
2023-04-03 10:35:00 +0700

    Add checking trust domain

--- apps/extension/scripts/kernel/handlers/connect.ts
+++ apps/extension/scripts/kernel/handlers/connect.ts
@@ -1,10 +1,37 @@
+import { ConnectOptions } from '@walless/core';
 import { MessengerCallback } from '@walless/messaging';
 
 import { db } from '../storage';
 
 export const handleConnect: MessengerCallback = async (payload, channel) => {
+	const connectOption = payload.options as ConnectOptions;
+
+	console.log('connect option:', connectOption);
+
+	const trustedDomains = await db.trustedDomains.toArray();
+
+	let isTrusted = trustedDomains.find(
+		(i) => i.domainName == connectOption.domain,
+	)?.trusted;
+
+	if (!isTrusted) {
+		isTrusted = await triggerRequireTrustedDomain(
+			connectOption.domain as string,
+		);
+	}
+
+	if (!isTrusted) {
+		return console.log(
+			`handle connect: Domain name ${connectOption.domain} is not trusted`,
+		);
+	}
+
 	const publicKeys = await db.publicKeys.toArray();
 	const solKey = publicKeys.find((i) => i.network === 'solana');
+
+	if (!solKey) {
+		return console.log('handle connect: Public key not found');
+	}
 
 	console.log('send back response:', solKey.id);
 	channel.postMessage({
@@ -12,4 +39,10 @@ export const handleConnect: MessengerCallback = async (payload, channel) => {
 		requestId: payload.requestId,
 		publicKey: solKey.id,
 	});
+};
+
+const triggerRequireTrustedDomain = async (domainName: string) => {
+	// Temporary trigger method for simulate user action to trust any site
+	console.log(domainName);
+	return true;
 };

7abfe74bf02086a0366f1d5ec71453af7945fc95
Sipo <sipo@Clouds-MacBook-Pro.local>
2023-04-03 12:11:35 +0700

    Remove temp code and fix object field

--- apps/extension/scripts/kernel/handlers/connect.ts
+++ apps/extension/scripts/kernel/handlers/connect.ts
@@ -1,3 +1,5 @@
+import { hostname } from 'os';
+
 import { ConnectOptions } from '@walless/core';
 import { MessengerCallback } from '@walless/messaging';
 
@@ -5,8 +7,6 @@ import { db } from '../storage';
 
 export const handleConnect: MessengerCallback = async (payload, channel) => {
 	const { onlyIfTrusted, domain } = payload.options as ConnectOptions;
-
-	console.log('connect option:', onlyIfTrusted, domain);
 
 	const trustedDomains = await db.trustedDomains.toArray();
 
@@ -43,6 +43,13 @@ export const handleConnect: MessengerCallback = async (payload, channel) => {
 
 const triggerRequireTrustedDomain = async (domainName: string) => {
 	// Temporary trigger method for simulate user action to trust any site
-	console.log(domainName);
+	const newTrustedDomain = await db.trustedDomains.add({
+		id: domainName,
+		domainName: domainName,
+		trusted: true,
+		timestamp: new Date(),
+		connectCount: 0,
+	});
+	console.log('New trust domain: ', newTrustedDomain);
 	return true;
 };

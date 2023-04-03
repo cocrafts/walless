a7dff7f211a2e67e1ffd2a215be3569c95cc66ae
Hieu Nguyen <dev.ngtronghieu@gmail.com>
2023-04-03 13:31:22 +0700

    update routes

--- apps/web/src/routing.tsx
+++ apps/web/src/routing.tsx
@@ -1,15 +1,26 @@
 import { createHashRouter } from 'react-router-dom';
 
 import DashboardScreen from './screens/Dashboard';
+import ExploreScreen from './screens/Explore';
 import ProfileScreen from './screens/Profile';
 
 export const router = createHashRouter([
 	{
-		path: '/profile',
-		element: <ProfileScreen />,
-	},
-	{
-		path: '/:layoutId?',
+		path: '/',
 		element: <DashboardScreen />,
+		children: [
+			{
+				path: '/',
+				element: <ExploreScreen />,
+			},
+			{
+				path: '/profile',
+				element: <ProfileScreen />,
+			},
+			{
+				path: '/:layoutId?',
+				element: <DashboardScreen />,
+			},
+		],
 	},
 ]);

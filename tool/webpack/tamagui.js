c5a57a6ec15f7cb31851409586b2194f4b1fedaf
kayaba <fluctlight-kayaba@gmail.com>
2023-04-05 05:35:03 +0700

    esbuild experimental, for flexibility in different platforms

--- tool/webpack/tamagui.js
+++ tool/webpack/tamagui.js
@@ -4,29 +4,39 @@ const { resolve } = require('path');
 const { TamaguiPlugin } = require('tamagui-loader')
 
 const workspaceRoot = resolve(__dirname, '../..');
+const useEsBuild = process.env.ESBUILD === 'true';
 
 const tamaguiBuild = (config, internal) => {
 	const { webpack } = internal.modules;
+	const isProd = process.env.ENV === 'production';
 	const original = config.module.rules[0];
 	const tamaguiOptions = {
 		config: './tamagui.config.ts',
 		components: ['@tamagui/core'],
 		importsWhitelist: ['constants.js', 'colors.js'],
 		logTimings: true,
-		disableExtraction: process.env.NODE_ENV !== 'production',
+		disableExtraction: !isProd,
 	};
 
 	config.resolveLoader.modules.push(resolve(workspaceRoot, 'node_modules'));
 	config.module.rules[0] = {
-		test: /\.[jt]sx?$/,
+		test: /\.(ts|js)x?$/,
 		use: [
-			{
+			useEsBuild ? {
+				loader: 'esbuild-loader',
+				options: {
+					loader: 'tsx',
+					target: [
+						'chrome67',
+						'edge79',
+						'firefox68',
+						'opera54',
+						'safari14',
+					],
+				}
+			} : {
 				loader: original.loader,
 				options: original.options,
-			},
-			{
-				loader: 'tamagui-loader',
-				options: tamaguiOptions,
 			},
 		],
 	};

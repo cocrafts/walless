bb6ac56abfad07c7dc7cf069b986a71adce7e3ca
kayaba <fluctlight-kayaba@gmail.com>
2023-04-04 15:39:15 +0700

    self alias Animated module in packages/app

---
+++ packages/app/components/alias.ts
@@ -0,0 +1,11 @@
+import { Platform } from 'react-native';
+import Animated, { runOnJS as originalRunOnJS } from 'react-native-reanimated';
+import { Image } from '@walless/gui';
+
+export const AnimatedImage = Animated.createAnimatedComponent(Image);
+
+export const runOnJS = Platform.select({
+	default: originalRunOnJS as never,
+	/* eslint-disable-next-line */
+	web: (cb: any) => cb?.(),
+});

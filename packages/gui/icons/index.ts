4b520cb701c9c9ba869e138c9c8fa7d6aaa6c4fe
Hieu <dev.ngtronghieu@gmail.com>
2023-04-04 23:44:23 +0700

    add more icons

--- packages/gui/icons/index.ts
+++ packages/gui/icons/index.ts
@@ -1,3 +1,6 @@
 export { AlertCircle } from './AlertCircle';
+export { Bell } from './Bell';
 export { CheckCircle } from './CheckCircle';
 export { ChevronDown } from './ChevronDown';
+export { IdCard } from './IdCard';
+export { Setting } from './Setting';

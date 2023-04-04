4395bf6e991760616f4445f21a21a86e0a278f6f
kayaba <fluctlight-kayaba@gmail.com>
2023-04-04 12:28:11 +0700

    unify tamagui version across projects

--- apps/web/index.web.js
+++ apps/web/index.web.js
@@ -1,11 +1,15 @@
 import { createRoot } from 'react-dom/client';
 
+import '@tamagui/polyfill-dev';
 import 'raf/polyfill';
 import 'setimmediate';
 
 import App from './src';
+import tamaguiConfig from './tamagui.config';
+
+import '@tamagui/core/reset.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(<App tamaguiConfig={tamaguiConfig} />);

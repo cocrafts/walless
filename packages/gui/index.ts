43aba5af2780be1b613f41fa00592ef3c3a2c6a9
FME849 <1414439@gmail.com>
2023-04-05 15:59:17 +0700

    remove HyperLink component which is a duplicate of Anchor component

--- packages/gui/index.ts
+++ packages/gui/index.ts
@@ -1,6 +1,5 @@
 export * from './components/Anchor';
 export * from './components/Button';
-export * from './components/HyperLink';
 export * from './components/ModalManager';
 export * from './components/Provider';
 export * from './components/styled';

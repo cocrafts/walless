a2521e456e0fc0c710d23cbfd9a0ed733dec1299
kayaba <fluctlight-kayaba@gmail.com>
2023-04-06 16:42:56 +0700

    correct @walless/gui aliases

--- packages/gui/components/styled.tsx
+++ packages/gui/components/styled.tsx
@@ -15,21 +15,19 @@ export const Stack = styled(TamaguiStack, {
 				right: 0,
 				bottom: 0,
 			},
-		},
+		} as const,
 		horizontal: {
 			true: {
 				flexDirection: 'row',
 			},
-		},
-	} as const,
+		} as const,
+	},
 });
 
 export const Circle = styled(TamaguiStack, {
 	name: 'Circle',
 	borderRadius: 10000000,
 });
-
-export type CircleProps = GetProps<typeof Circle>;
 
 export const Text = styled(TamaguiText, {
 	name: 'Text',

3c0b1eab78548b5099e7222ff8d4d1cfba280022
FME849 <1414439@gmail.com>
2023-04-07 19:28:25 +0700

    add DashboardButton component to @walless/app

---
+++ packages/app/components/DashboardButton/ActiveHighlight.tsx
@@ -0,0 +1,17 @@
+import { Stack } from '@walless/gui';
+
+export const ActiveHighlight = () => {
+	return (
+		<Stack position="absolute" top={10} bottom={10} left={0}>
+			<Stack
+				flex={1}
+				width={2}
+				backgroundColor="#FFFFFF"
+				borderTopRightRadius={5}
+				borderBottomRightRadius={5}
+			/>
+		</Stack>
+	);
+};
+
+export default ActiveHighlight;

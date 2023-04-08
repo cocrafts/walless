63d25bafd05407a27667e5b7335773e9e45d69bb
kayaba <fluctlight-kayaba@gmail.com>
2023-04-08 16:09:24 +0700

    read *.md as raw text resource

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,15 +1,31 @@
 const withPlugins = require('next-compose-plugins');
 const { withTamagui } = require('@tamagui/next-plugin');
 
-module.exports = withPlugins([
-	withTamagui({
-		config: './tamagui.config.js',
-		components: ['@tamagui/core'],
-		useReactNativeWebLite: true,
-		disableExtraction: process.env.NODE_ENV !== 'production',
-		excludeReactNativeWebExports: ['Switch', 'ProgressBar', 'Picker'],
-	}),
+module.exports = withPlugins(
+	[
+		withTamagui({
+			config: './tamagui.config.js',
+			components: ['@tamagui/core'],
+			useReactNativeWebLite: true,
+			disableExtraction: process.env.NODE_ENV !== 'production',
+			excludeReactNativeWebExports: ['Switch', 'ProgressBar', 'Picker'],
+		}),
+		{
+			transpilePackages: [
+				'@walless/gui',
+				'@walless/icons',
+				'@walless/markdown',
+			],
+		},
+	],
 	{
-		transpilePackages: ['@walless/gui', '@walless/icons', '@walless/markdown'],
+		webpack: (config) => {
+			config.module.rules.push({
+				test: /\.md$/i,
+				type: 'asset/source',
+			});
+
+			return config;
+		},
 	},
-]);
+);

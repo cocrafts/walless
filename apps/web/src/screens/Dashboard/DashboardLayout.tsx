277d076ae2dc4ee8674f0ec6115470ffbf99770e
kayaba-akihiko <kayaba.fluctlight@gmail.com>
2023-04-11 01:53:16 +0700

    more effective Dashboard markup

--- apps/web/src/screens/Dashboard/DashboardLayout.tsx
+++ apps/web/src/screens/Dashboard/DashboardLayout.tsx
@@ -13,37 +13,27 @@ const DashboardLayout: FC<Props> = ({ children }) => {
 	const onProfilePress = () => router.navigate('/profile');
 
 	return (
-		<Stack
-			backgroundColor="#19232C"
-			flex={1}
-			display="flex"
-			flexDirection="row"
-		>
-			<Stack
-				width={50}
-				backgroundColor="#131C24"
-				alignItems="stretch"
-				paddingVertical={20}
-			>
-				<Stack flex={1} alignItems="stretch">
-					<Stack flex={1}>
-						<DashboardButton onPress={onExplorePress}>
-							<Compass size={20} />
-						</DashboardButton>
-					</Stack>
+		<Stack backgroundColor="#19232C" flex={1} flexDirection="row">
+			<Stack backgroundColor="#131C24" padding={8}>
+				<Stack flex={1}>
+					<DashboardButton onPress={onExplorePress}>
+						<Compass size={20} />
+					</DashboardButton>
+				</Stack>
 
-					<Stack alignItems="center" justifyContent="flex-end">
-						<DashboardButton onPress={onProfilePress}>
-							<Image
-								src={{ uri: '/img/mock-avatar.png' }}
-								width={36}
-								height={36}
-							/>
-						</DashboardButton>
-					</Stack>
+				<Stack alignItems="center" justifyContent="flex-end">
+					<DashboardButton onPress={onProfilePress}>
+						<Image
+							src={{ uri: '/img/mock-avatar.png' }}
+							width={36}
+							height={36}
+						/>
+					</DashboardButton>
 				</Stack>
 			</Stack>
-			<ScrollView flexGrow={1}>{children}</ScrollView>
+			<Stack flex={1}>
+				<ScrollView>{children}</ScrollView>
+			</Stack>
 		</Stack>
 	);
 };

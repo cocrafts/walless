fe422db6cfc45e1fd683ba6c6e140c2d1b10a58a
Tan Le <tanlethanh8266@gmail.com>
2023-04-12 12:45:33 +0700

    Add type for render and value for parent action

--- apps/web/src/screens/Profile/modals/SendToken/internal.tsx
+++ apps/web/src/screens/Profile/modals/SendToken/internal.tsx
@@ -1,6 +1,9 @@
+import { Networks } from '@walless/core';
+
 export interface DropdownItemProps {
 	id: string;
 	name: string;
+	value?: Networks | string;
 	icon?: React.ReactNode;
 	onPress?: () => void;
 	isSelected?: boolean;
@@ -8,6 +11,7 @@ export interface DropdownItemProps {
 
 export interface DropdownProps {
 	name: string;
+	setValue: (value: unknown) => void;
 	icon?: React.ReactNode;
 	items: DropdownItemProps[];
 }
@@ -16,16 +20,19 @@ export const tokens: DropdownItemProps[] = [
 	{
 		id: 'tk1',
 		name: 'SOL',
+		value: 'SOL',
 		icon: '/img/send-token/icon-solana.png',
 	},
 	{
 		id: 'tk2',
 		name: 'SUI',
+		value: 'SUI',
 		icon: '/img/send-token/icon-sui.png',
 	},
 	{
 		id: 'tk3',
 		name: 'URG',
+		value: 'SUI',
 		icon: '/img/send-token/icon-urg.png',
 	},
 ];
@@ -34,22 +41,29 @@ export const networks: DropdownItemProps[] = [
 	{
 		id: 'nw1',
 		name: 'Solana',
+		value: Networks.solana,
 		icon: '/img/send-token/icon-solana.png',
 	},
 	{
 		id: 'nw2',
 		name: 'SUI',
+		value: Networks.sui,
 		icon: '/img/send-token/icon-sui.png',
 	},
 ];
 
-export const mockDropdownItems: DropdownProps[] = [
+export const dropdownItems: Omit<
+	DropdownProps & { type: string },
+	'setValue'
+>[] = [
 	{
 		name: 'Select token',
+		type: 'token',
 		items: tokens,
 	},
 	{
 		name: 'Select network',
+		type: 'network',
 		items: networks,
 	},
 ];

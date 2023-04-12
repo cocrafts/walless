5a7e314730b90d365e89654eb252e8ed9ced99c4
kayaba-akihiko <fluctlight-kayaba@gmail.com>
2023-04-12 17:30:17 +0700

    "target" prop for Anchor component

--- apps/landing/components/Anchor.tsx
+++ apps/landing/components/Anchor.tsx
@@ -1,18 +1,24 @@
 import { UrlObject } from 'url';
 
-import { FC, ReactNode } from 'react';
+import { FC, HTMLAttributeAnchorTarget, ReactNode } from 'react';
 import { TextProps } from '@tamagui/core';
 import { Text } from '@walless/gui';
 import Link from 'next/link';
 
 export type AnchorProps = TextProps & {
 	href: string | UrlObject;
+	target?: HTMLAttributeAnchorTarget;
 	children?: ReactNode;
 };
 
-export const Anchor: FC<AnchorProps> = ({ href, children, ...textProps }) => {
+export const Anchor: FC<AnchorProps> = ({
+	href,
+	target,
+	children,
+	...textProps
+}) => {
 	return (
-		<Link href={href}>
+		<Link href={href} target={target}>
 			<Text hoverStyle={hoverStyle} pressStyle={pressStyle} {...textProps}>
 				{children}
 			</Text>

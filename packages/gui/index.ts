45683373e56b95340e4c32ed740b3e8bf717ee4a
kayaba-akihiko <kayaba.fluctlight@gmail.com>
2023-04-13 05:42:13 +0700

    use and export shared fullScreenStyle

--- packages/gui/index.ts
+++ packages/gui/index.ts
@@ -4,4 +4,5 @@ export * from './components/Image';
 export * from './components/Input';
 export * from './components/Provider';
 export * from './components/ScrollView';
+export * from './components/shared';
 export * from './components/styled';

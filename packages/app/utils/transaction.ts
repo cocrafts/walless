058c424c72db61f59c22f98602cf4d5e251d1b6b
Tan Le <tanlethanh8266@gmail.com>
2023-04-12 16:14:10 +0700

    Add get public key by network

--- packages/app/utils/transaction.ts
+++ packages/app/utils/transaction.ts
@@ -3,10 +3,13 @@ import {
 	Connection,
 	Keypair,
 	LAMPORTS_PER_SOL,
+	PublicKey,
 	SystemProgram,
 	TransactionMessage,
+	VersionedTransaction,
 } from '@solana/web3.js';
 import { Networks } from '@walless/core';
+import { db } from 'utils/storage';
 
 import { connection } from './../../network/src/utils/connection';
 
@@ -14,19 +17,42 @@ const solConn = new Connection(clusterApiUrl('devnet'));
 const sampleKeypair = Keypair.generate();
 
 type SendTokenProps = {
+	sender: string;
 	token: string;
 	network: Networks;
 	receiver: string;
 	amount: number;
 };
 
-export const sendToken = ({
+export const getWalletPublicKey = async (network: Networks) => {
+	const keyString = (await db.publicKeys.toArray()).find(
+		(e) => e.network == network,
+	)?.id;
+
+	console.log(keyString);
+	return keyString;
+};
+
+export const sendToken = async ({
+	sender,
 	token,
 	network,
 	receiver,
 	amount,
 }: SendTokenProps) => {
-	return 0;
+	if (network == Networks.solana) {
+		if (token == 'SOL') {
+			return await sendSOL(
+				new PublicKey(sender),
+				new PublicKey(receiver),
+				amount,
+			);
+		} else {
+			throw Error('Token is not supported');
+		}
+	} else {
+		throw Error('Network is not supported');
+	}
 };
 
 export const getTransactionFee = async (network: Networks) => {
@@ -51,4 +77,32 @@ export const getTransactionFee = async (network: Networks) => {
 			((await solConn.getFeeForMessage(message)).value || 0) / LAMPORTS_PER_SOL
 		);
 	} else return 0;
+};
+
+const sendSOL = async (
+	sender: PublicKey,
+	receiver: PublicKey,
+	amount: number,
+) => {
+	const instructions = [
+		SystemProgram.transfer({
+			fromPubkey: sender,
+			toPubkey: receiver,
+			lamports: amount * LAMPORTS_PER_SOL,
+		}),
+	];
+
+	const blockhash = await connection
+		.getLatestBlockhash()
+		.then((res) => res.blockhash);
+
+	const message = new TransactionMessage({
+		payerKey: new PublicKey(sender),
+		recentBlockhash: blockhash,
+		instructions,
+	}).compileToV0Message();
+
+	const transaction = new VersionedTransaction(message);
+
+	return 0;
 };

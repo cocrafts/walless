725860d41a07a4053372b5ba24356fac74769a61
Tan Le <tanlethanh8266@gmail.com>
2023-04-12 11:47:54 +0700

    Add handler for onpress send

--- apps/web/src/screens/Profile/index.tsx
+++ apps/web/src/screens/Profile/index.tsx
@@ -1,3 +1,4 @@
+import { AnimateDirections, modalActions } from '@walless/app';
 import { Stack } from '@walless/gui';
 
 import Collectibles from './components/Collectibles';
@@ -5,8 +6,17 @@ import History from './components/History';
 import MainFeatures from './components/MainFeatures';
 import TokenValue from './components/TokenValue';
 import Widgets from './components/Widgets';
+import SendTokenScreen from './modals/SendToken';
 
 const ProfileScreen = () => {
+	const handlePressSendBtn = () => {
+		modalActions.show({
+			id: 'send-token',
+			component: SendTokenScreen,
+			animateDirection: AnimateDirections.Top,
+		});
+	};
+
 	return (
 		<Stack maxHeight="100vh" display="flex" alignItems="center" gap={28}>
 			<Stack alignSelf="flex-end" margin={15}>
@@ -16,7 +26,7 @@ const ProfileScreen = () => {
 			<TokenValue />
 
 			<Stack marginTop={27} marginBottom={34}>
-				<MainFeatures />
+				<MainFeatures onPressSendBtn={handlePressSendBtn} />
 			</Stack>
 
 			<Collectibles />

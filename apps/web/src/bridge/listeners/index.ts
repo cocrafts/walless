2cf7c4888cdb4e99c98c18155f62735a0ad108a7
Tan Le <tanlethanh8266@gmail.com>
2023-04-14 17:29:36 +0700

    Add request sign and send transaction

--- apps/web/src/bridge/listeners/index.ts
+++ apps/web/src/bridge/listeners/index.ts
@@ -1,7 +1,24 @@
+import { VersionedTransaction } from '@solana/web3.js';
 import { encryptedMessenger } from 'bridge/utils/messaging';
+import { encode } from 'bs58';
 
 export const registerMessageHandlers = async () => {
 	setTimeout(async () => {
 		await encryptedMessenger.send('kernel', { type: 'notify-wallet-open' });
 	}, 50);
+};
+
+export const requestSignAndSendTransaction = async (
+	transaction: VersionedTransaction,
+	options: unknown = undefined,
+	passcode: string | undefined = undefined,
+) => {
+	const res = await encryptedMessenger.request('kernel', {
+		type: 'sign-and-send-transaction',
+		transaction: encode(transaction.serialize()),
+		options,
+		passcode,
+	});
+
+	return res;
 };

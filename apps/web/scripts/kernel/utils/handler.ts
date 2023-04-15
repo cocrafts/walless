17eae1d386cd5a521ce84c17d37df34a48f8ca41
Tan Le <tanlethanh8266@gmail.com>
2023-04-15 09:28:14 +0700

    Add settings with getPrivateKey method

--- apps/web/scripts/kernel/utils/handler.ts
+++ apps/web/scripts/kernel/utils/handler.ts
@@ -1,7 +1,12 @@
+import { Networks } from '@walless/core';
 import { decryptWithPasscode } from '@walless/crypto';
 import { PrivateKeyRecord, PublicKeyRecord } from '@walless/storage';
 
 import { db } from '../storage';
+
+export const settings = {
+	requirePasscode: true,
+};
 
 export const triggerActionToGetPrivateKey = async () => {
 	try {
@@ -14,7 +19,19 @@ export const triggerActionToGetPrivateKey = async () => {
 		return await decryptWithPasscode('123456', encrypted as PrivateKeyRecord);
 	} catch (error) {
 		console.log('Get private key error');
-		console.log(error.message);
+		console.log((error as Error).message);
 		return null;
+	}
+};
+
+export const getPrivateKey = async (network: Networks, passcode: string) => {
+	if (network == Networks.solana) {
+		const publicKeys = await db.publicKeys.toArray();
+		const solKey = publicKeys.find(
+			(i) => i.network === 'solana',
+		) as PublicKeyRecord;
+		const privateKeys = await db.privateKeys.toArray();
+		const encrypted = privateKeys.find((i) => i.id === solKey.privateKeyId);
+		return await decryptWithPasscode(passcode, encrypted as PrivateKeyRecord);
 	}
 };

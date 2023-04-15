92f059d0a17e650badf84712066e743af72128d1
kayaba-akihiko <fluctlight-kayaba@gmail.com>
2023-04-16 05:12:06 +0700

    large WalletCard icon

--- packages/app/features/NetworkLayout/WalletCard/index.tsx
+++ packages/app/features/NetworkLayout/WalletCard/index.tsx
@@ -1,5 +1,5 @@
 import { type FC, useState } from 'react';
-import { type StackProps, ImageBackground } from '@walless/gui';
+import { type StackProps, Image, ImageBackground, Stack } from '@walless/gui';
 import { TokenRecord } from '@walless/storage';
 
 import WalletAddress from './Address';
@@ -24,11 +24,21 @@ export const WalletCard: FC<Props> = ({ index = 0, skin, token }) => {
 			source={skin.backgroundSrc}
 			aspectRatio={318 / 145}
 			borderRadius={12}
-			padding={18}
+			paddingHorizontal={18}
+			justifyContent="center"
 			overflow="hidden"
 		>
 			<WalletAddress index={index} skin={skin} token={token} />
 			<WalletBalance token={token} isPrivate={isPrivate} onHide={handleHide} />
+			<Stack
+				position="absolute"
+				top={0}
+				right={-34}
+				bottom={0}
+				justifyContent="center"
+			>
+				<Image src={skin.largeIconSrc} width={130} height={130} />
+			</Stack>
 		</ImageBackground>
 	);
 };

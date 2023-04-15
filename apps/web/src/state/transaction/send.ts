068131101914f61c85f7b7317a1fbb8fb3b0c22c
Tan Le <tanlethanh8266@gmail.com>
2023-04-15 09:23:12 +0700

    Moving create and send transaction to global scope

---
+++ apps/web/src/state/transaction/send.ts
@@ -0,0 +1,56 @@
+import {
+	BindDirections,
+	constructTransaction,
+	modalActions,
+	modalState,
+} from '@walless/app';
+import { Networks } from '@walless/core';
+import { ResponseCode } from '@walless/messaging';
+import { requestSignAndSendTransaction } from 'bridge/listeners';
+import PasscodeScreen from 'screens/Profile/modals/Passcode';
+import TransactionSuccessfulScreen from 'screens/Profile/modals/TransactionSuccessful';
+
+export interface TransactionPayload {
+	sender: string;
+	receiver: string;
+	amount: number;
+	network: Networks;
+	token: string;
+	passcode?: string;
+}
+
+export const createAndSend = async (
+	payload: TransactionPayload,
+	passcode?: string,
+) => {
+	const transaction = await constructTransaction({
+		...payload,
+	});
+
+	const res = await requestSignAndSendTransaction(transaction, null, passcode);
+
+	if (res?.responseCode === ResponseCode.SUCCESS) {
+		modalState.map.clear();
+		modalActions.show({
+			id: 'transaction-successfully',
+			bindingDirection: BindDirections.InnerBottom,
+			component: TransactionSuccessfulScreen as never,
+			context: {
+				...payload,
+				signatureString: res.signatureString,
+			},
+		});
+	} else if (res?.responseCode === ResponseCode.REQUIRE_PASSCODE) {
+		modalState.map.clear();
+		modalActions.show({
+			id: 'confirm-transaction-with-passcode',
+			bindingDirection: BindDirections.InnerBottom,
+			component: PasscodeScreen as never,
+			context: {
+				...payload,
+			},
+		});
+	}
+
+	return res;
+};

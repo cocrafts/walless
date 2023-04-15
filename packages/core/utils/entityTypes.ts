87cf3a7cf79323a70aa4c0fdc1306933b1a928d9
kayaba-akihiko <fluctlight-kayaba@gmail.com>
2023-04-15 23:30:18 +0700

    register Extension entity interface

--- packages/core/utils/entityTypes.ts
+++ packages/core/utils/entityTypes.ts
@@ -1,6 +1,17 @@
+import { type ImageSourcePropType } from 'react-native';
+
 export interface UserProfile {
 	id?: string;
 	email?: string;
 	name?: string;
 	profileImage?: string;
+}
+
+export type ExtensionType = 'layout' | 'dApp' | 'native';
+
+export interface ExtensionConfig {
+	id: string;
+	color?: string;
+	icon: ImageSourcePropType;
+	type: ExtensionType;
 }

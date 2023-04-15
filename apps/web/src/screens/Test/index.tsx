7a6e00c3dec7972891250f5199cb8fb83ce2dd68
ltminhthu <ltminhthu.209@gmail.com>
2023-04-15 16:29:54 +0700

    create test screen

---
+++ apps/web/src/screens/Test/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import { Stack } from '@walless/gui';
+
+import InputDropdown from './components/InputDropdown';
+
+const TestScreen = () => {
+	return (
+		<Stack>
+			<InputDropdown />
+		</Stack>
+	);
+};
+
+export default TestScreen;

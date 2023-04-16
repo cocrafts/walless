a9743938f50341a5f113c9b0be19d4d3d1231ad5
Tan Le <tanlethanh8266@gmail.com>
2023-04-16 12:50:57 +0700

    Change Social link

--- apps/landing/components/layouts/Home/Footer/SocialIcons.tsx
+++ apps/landing/components/layouts/Home/Footer/SocialIcons.tsx
@@ -27,33 +27,29 @@ const SocialIcons = () => {
 
 			<SocialIcon
 				size={48}
-				link="https://discord.gg/2bzf9qjuN3"
+				link="https://twitter.com/walless_wallet"
 				icon={<Twitter size={40} />}
 			/>
 
 			<SocialIcon
 				size={48}
-				link="https://discord.gg/2bzf9qjuN3"
+				link="https://github.com/cocrafts/walless"
 				icon={<Github size={40} />}
 			/>
 
 			<SocialIcon
 				size={48}
-				link="https://discord.gg/2bzf9qjuN3"
+				link="https://www.linkedin.com/company/wallessio/"
 				icon={<LinkedIn size={40} />}
 			/>
 
 			<SocialIcon
 				size={48}
-				link="https://discord.gg/2bzf9qjuN3"
+				link="https://www.youtube.com/@Wallesslabs"
 				icon={<Youtube size={40} />}
 			/>
 
-			<SocialIcon
-				size={48}
-				link="https://discord.gg/2bzf9qjuN3"
-				icon={<Mail size={40} />}
-			/>
+			<SocialIcon size={48} link="hello@walless.io" icon={<Mail size={40} />} />
 		</Stack>
 	);
 };

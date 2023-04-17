9867f680bcd7faec8e913a4a087c68f4c167c61c
kayaba-akihiko <kayaba.fluctlight@gmail.com>
2023-04-17 20:35:53 +0700

    styling PinInput

--- packages/app/components/PinInput/Pin.tsx
+++ packages/app/components/PinInput/Pin.tsx
@@ -4,16 +4,22 @@ import {
 	TextInput,
 	TextInputKeyPressEventData,
 } from 'react-native';
-import { Input } from '@walless/gui';
+import { Input, Stack } from '@walless/gui';
 
 interface Props {
 	index: number;
+	width?: number;
+	height?: number;
 	pins: string;
 	onChange?: (value: string, index: number) => void;
 }
 
+const dotSize = 10;
+
 export const Pin = forwardRef<TextInput, Props>(
-	({ index, pins, onChange }, ref) => {
+	({ index, width = 42, height = 48, pins, onChange }, ref) => {
+		const value = pins[index] || '';
+		const filled = value?.length > 0;
 		const handleKeyPress = ({
 			nativeEvent,
 		}: NativeSyntheticEvent<TextInputKeyPressEventData>) => {
@@ -23,15 +29,39 @@ export const Pin = forwardRef<TextInput, Props>(
 		};
 
 		return (
-			<Input
-				ref={ref}
-				pointerEvents="none"
-				value={pins[index] || ''}
-				maxWidth={42}
-				maxLength={1}
-				onChangeText={(value: string) => onChange?.(value, index)}
-				onKeyPress={handleKeyPress}
-			/>
+			<Stack>
+				<Input
+					ref={ref}
+					secureTextEntry
+					pointerEvents="none"
+					textAlign="center"
+					value={value}
+					fontSize={14}
+					width={width}
+					height={height}
+					maxLength={1}
+					borderRadius={12}
+					borderWidth={2}
+					borderColor={filled ? 'rgba(255, 255, 255, 0.1)' : 'transparent'}
+					carretColor="white"
+					backgroundColor="#0e141a"
+					focusStyle={{ borderColor: 'rgba(255, 255, 255, 0.15)' }}
+					onChangeText={(value: string) => onChange?.(value, index)}
+					onKeyPress={handleKeyPress}
+				/>
+				{filled && (
+					<Stack
+						pointerEvents="none"
+						position="absolute"
+						top={height / 2 - dotSize / 2}
+						left={width / 2 - dotSize / 2}
+						width={dotSize}
+						height={dotSize}
+						borderRadius={dotSize / 2}
+						backgroundColor="white"
+					/>
+				)}
+			</Stack>
 		);
 	},
 );

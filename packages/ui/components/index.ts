4f9c5bd5a6d621b2500ef33f1a9479dc2f6f5411
kayaba-akihiko <kayaba.fluctlight@gmail.com>
2023-04-18 11:20:36 +0700

    remove legacy @walless/ui

--- packages/ui/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Avatar } from './Avatar';
-export { Button } from './Button';
-export { ContextMenuContainer } from './ContextMenuContainer/index.web';
-export { IconButton } from './IconButton';
--- packages/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './components';
-export * from './icons';
-export * from './managed';
-export * from './utils/state/env';
-export * from './utils/types';

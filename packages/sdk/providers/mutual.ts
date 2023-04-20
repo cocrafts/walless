d7b85e91bc9880183b7278b6589ed4648e9b322f
Tan Le <tanlethanh8266@gmail.com>
2023-04-20 12:36:52 +0700

    Refactor with RequestType

--- packages/sdk/providers/mutual.ts
+++ packages/sdk/providers/mutual.ts
@@ -1,11 +1,12 @@
 import { ConnectOptions } from '@walless/core';
+import { RequestType } from '@walless/messaging';
 
 import { sendRequest } from '../utils/messaging';
 
 export const requestConnect = async (options: ConnectOptions) => {
 	return await sendRequest({
 		from: 'walless@sdk',
-		type: 'request-connect',
+		type: RequestType.REQUEST_CONNECT,
 		options,
 	});
 };

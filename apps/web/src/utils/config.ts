34b9fd4b70197504d81bdb178d8283b980dd5eeb
Tan Le <tanlethanh8266@gmail.com>
2023-04-20 17:27:58 +0700

    Add icon

--- apps/web/src/utils/config.ts
+++ apps/web/src/utils/config.ts
@@ -3,24 +3,5 @@ export const resources = {
 		icon: {
 			uri: '/img/walless-icon.png',
 		},
-		text: {
-			uri: '/img/walless-text.png',
-		},
-	},
-	home: {
-		preview: {
-			explore: {
-				uri: '/img/home/preview-explore.png',
-			},
-			solana: {
-				uri: '/img/home/preview-solana.png',
-			},
-			sui: {
-				uri: '/img/home/preview-sui.png',
-			},
-			underRealm: {
-				uri: '/img/home/preview-under-realm.png',
-			},
-		},
 	},
 };

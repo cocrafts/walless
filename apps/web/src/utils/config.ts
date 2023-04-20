93a411193877eeef958ef562fec5624e776cfac3
Tan Le <tanlethanh8266@gmail.com>
2023-04-20 13:58:16 +0700

    Add config.ts with resources

---
+++ apps/web/src/utils/config.ts
@@ -0,0 +1,26 @@
+export const resources = {
+	walless: {
+		icon: {
+			uri: '/img/walless-icon.png',
+		},
+		text: {
+			uri: '/img/walless-text.png',
+		},
+	},
+	home: {
+		preview: {
+			explore: {
+				uri: '/img/home/preview-explore.png',
+			},
+			solana: {
+				uri: '/img/home/preview-solana.png',
+			},
+			sui: {
+				uri: '/img/home/preview-sui.png',
+			},
+			underRealm: {
+				uri: '/img/home/preview-under-realm.png',
+			},
+		},
+	},
+};

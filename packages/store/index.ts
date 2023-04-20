730bcf258c7c0649dc7bc51f251dbcd27d5ab172
kayaba-akihiko <kayaba.fluctlight@gmail.com>
2023-04-20 16:53:03 +0700

    configure and create storage instance from @walless/store

--- packages/store/index.ts
+++ packages/store/index.ts
@@ -1,1 +1,24 @@
-export * from './utils/instance';
+import HttpPouch from 'pouchdb-adapter-http';
+import PouchDB from 'pouchdb-core';
+import find from 'pouchdb-find';
+import mapreduce from 'pouchdb-mapreduce';
+import replication from 'pouchdb-replication';
+
+export const create = (
+	name: string,
+	storageEngine: PouchDB.Plugin,
+): PouchDB.Database => {
+	PouchDB.plugin(HttpPouch)
+		.plugin(replication)
+		.plugin(mapreduce)
+		.plugin(find)
+		.plugin(storageEngine);
+
+	return new PouchDB(name);
+};
+
+export const configure = async (db: PouchDB.Database): Promise<void> => {
+	await db.createIndex({
+		index: { fields: ['type', 'network'] },
+	});
+};

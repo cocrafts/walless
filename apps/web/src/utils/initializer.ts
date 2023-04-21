77d68b15ebc757bf675cb13b79e379d05e04c711
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-04-21 18:24:26 +0700

    live state also watch and sync public keys

--- apps/web/src/utils/initializer.ts
+++ apps/web/src/utils/initializer.ts
@@ -1,13 +1,11 @@
 import { injectRuntime } from 'bridge/entry';
-import { initializeStateSync } from 'state/live';
-import { initializeWalletState } from 'state/wallet';
+import { initializeLiveState } from 'state/live';
 import { initializeStorage } from 'utils/pouch';
 
 export const initializeServices = async () => {
 	await Promise.all([
 		injectRuntime(),
 		initializeStorage(),
-		initializeWalletState(),
-		initializeStateSync(),
+		initializeLiveState(),
 	]);
 };

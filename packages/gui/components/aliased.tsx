2b75224c637fd701fc8f3122ecc481beb3472748
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-04-22 11:19:34 +0700

    alias AnimatedView from @walless/gui

--- packages/gui/components/aliased.tsx
+++ packages/gui/components/aliased.tsx
@@ -1,5 +1,6 @@
-import { Image, Pressable } from 'react-native';
+import { Image, Pressable, View } from 'react-native';
 import Animated from 'react-native-reanimated';
 
 export const AnimatedImage = Animated.createAnimatedComponent(Image);
 export const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+export const AnimatedView = Animated.createAnimatedComponent(View);

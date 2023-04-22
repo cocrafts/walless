5d2cc5a5a51500c4c12d88df9454028e77a999c7
Hieu Nguyen <dev.ngtronghieu@gmail.com>
2023-04-22 14:05:29 +0700

    add spacing for body

--- apps/landing/components/layouts/Home/index.tsx
+++ apps/landing/components/layouts/Home/index.tsx
@@ -21,7 +21,7 @@ export const HomeLayout: FC<LayoutProps> = ({
 					showsVerticalScrollIndicator={false}
 					marginTop={navigationHeight}
 				>
-					{children}
+					<Stack $gtMd={{ marginHorizontal: 72 }}>{children}</Stack>
 					<Footer />
 				</ScrollView>
 				<Navigation />

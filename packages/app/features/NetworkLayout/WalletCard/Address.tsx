380fcc52aa0ec76279b266bf767a65e37951f3e2
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-04-25 15:24:32 +0700

    migrate WalletCard address (no more Tamagui)

--- packages/app/features/NetworkLayout/WalletCard/Address.tsx
+++ packages/app/features/NetworkLayout/WalletCard/Address.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { type ViewStyle, Image, StyleSheet } from 'react-native';
 import { shortenAddress } from '@walless/core';
+import { Hoverable, Text, View } from '@walless/gui';
 import { Copy } from '@walless/icons';
 import { TokenRecord } from '@walless/storage';
-import { Image, Stack, Text } from '@walless/ui';
 
 import { CardSkin } from './shared';
 
@@ -15,58 +15,68 @@ interface Props {
 
 export const WalletAddress: FC<Props> = ({ index, skin, token }) => {
 	const { iconSrc, iconColor = '#ffffff', iconSize } = skin;
+	const iconContainerStyle: ViewStyle = {
+		width: iconWrapperSize,
+		height: iconWrapperSize,
+		borderRadius: iconWrapperSize / 2,
+		backgroundColor: iconColor,
+		alignItems: 'center',
+		justifyContent: 'center',
+	};
+	const iconStyle = {
+		width: iconSize,
+		height: iconSize,
+		borderRadius: iconSize / 2,
+	};
 
 	const onCopy = () => {
 		navigator.clipboard.writeText(token.id as string);
 	};
 
 	return (
-		<Stack
-			horizontal
-			alignSelf="flex-start"
-			alignItems="center"
-			padding={5}
-			backgroundColor="rgba(255, 255, 255, 0.2)"
-			borderRadius={30}
-		>
-			<Stack
-				width={iconWrapperSize}
-				height={iconWrapperSize}
-				borderRadius={iconWrapperSize / 2}
-				backgroundColor={iconColor}
-				alignItems="center"
-				justifyContent="center"
-			>
-				<Image
-					width={iconSize}
-					height={iconSize}
-					src={iconSrc}
-					borderRadius={iconSize / 2}
-				/>
-			</Stack>
-			<Text marginHorizontal={5} fontSize={13}>
+		<View style={styles.container}>
+			<View style={iconContainerStyle}>
+				<Image style={iconStyle} source={iconSrc} />
+			</View>
+			<Text style={styles.addressText}>
 				{`Wallet #${index + 1}: ${shortenAddress(token.id as string)}`}
 			</Text>
-			<TouchableOpacity activeOpacity={0.8} onPress={onCopy}>
-				<Stack
-					width={iconWrapperSize}
-					height={iconWrapperSize}
-					alignItems="center"
-					justifyContent="center"
-				>
-					<Stack
-						fullscreen
-						opacity={0.4}
-						backgroundColor="#FFFFFF"
-						borderRadius={iconWrapperSize / 2}
-					/>
+			<Hoverable onPress={onCopy}>
+				<View style={styles.iconWrapper}>
+					<View fullScreen style={styles.iconInner} />
 					<Copy size={iconWrapperSize - 8} />
-				</Stack>
-			</TouchableOpacity>
-		</Stack>
+				</View>
+			</Hoverable>
+		</View>
 	);
 };
 
 export default WalletAddress;
 
 const iconWrapperSize = 20;
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignSelf: 'flex-start',
+		alignItems: 'center',
+		padding: 5,
+		backgroundColor: 'rgba(255, 255, 255, 0.2)',
+		borderRadius: 30,
+	},
+	addressText: {
+		marginHorizontal: 5,
+		fontSize: 13,
+	},
+	iconWrapper: {
+		width: iconWrapperSize,
+		height: iconWrapperSize,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	iconInner: {
+		opacity: 0.4,
+		backgroundColor: '#FFFFFF',
+		borderRadius: iconWrapperSize / 2,
+	},
+});

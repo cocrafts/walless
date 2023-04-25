eb152b08e80653d1d3035cbb90e0f8b1b109e557
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-04-25 12:45:31 +0700

    solana metadata helper, incl legacy json file

---
+++ apps/web/scripts/kernel/utils/solana/metadata.ts
@@ -0,0 +1,41 @@
+import { LegacySolanaMetadata, Networks } from '@walless/core';
+import { getSolanaMetadata, GetSolanaMetadataFunction } from '@walless/network';
+import { MetadataDocument } from '@walless/store';
+import { db } from 'utils/pouch';
+
+import legacyRegistry from './solanaTokenList.json';
+
+export const getLazySolanaMetatadata: GetSolanaMetadataFunction = async (
+	connection,
+	mintAddress,
+) => {
+	const local = getLocalMetadata(mintAddress);
+	if (local) return local;
+
+	const cached = await db.safeGet<MetadataDocument>(mintAddress);
+	const timestamp = new Date(cached?.timestamp || '2000-01-01');
+	const cachedTime = new Date().getTime() - timestamp.getTime();
+	if (cached && cachedTime < 1) return cached;
+
+	const remote = await getSolanaMetadata(connection, mintAddress);
+	await db.upsert(mintAddress, async () => remote);
+	return remote;
+};
+
+export const tokenMap = (
+	(legacyRegistry as any).tokens as LegacySolanaMetadata[]
+).reduce((a, i) => {
+	a[i.address] = {
+		_id: i.address,
+		type: 'Metadata',
+		network: Networks.solana,
+		name: i.name,
+		symbol: i.symbol,
+		imageUri: i.logoURI,
+	} as MetadataDocument;
+	return a;
+}, {} as Record<string, MetadataDocument>);
+
+export const getLocalMetadata = (address: string) => {
+	return tokenMap[address];
+};

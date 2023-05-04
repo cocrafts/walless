d02c4a2df8bb7669b4cc0626891217c7cf7493e0
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-05-04 12:22:42 +0700

    NotificationModal export

--- packages/app/modals/NotificationModal/index.tsx
+++ packages/app/modals/NotificationModal/index.tsx
@@ -29,7 +29,7 @@ export const NotificationModal: FC<Props> = ({ config }) => {
 	);
 };
 
-export default Notification;
+export default NotificationModal;
 
 const styles = StyleSheet.create({
 	container: {

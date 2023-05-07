351b44358399ff9f6718b19d771ccc0e38fe6fdc
Tan Le <tanlethanh8266@gmail.com>
2023-05-07 22:18:39 +0700

    Basic transaction result

---
+++ packages/app/features/Send/TransactionResult/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from '@walless/gui';
+
+const TransactionResult = () => {
+	return (
+		<View>
+			<Text>TransactionResult</Text>
+		</View>
+	);
+};
+
+export default TransactionResult;
+
+const styles = StyleSheet.create({});

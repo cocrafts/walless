08bb8be0a527a14153c8edf89e1825bcf7238db0
Tan Le <tanlethanh8266@gmail.com>
2023-05-07 22:14:57 +0700

    Add send token modal

---
+++ apps/web/src/screens/Profile/modals/SendModal/index.tsx
@@ -0,0 +1,36 @@
+import { type FC } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { SendTokenScreen } from '@walless/app';
+import { modalActions, ModalConfigs } from '@walless/gui';
+import { useTokens } from 'utils/hooks';
+import { getTransactionFee } from 'utils/transaction';
+
+export const SendModal: FC<{ config: ModalConfigs }> = ({ config }) => {
+	console.log(config.context);
+
+	const tokenList = useTokens();
+
+	return (
+		<View style={styles.container}>
+			<SendTokenScreen
+				tokenList={tokenList}
+				getTransactionFee={getTransactionFee}
+				onClose={() => modalActions.hide(config.id as string)}
+				onPressSendButton={() => console.log('Handle send token')}
+			/>
+		</View>
+	);
+};
+
+export default SendModal;
+
+const styles = StyleSheet.create({
+	container: {
+		display: 'flex',
+		backgroundColor: '#141B21',
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});

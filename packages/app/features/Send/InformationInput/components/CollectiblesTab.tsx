9972389f20a9165eb8649c39b0517c56a01d2f9e
Tan Le <tanlethanh8266@gmail.com>
2023-05-07 22:17:37 +0700

    Basic collectibles tab

---
+++ packages/app/features/Send/InformationInput/components/CollectiblesTab.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { Stack, Text } from '@walless/ui';
+
+export const CollectiblesTab: FC = () => {
+	return (
+		<Stack>
+			<Text>In development</Text>
+		</Stack>
+	);
+};

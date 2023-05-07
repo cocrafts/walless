af652034cd6f0f5728ce0a864de5dd5031bf28c9
Tan Le <tanlethanh8266@gmail.com>
2023-05-07 22:18:57 +0700

    Basic transaction confirm

---
+++ packages/app/features/Send/TransactionConfirmation/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from '@walless/gui';
+
+const TransactionConfirmation = () => {
+	return (
+		<View>
+			<Text>ConfirmScreen</Text>
+		</View>
+	);
+};
+
+export default TransactionConfirmation;
+
+const styles = StyleSheet.create({});

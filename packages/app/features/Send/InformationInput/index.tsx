ffb922f71328e63ce4aa71e0d6e68fa4efb9ce73
Tan Le <tanlethanh8266@gmail.com>
2023-05-07 22:17:23 +0700

    Implement first step of send features

---
+++ packages/app/features/Send/InformationInput/index.tsx
@@ -0,0 +1,40 @@
+import { type FC, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { type SliderHandle, View } from '@walless/gui';
+
+import { CollectiblesTab, TabBar, TokensTab } from './components';
+
+interface Props {
+	navigator: SliderHandle;
+}
+
+const InformationInput: FC<Props> = ({ navigator }) => {
+	const [isTokensTab, setIsTokensTab] = useState(true);
+
+	const handlePressContinue = () => {
+		navigator.slideNext();
+	};
+
+	return (
+		<View style={styles.container}>
+			<View></View>
+
+			<TabBar isTokensTab={isTokensTab} setIsTokensTab={setIsTokensTab} />
+
+			{isTokensTab ? (
+				<TokensTab onContinue={handlePressContinue} />
+			) : (
+				<CollectiblesTab />
+			)}
+		</View>
+	);
+};
+
+export default InformationInput;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		width: 336,
+	},
+});

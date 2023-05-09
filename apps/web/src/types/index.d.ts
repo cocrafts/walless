ee8ffdc5b58437a6c8c7ddbdd5a268c5f2eab6ee
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-05-10 05:44:30 +0700

    load web3auth clientId externally

--- apps/web/src/types/index.d.ts
+++ apps/web/src/types/index.d.ts
@@ -2,7 +2,8 @@
 
 declare global {
 	const BUILD_TARGET: string;
-	const GOOGLE_CLIENT_ID: string;
+	const BROWSER_CLIENT_ID: string;
+	const EXTENSION_CLIENT_ID: string;
 	const FIREBASE_API_KEY: string;
 	const FIREBASE_AUTH_DOMAIN: string;
 	const FIREBASE_PROJECT_ID: string;
@@ -10,6 +11,7 @@ declare global {
 	const FIREBASE_MESSAGING_SENDER_ID: string;
 	const FIREBASE_APP_ID: string;
 	const FIREBASE_MEASUREMENT_ID: string;
+	const WEB3AUTH_ID: string;
 }
 
 export {};

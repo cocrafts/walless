74e011501f0188ab67916de8445fb6330e04649b
FME849 <1414439@gmail.com>
2023-05-11 15:56:20 +0700

    add CollectibleList to use in design tool detail

---
+++ apps/landing/features/home/EditTool/PreviewScreen/Detail/CollectibleList/Item.tsx
@@ -0,0 +1,43 @@
+import { type FC } from 'react';
+import { type MetadataDocument } from '@walless/store';
+import { Image, Stack, Text } from '@walless/ui';
+
+interface Props {
+	item: MetadataDocument;
+}
+
+const CollectibleItem: FC<Props> = ({ item }) => {
+	const imgSize = 130;
+	const { symbol, imageUri } = item;
+	const iconSource = {
+		uri: imageUri || '/img/question.png',
+	};
+	return (
+		<Stack
+			padding={12}
+			backgroundColor={'#131C24'}
+			alignItems="center"
+			gap={10}
+			borderRadius={10}
+		>
+			<Image
+				src={iconSource}
+				width={imgSize}
+				height={imgSize}
+				backgroundColor={'#0B1218'}
+				borderRadius={10}
+			/>
+
+			<Stack alignItems="center">
+				<Text fontSize={16} fontWeight={'500'}>
+					{symbol}
+				</Text>
+				<Text color={'#566674'} fontSize={13} fontWeight={'400'}>
+					40
+				</Text>
+			</Stack>
+		</Stack>
+	);
+};
+
+export default CollectibleItem;
---
+++ apps/landing/features/home/EditTool/PreviewScreen/Detail/CollectibleList/index.tsx
@@ -0,0 +1,33 @@
+import { type FC } from 'react';
+import { type MetadataDocument } from '@walless/store';
+import { ScrollView, Stack } from '@walless/ui';
+
+import CollectibleItem from './Item';
+
+interface Props {
+	items: MetadataDocument[];
+}
+
+export const CollectibeList: FC<Props> = ({ items }) => {
+	const leftColumnItems = items.filter((_, index) => index % 2 === 0);
+	const rightColumnItems = items.filter((_, index) => index % 2 !== 0);
+
+	return (
+		<ScrollView marginVertical={12} contentContainerStyle={{ gap: 15 }}>
+			<Stack horizontal gap={15}>
+				<Stack flex={1} gap={15} flexBasis={0}>
+					{leftColumnItems.map((item) => (
+						<CollectibleItem key={item._id} item={item} />
+					))}
+				</Stack>
+				<Stack flex={1} gap={15}>
+					{rightColumnItems.map((item) => (
+						<CollectibleItem key={item._id} item={item} />
+					))}
+				</Stack>
+			</Stack>
+		</ScrollView>
+	);
+};
+
+export default CollectibeList;

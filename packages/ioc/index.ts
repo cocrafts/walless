ef9154d8dc3b9851aa52313ca4ea0d2e1ec58b02
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-05-13 02:28:58 +0700

    make ioc (aka dynamic-modules) a separate package, not-part-of packages/app

--- packages/ioc/index.ts
+++ packages/ioc/index.ts
@@ -2,10 +2,10 @@ import { type Engine } from '@walless/engine';
 import { type EncryptionKeyVault } from '@walless/messaging';
 import { type Database } from '@walless/store';
 
-export interface Modules {
+export interface DynamicModules {
 	engine: Engine;
 	storage: Database;
 	encryptionKeyVault: EncryptionKeyVault;
 }
 
-export const modules: Modules = {} as never;
+export const modules: DynamicModules = {} as never;

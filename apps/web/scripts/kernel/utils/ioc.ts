6fdd74af69bb379a75295e82b5334d8adfb1d15b
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-05-13 02:31:28 +0700

    kernel version of the ioc

---
+++ apps/web/scripts/kernel/utils/ioc.ts
@@ -0,0 +1,15 @@
+import { modules } from '@walless/ioc';
+import { createEncryptionKeyVault } from '@walless/messaging';
+import { configure, create } from '@walless/store';
+import IDBPouch from 'pouchdb-adapter-idb';
+
+export const injectModules = async () => {
+	modules.storage = create('engine', IDBPouch);
+	modules.encryptionKeyVault = createEncryptionKeyVault(modules.storage);
+
+	await Promise.all([configure(modules.storage)]);
+
+	return modules;
+};
+
+export default modules;

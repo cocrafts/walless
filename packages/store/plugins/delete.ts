558726e042b85bce695a2ce08278d32b09c819b4
ltminhthu <ltminhthu.209@gmail.com>
2023-05-16 17:31:52 +0700

    refactor delete function in db

--- packages/store/plugins/delete.ts
+++ packages/store/plugins/delete.ts
@@ -1,8 +1,8 @@
 export async function docRemove<T extends object>(
-  this: PouchDB.Database,
-  id: string,
+	this: PouchDB.Database,
+	id: string,
 ) {
-  const doc = await this.get(id);
-  doc._deleted = true;
-  this.put(doc);
+	const doc: T = await this.get(id);
+	doc._deleted = true;
+	this.put(doc);
 }

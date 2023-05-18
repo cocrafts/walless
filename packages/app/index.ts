231106c329d34395aaf7e783c0ae18742fd0430c
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-05-18 20:21:41 +0700

    sign in with password

--- packages/app/index.ts
+++ packages/app/index.ts
@@ -8,6 +8,7 @@ export * from './features/Login';
 export * from './features/MainFeatures';
 export * from './features/NetworkLayout';
 export * from './features/Passcode';
+export * from './features/Password';
 export * from './features/Send';
 export * from './features/Splash';
 export * from './modals/NotificationModal';

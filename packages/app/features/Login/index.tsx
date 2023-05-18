6211a17e52a0f5fe440d9e9676398ce3bbb27146
ltminhthu <ltminhthu.209@gmail.com>
2023-05-18 23:00:34 +0700

    add Import seed phrase button

--- packages/app/features/Login/index.tsx
+++ packages/app/features/Login/index.tsx
@@ -8,6 +8,7 @@ import {
 	StyleSheet,
 } from 'react-native';
 import { Anchor, Button, Text, View } from '@walless/gui';
+import { Google } from '@walless/icons';
 
 interface Props {
 	style?: ViewStyle;
@@ -31,16 +32,28 @@ export const LoginFeature: FC<Props> = ({
 
 	return (
 		<View style={[styles.container, style]}>
-			<View style={styles.innerContainer}>
+			<View style={styles.headerContainer}>
 				<Image style={logoStyle} source={logoSrc} resizeMode="cover" />
-				<Text>Sign in to continue</Text>
+				<Text style={styles.title}>Sign in to continue</Text>
+			</View>
+			<View style={styles.innerContainer}>
 				<View style={styles.commandContainer}>
 					{loading ? (
 						<ActivityIndicator color="white" />
 					) : (
-						<Button title="Sign in with Google" onPress={onGoogleSignIn} />
+						<Button style={styles.signInButton} onPress={onGoogleSignIn}>
+							<Google />{' '}
+							<Text style={styles.buttonText}>Sign in with Google</Text>
+						</Button>
 					)}
 				</View>
+				<Text style={styles.subText}>Advanced mode</Text>
+				<Button
+					style={styles.seedPhraseButton}
+					disabled
+					title="Create or Import"
+					titleStyle={styles.buttonText}
+				/>
 			</View>
 			<View style={styles.footerContainer}>
 				<Text style={styles.footerText}>
@@ -58,16 +71,51 @@ export default LoginFeature;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		justifyContent: 'space-between',
 	},
-	innerContainer: {
-		flex: 1,
+	headerContainer: {
 		alignItems: 'center',
 		justifyContent: 'center',
+		paddingTop: 48,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: '400',
+	},
+	innerContainer: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		gap: 18,
+		paddingBottom: 48,
 	},
 	commandContainer: {
-		minHeight: 50,
-		marginTop: 15,
 		justifyContent: 'center',
+	},
+	signInButton: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#0694D3',
+		gap: 8,
+		width: 342,
+		height: 48,
+		borderRadius: 16,
+	},
+	seedPhraseButton: {
+		backgroundColor: '#243F56',
+		width: 342,
+		height: 48,
+		borderRadius: 16,
+	},
+	buttonText: {
+		fontSize: 16,
+		fontWeight: '500',
+		color: 'white',
+	},
+	subText: {
+		fontSize: 14,
+		color: '#566674',
+		fontWeight: '400',
 	},
 	footerContainer: {
 		alignItems: 'center',

df57997f9b5ccbde1d70fe0780eb934564d4684a
ltminhthu <ltminhthu.209@gmail.com>
2023-05-24 17:00:30 +0700

    add some functions, states to login and invitation

--- apps/web/src/screens/Invitation/index.tsx
+++ apps/web/src/screens/Invitation/index.tsx
@@ -1,43 +1,26 @@
-import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { InvitationFeature } from '@walless/app';
 import { View } from '@walless/gui';
 import { appState } from 'state/app';
+import { enterInvitationCode } from 'state/app/authentication';
 import { router } from 'utils/routing';
 import { useSnapshot } from 'valtio';
 
 const InvitationScreen = () => {
-	const [invitationCode, setInvitationCode] = useState('');
-	const [isValid, setIsValid] = useState(false);
 	const { invitationError } = useSnapshot(appState);
-
-	const validateInvitationCode = (value: string) => {
-		console.log('validating...', value);
-		return false;
-	};
+	appState.isAbleToSignIn = true;
 
 	const onInvitationCodeChange = async (value: string) => {
-		setInvitationCode(value);
-
-		setIsValid(validateInvitationCode(value));
-
-		// if (invitationError && value.length > 0) {
-		// 	appState.invitationError = '';
-		// }
-
-		if (isValid) {
-			handleConnect();
-		} else {
-			appState.invitationError = value;
+		if (invitationError && value.length > 0) {
+			appState.invitationError = '';
 		}
+
+		enterInvitationCode(value);
 	};
 
-	const handleNavigateToLogIn = () => {
+	const handleAlreadyHaveAccount = () => {
+		appState.invitationError = undefined;
 		router.navigate('/login');
-	};
-
-	const handleConnect = () => {
-		console.log(invitationCode);
 	};
 
 	return (
@@ -46,7 +29,7 @@ const InvitationScreen = () => {
 				onEnter={onInvitationCodeChange}
 				logoSrc={{ uri: '/img/icon-lg.png' }}
 				error={invitationError}
-				onNavigateToLogIn={handleNavigateToLogIn}
+				onNavigateToLogIn={handleAlreadyHaveAccount}
 			/>
 		</View>
 	);
--- apps/web/src/screens/Login.tsx
+++ apps/web/src/screens/Login.tsx
@@ -3,15 +3,24 @@ import { LoginFeature } from '@walless/app';
 import { appActions, appState } from 'state/app';
 import { resources } from 'utils/config';
 import { useSnapshot } from 'utils/hooks';
+import { router } from 'utils/routing';
 
 export const LoginScreen: FC = () => {
 	const { authenticationLoading } = useSnapshot(appState);
+	const { invitationCode } = useSnapshot(appState);
+	const { isAbleToSignIn } = useSnapshot(appState);
+	const handleNavigateToGetInvitationCode = () => {
+		router.navigate('/invitation');
+		appState.isAbleToSignIn = true;
+	};
 
 	return (
 		<LoginFeature
 			loading={authenticationLoading}
 			logoSrc={resources.walless.icon}
-			onGoogleSignIn={appActions.signInWithGoogle}
+			onGoogleSignIn={() => appActions.signInWithGoogle(invitationCode)}
+			isAbleToSignIn={isAbleToSignIn}
+			onGetInvitationCode={handleNavigateToGetInvitationCode}
 		/>
 	);
 };

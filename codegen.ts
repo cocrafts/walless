429dd8f4b4e27764dd6a85a68c4435422ea6bb29
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-05-26 15:38:35 +0700

    graphql-codegen script and schema-gen upgrade

---
+++ codegen.ts
@@ -0,0 +1,13 @@
+import { type CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+	overwrite: true,
+	schema: 'https://api-dev.walless.io/light',
+	generates: {
+		'./packages/graphql/types.ts': {
+			plugins: ['typescript', 'typescript-resolvers'],
+		},
+	},
+};
+
+export default config;

be02edc6b5419b5adccf3d280debc2db0e895c20
Tan Le <tanlethanh8266@gmail.com>
2023-05-26 20:19:55 +0700

    Update index page

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,15 +1,41 @@
 import { StyleSheet } from 'react-native';
-import { Text, View } from '@walless/gui';
+import Animated, {
+	useAnimatedStyle,
+	useSharedValue,
+	withSpring,
+} from 'react-native-reanimated';
+import { Button, Text, View } from '@walless/gui';
 
-const index = () => {
+const AnimatedBox = Animated.createAnimatedComponent(View);
+
+const IndexPage = () => {
+	const size = useSharedValue(1);
+
+	const boxStyle = useAnimatedStyle(() => {
+		return {
+			transform: [
+				{
+					scale: size.value,
+				},
+			],
+		};
+	}, [size]);
+
+	const handlePress = () => {
+		if (size.value < 1) size.value = withSpring(size.value * 1.5);
+		else size.value = withSpring(size.value * 0.5);
+	};
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.h1}>Hello tan</Text>
+			<AnimatedBox style={[styles.animatedBox, boxStyle]} />
+			<Button onPress={() => handlePress()} title={'Click me!'} />
 		</View>
 	);
 };
 
-export default index;
+export default IndexPage;
 
 const styles = StyleSheet.create({
 	container: {
@@ -19,7 +45,13 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
+	animatedBox: {
+		width: 100,
+		height: 100,
+		backgroundColor: 'blue',
+	},
 	h1: {
 		fontSize: 20,
+		color: 'black',
 	},
 });

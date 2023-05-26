7d998cca6498e64216c7fb36ed9651d4fad944d8
Tan Le <tanlethanh8266@gmail.com>
2023-05-26 20:19:47 +0700

    Add rerender in the first time render to make animated work

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,10 +1,20 @@
-import { type FC } from 'react';
+import { type FC, useEffect, useState } from 'react';
 import { type AppProps } from 'next/app';
 import Head from 'next/head';
 
 import 'raf/polyfill';
 
 export const App: FC<AppProps> = ({ Component, pageProps }) => {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [_, setRender] = useState({});
+
+	/**
+	 * This effect makes reanimated work
+	 * */
+	useEffect(function updateState() {
+		setRender({});
+	}, []);
+
 	return (
 		<>
 			<Head>

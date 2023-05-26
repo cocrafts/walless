8ddd6f3ec3926449a413d30c244dbf8f744f5e85
Tan Le <tanlethanh8266@gmail.com>
2023-05-26 20:18:41 +0700

    Revert config using `transpliePackage` instead of using externalize

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,11 +1,17 @@
 const { DefinePlugin } = require('webpack');
-const path = require('path');
 
 module.exports = {
 	swcMinify: true,
 	reactStrictMode: true,
-	transpilePackages: ['@walless/gui', '@walless/markdown'],
-	webpack: (config, { dev, isServer }) => {
+	transpilePackages: [
+		'@walless/gui',
+		'@walless/markdown',
+		'react',
+		'react-native',
+		'react-native-reanimated',
+		'react-native-svg',
+	],
+	webpack: (config, { dev }) => {
 		config.module.rules.push({
 			test: /\.md$/i,
 			type: 'asset/source',
@@ -30,41 +36,6 @@ module.exports = {
 			}),
 		);
 
-		/**
-		 * Server react-native compat
-		 */
-		if (isServer) {
-			// externalize react native things from bundle
-			config.externals = [
-				...config.externals.map((external) => {
-					if (typeof external !== 'function') return external;
-
-					// only runs on server
-					return async (ctx, cb) => {
-						const mustExternalize = externalize(ctx.context, ctx.request);
-
-						if (mustExternalize) return external(ctx, cb);
-						else return undefined;
-					};
-				}),
-			];
-		}
-
 		return config;
 	},
-};
-
-const externalize = (context, request) => {
-	const SEP = path.sep;
-	const fullPath = request[0] === '.' ? path.join(context, request) : request;
-
-	if (fullPath.includes('react-native-web')) return false;
-
-	// must inline react-native so we can alias to react-native-web
-	if (fullPath === 'react-native' || fullPath.startsWith(`react-native${SEP}`))
-		return false;
-
-	if (/^@?react-native-/.test(request)) return false;
-
-	return true;
 };

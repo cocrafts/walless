fa0705d4c3829643fd746083f126749f4cd39675
Tan Le <tanlethanh8266@gmail.com>
2023-05-30 15:57:57 +0700

    Separate code to use loadContent in CSR

---
+++ apps/docs/src/utils/content/index.ts
@@ -0,0 +1,18 @@
+import { type DocsTree } from '../types';
+
+export const loadContent = (
+	docsTree: DocsTree,
+	path: string,
+	nodes?: string[],
+): string | undefined => {
+	if (typeof nodes == 'undefined')
+		nodes = path.split('/').filter((node) => node.length > 0);
+
+	if (nodes.length == 0) return docsTree.content;
+	else if (!docsTree.children) return;
+
+	for (const child of docsTree.children) {
+		if (child.name == nodes[0])
+			return loadContent(child, path, nodes.splice(1));
+	}
+};
--- apps/docs/src/utils/engine.ts
+++ apps/docs/src/utils/engine.ts
@@ -32,20 +32,3 @@ export const loadMarkdown = async (markdownPath?: string) => {
 
 	return node;
 };
-
-export const loadContent = (
-	docsTree: DocsTree,
-	path: string,
-	nodes?: string[],
-): string | undefined => {
-	if (typeof nodes == 'undefined')
-		nodes = path.split('/').filter((node) => node.length > 0);
-
-	if (nodes.length == 0) return docsTree.content;
-	else if (!docsTree.children) return;
-
-	for (const child of docsTree.children) {
-		if (child.name == nodes[0])
-			return loadContent(child, path, nodes.splice(1));
-	}
-};

29fe1c806befbb8ce426b0ac5aa73bb17c63ce06
Tan Le <tanlethanh8266@gmail.com>
2023-05-31 16:31:21 +0700

    Refactor engine to support indexing

--- apps/docs/src/utils/engine.ts
+++ apps/docs/src/utils/engine.ts
@@ -4,31 +4,46 @@ import path from 'path';
 import { simplifyString } from './helpers';
 import { type DocsTree } from './types';
 
-export const loadMarkdown = async (markdownPath?: string) => {
+export const loadMarkdown = async (
+	markdownPath?: string,
+	simplifiedParentPath?: string,
+) => {
 	if (typeof markdownPath == 'undefined')
 		markdownPath = path.join(process.cwd(), 'markdown');
-
-	console.log(markdownPath);
 
 	const pathNodes = markdownPath
 		.split(path.sep + 'markdown')[1]
 		.split(path.sep);
 
-	const relativePath = pathNodes.join('/')?.replace('.md', '') || '/';
+	const fileName = pathNodes[pathNodes.length - 1]?.replace('.md', '');
+
+	const isRoot = !fileName;
 
 	const node: DocsTree = {
-		name: pathNodes[pathNodes.length - 1]?.replace('.md', '') || 'root',
-		path: simplifyString(relativePath),
+		name: isRoot ? 'root' : fileName,
+		path: isRoot ? '/' : `${simplifiedParentPath}/${simplifyString(fileName)}`,
 	};
+
+	const lastDotIdx = fileName.lastIndexOf('.');
+	if (lastDotIdx !== -1) {
+		node.prefix = fileName.slice(0, lastDotIdx);
+		node.name = fileName.slice(lastDotIdx + 1);
+		node.path = `${simplifiedParentPath}/${simplifyString(node.name)}`;
+	}
 
 	const stats = fs.statSync(markdownPath);
 	if (stats.isDirectory()) {
 		const childrenNames = fs.readdirSync(markdownPath);
 		node.children = await Promise.all(
 			childrenNames.map((name) =>
-				loadMarkdown(path.join(markdownPath as string, name)),
+				loadMarkdown(
+					path.join(markdownPath as string, name),
+					isRoot ? '' : node.path,
+				),
 			),
 		);
+
+		node.children.sort(compareDocsTree);
 	} else {
 		node.content = fs.readFileSync(markdownPath, 'utf-8');
 	}

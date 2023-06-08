b43db4d4134c517571eba593c761a4b7784110d5
FME849 <1414439@gmail.com>
2023-06-08 12:25:16 +0700

    use dynamic url to open popup

--- apps/web/scripts/kernel/handlers/shared.ts
+++ apps/web/scripts/kernel/handlers/shared.ts
@@ -1,14 +1,18 @@
 import { type MiniBroadcast } from '@walless/core';
+import { type PopupType } from '@walless/messaging';
 
 export const requestSourceMap: Record<string, MiniBroadcast> = {};
 export const popupIdentityMap: Record<string, chrome.windows.Window> = {};
 
-export const handleOpenPopup = async (requestId: string) => {
+export const handleOpenPopup = async (
+	popupType: PopupType,
+	requestId: string,
+) => {
 	const popupIdentity = await chrome.windows.create({
 		type: 'popup',
 		width: 410,
 		height: 600,
-		url: `popup.html/#/request-connection/${requestId}`,
+		url: `popup.html/#/${popupType}/${requestId}`,
 		focused: true,
 	});
 

63305548c7ce7607b8bb5f9c7487811aedddc325
ltminhthu <ltminhthu.209@gmail.com>
2023-06-09 16:32:11 +0700

    add log out feature

---
+++ apps/web/src/screens/Setting/components/LogOut/ConfirmModal.tsx
@@ -0,0 +1,79 @@
+import { StyleSheet } from 'react-native';
+import { Button, modalActions, Text, View } from '@walless/gui';
+import modules from 'utils/ioc';
+import { router } from 'utils/routing';
+
+const ConfirmModal = () => {
+	const handleLogOut = async () => {
+		await modalActions.destroy('log-out-modal');
+		await modules.storage.clear();
+		await router.navigate('/login');
+	};
+
+	const handleCancelLogOut = () => {
+		modalActions.hide('log-out-modal');
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.titleContainer}>
+				<Text style={styles.title}>Log out</Text>
+				<Text style={styles.subText}>
+					Are you sure that you want to log out?
+				</Text>
+			</View>
+			<View style={styles.buttonContainer}>
+				<Button style={styles.cancelButton} onPress={handleCancelLogOut}>
+					Cancel
+				</Button>
+				<Button style={styles.logOutButton} onPress={handleLogOut}>
+					Log out
+				</Button>
+			</View>
+		</View>
+	);
+};
+
+export default ConfirmModal;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginHorizontal: 20,
+		marginVertical: 230,
+		borderRadius: 16,
+	},
+	titleContainer: {
+		flexGrow: 1,
+		paddingHorizontal: 20,
+		paddingVertical: 28,
+		backgroundColor: '#1F2A34',
+		borderTopRightRadius: 16,
+		borderTopLeftRadius: 16,
+		gap: 6,
+	},
+	title: {
+		fontSize: 20,
+		color: '#ffffff',
+	},
+	subText: {
+		color: '#A4B3C1',
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+		paddingHorizontal: 16,
+		paddingVertical: 12,
+		backgroundColor: '#19232C',
+		borderBottomRightRadius: 16,
+		borderBottomLeftRadius: 16,
+		gap: 8,
+	},
+	logOutButton: {
+		paddingHorizontal: 20,
+		backgroundColor: '#A54545',
+	},
+	cancelButton: {
+		backgroundColor: 'transparent ',
+	},
+});
---
+++ apps/web/src/screens/Setting/components/LogOut/index.tsx
@@ -0,0 +1,58 @@
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { modalActions, Text, View } from '@walless/gui';
+import { LogOut as LogOutIcon } from '@walless/icons';
+
+import ConfirmModal from './ConfirmModal';
+
+const LogOut = () => {
+	const handleShowLogOutModal = () => {
+		modalActions.show({
+			id: 'log-out-modal',
+			component: ConfirmModal,
+			withoutMask: false,
+		});
+	};
+
+	return (
+		<TouchableOpacity style={styles.container} onPress={handleShowLogOutModal}>
+			<View style={styles.titleContainer}>
+				<View style={styles.iconContainer}>
+					<LogOutIcon color="#A45151" size={16} />
+				</View>
+				<Text style={styles.title}>Log out</Text>
+			</View>
+		</TouchableOpacity>
+	);
+};
+
+export default LogOut;
+
+const styles = StyleSheet.create({
+	container: {
+		flexGrow: 1,
+		flexDirection: 'row',
+		backgroundColor: 'transparent',
+		padding: 12,
+		hoverStyle: {
+			backgroundColor: '#1F2A34',
+		},
+	},
+	iconContainer: {
+		width: 30,
+		height: 30,
+		borderRadius: 8,
+		backgroundColor: '#422626',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	titleContainer: {
+		alignItems: 'center',
+		flexDirection: 'row',
+		flex: 1,
+		gap: 12,
+		margin: 0,
+	},
+	title: {
+		color: '#A45151',
+	},
+});

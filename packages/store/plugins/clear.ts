65e0786b137cd123a7ad86ff1ca32aefc75e1790
ltminhthu <ltminhthu.209@gmail.com>
2023-06-11 14:52:34 +0700

    remove .then in clear database function

--- packages/store/plugins/clear.ts
+++ packages/store/plugins/clear.ts
@@ -1,9 +1,6 @@
 export async function clear(this: PouchDB.Database) {
-	await this.allDocs().then((result) => {
-		return Promise.all(
-			result.rows.map((row) => {
-				return this.remove(row.id, row.value.rev);
-			}),
-		);
+	const { rows } = await this.allDocs();
+	await rows.map((row) => {
+		return this.remove(row.id, row.value.rev);
 	});
 }

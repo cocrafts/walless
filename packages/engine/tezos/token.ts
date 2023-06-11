71f55119c90cad206dfa1c9360e421e86920b194
Tan Le <tanlethanh8266@gmail.com>
2023-06-12 05:27:01 +0700

    Add token utils of tezos

---
+++ packages/engine/tezos/token.ts
@@ -0,0 +1,174 @@
+import { type TezosToolkit } from '@taquito/taquito';
+import { type Endpoint, Networks } from '@walless/core';
+import { type TokenDocument } from '@walless/store';
+
+import { type GetTezosMetadataFunction, getTezosMetadata } from './metadata';
+import { tezosEndpoints } from './pool';
+
+interface TokenByAddressOption {
+	endpoint: Endpoint;
+	connection: TezosToolkit;
+	address: string;
+	metadataFetcher?: GetTezosMetadataFunction;
+}
+
+export const tezosTokensByAddress = async ({
+	endpoint,
+	connection,
+	address,
+	metadataFetcher = getTezosMetadata,
+}: TokenByAddressOption): Promise<TokenDocument[]> => {
+	if (endpoint == tezosEndpoints.tezosMainnet) {
+		const tokens: TokenDocument[] = [tezosNativeToken];
+		tokens.push(
+			...KNOWN_TEZOS_MAINNET_TOKENS.map((token) => {
+				return {
+					...token,
+					_id: `${token.account.address}/${token.account.tokenId}`,
+					type: 'Token',
+					network: Networks.tezos,
+				} as TokenDocument;
+			}),
+		);
+
+		return tokens;
+	} else if (endpoint == tezosEndpoints.ghostnetTestnet) {
+		// return [tezosNativeToken];
+
+		const tokens: TokenDocument[] = [tezosNativeToken];
+		tokens.push(
+			...KNOWN_TEZOS_MAINNET_TOKENS.map((token) => {
+				return {
+					...token,
+					_id: `${token.account.address}/${token.account.tokenId}`,
+					type: 'Token',
+					network: Networks.tezos,
+				} as TokenDocument;
+			}),
+		);
+
+		return tokens;
+	} else {
+		// These params used to fetch unknown tokens
+		console.log({ connection, address, metadataFetcher });
+		throw Error('This network is not supported');
+	}
+};
+
+const tezosNativeToken: TokenDocument = {
+	_id: 'tezos-native-token',
+	network: Networks.tezos,
+	type: 'Token',
+	metadata: {
+		name: 'Tezos',
+		symbol: 'TEZ',
+		imageUri: '/img/network/tezos-icon-sm.png',
+	},
+	account: {
+		balance: '0',
+		decimals: 6,
+	},
+};
+
+const KNOWN_TEZOS_MAINNET_TOKENS: Omit<
+	TokenDocument,
+	'_id' | 'type' | 'network'
+>[] = [
+	{
+		metadata: {
+			name: 'Tether USD',
+			symbol: 'USDt',
+			imageUri:
+				'https://ipfs.io/ipfs/QmRymVGWEudMfLrbjaEiXxngCRTDgWCsscjQMwizy4ZJjX',
+		},
+		account: {
+			tokenId: 0,
+			address: 'KT1XnTn74bUtxHfDtBmm2bGZAQfhPbvKWR8o',
+			balance: '0',
+			decimals: 6,
+		},
+	},
+	{
+		metadata: {
+			name: 'youves uUSD',
+			symbol: 'uUSD',
+			imageUri:
+				'https://ipfs.io/ipfs/QmbvhanNCxydZEbGu1RdqkG3LcpNGv7XYsCHgzWBXnmxRd',
+		},
+		account: {
+			tokenId: 0,
+			address: 'KT1XRPEPXbZK25r3Htzp2o1x7xdMMmfocKNW',
+			balance: '0',
+			decimals: 12,
+		},
+	},
+	{
+		metadata: {
+			name: 'Kolibri',
+			symbol: 'kUSD',
+			imageUri: 'https://kolibri-data.s3.amazonaws.com/logo.png',
+		},
+		account: {
+			tokenId: 0,
+			address: 'KT1K9gCRgaLRFKTErYt1wVxA3Frb9FjasjTV',
+			balance: '0',
+			decimals: 18,
+		},
+	},
+	{
+		metadata: {
+			name: 'Tezos BTC',
+			symbol: 'tzBTC',
+			imageUri:
+				'https://tzbtc.io/wp-content/uploads/2020/03/tzbtc_logo_single.svg',
+		},
+		account: {
+			tokenId: 0,
+			address: 'KT1PWx2mnDueood7fEmfbBDKx1D9BAnnXitn',
+			balance: '0',
+			decimals: 8,
+		},
+	},
+	{
+		metadata: {
+			name: 'youves uBTC',
+			symbol: 'uBTC',
+			imageUri:
+				'https://ipfs.io/ipfs/Qmbev41h4axBqVzxsXP2NSaAF996bJjJBPb8FFZVqTvJTY',
+		},
+		account: {
+			tokenId: 2,
+			address: 'KT1XRPEPXbZK25r3Htzp2o1x7xdMMmfocKNW',
+			balance: '0',
+			decimals: 12,
+		},
+	},
+	{
+		metadata: {
+			name: 'Quipuswap governance token',
+			symbol: 'QUIPU',
+			imageUri:
+				'https://ipfs.io/ipfs/Qmb2GiHN9EjcrN29J6y9PsXu3ZDosXTv6uLUWGZfRRSzS2/quipu.png',
+		},
+		account: {
+			tokenId: 0,
+			address: 'KT193D4vozYnhGJQVtw7CoxxqphqUEEwK6Vb',
+			balance: '0',
+			decimals: 6,
+		},
+	},
+	{
+		metadata: {
+			name: 'youves YOU Governance',
+			symbol: 'YOU',
+			imageUri:
+				'https://ipfs.io/ipfs/QmYAJaJvEJuwvMEgRbBoAUKrTxRTT22nCC9RuY7Jy4L4Gc',
+		},
+		account: {
+			tokenId: 0,
+			address: 'KT1Xobej4mc6XgEjDoJoHtTKgbD1ELMvcQuL',
+			balance: '0',
+			decimals: 12,
+		},
+	},
+];

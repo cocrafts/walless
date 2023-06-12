34753e92ace1838aef6e183a41d7b44dc15cf6d0
Tan Le <tanlethanh8266@gmail.com>
2023-06-12 12:12:13 +0700

    Basic setup for dapp store app

---
+++ apps/dappstore/next.config.js
@@ -0,0 +1,35 @@
+const { DefinePlugin } = require('webpack');
+
+module.exports = {
+	swcMinify: true,
+	reactStrictMode: true,
+	transpilePackages: [
+		'@walless/gui',
+		'react',
+		'react-native',
+		'react-native-reanimated',
+		'react-native-svg',
+	],
+	webpack: (config, { dev }) => {
+		config.resolve.alias = {
+			...(config.resolve.alias || {}),
+			'react-native$': 'react-native-web',
+		};
+
+		config.resolve.extensions = [
+			'.web.js',
+			'.web.jsx',
+			'.web.ts',
+			'.web.tsx',
+			...config.resolve.extensions,
+		];
+
+		config.plugins.push(
+			new DefinePlugin({
+				__DEV__: dev,
+			}),
+		);
+
+		return config;
+	},
+};

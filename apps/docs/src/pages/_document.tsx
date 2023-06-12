130b921b9e9ca1f92f79c945bb3f418974f91a60
Tan Le <tanlethanh8266@gmail.com>
2023-06-12 12:11:48 +0700

    Remove unused getInitialProps method

--- apps/docs/src/pages/_document.tsx
+++ apps/docs/src/pages/_document.tsx
@@ -1,20 +1,4 @@
-import { Children } from 'react';
-import { AppRegistry } from 'react-native';
-import {
-	type DocumentContext,
-	Head,
-	Html,
-	Main,
-	NextScript,
-} from 'next/document';
-
-const normalizeNextElements = `
-	#__next {
-		display: flex;
-		flex-direction: column;
-		height: 100%;
-	}
-`;
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export const Document = () => {
 	return (
@@ -47,23 +31,6 @@ export const Document = () => {
 };
 
 export default Document;
-
-export const getInitialProps = async ({ renderPage }: DocumentContext) => {
-	AppRegistry.registerComponent('Main', () => Main);
-
-	// eslint-disable-next-line
-	const { getStyleElement } = (AppRegistry as any).getApplication('Main');
-	const page = await renderPage();
-	const styles = [
-		<style
-			key="style"
-			dangerouslySetInnerHTML={{ __html: normalizeNextElements }}
-		/>,
-		getStyleElement(),
-	];
-
-	return { ...page, styles: Children.toArray(styles) };
-};
 
 const fontUri =
 	'https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,900&display=swap';

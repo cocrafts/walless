88fd5b698031f3c92e1a31dae7cdf4fcb369a9f0
Tan Le <tanlethanh8266@gmail.com>
2023-06-14 03:05:36 +0700

    Update import

---
+++ apps/docs/src/components/Drawer/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from '@walless/gui';
+
+export const Drawer = () => {
+	return (
+		<View>
+			<Text>index</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});

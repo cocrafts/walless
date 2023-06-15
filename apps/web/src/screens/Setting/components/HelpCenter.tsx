7c91a7ece9a9b7a68e3a3324cc15fb17ac025aa4
ltminhthu <ltminhthu.209@gmail.com>
2023-06-15 16:31:31 +0700

    refactor setting components

--- apps/web/src/screens/Setting/components/HelpCenter.tsx
+++ apps/web/src/screens/Setting/components/HelpCenter.tsx
@@ -1,9 +1,9 @@
-import { Book, Message, Window } from '@walless/icons';
+import { Book, Message, Shield, Window } from '@walless/icons';
 import { Stack, Text } from '@walless/ui';
 
 import ForwardLink from './ForwardLink';
 
-const HelpCenter = () => {
+export const HelpCenter = () => {
 	return (
 		<Stack gap={12}>
 			<Text fontSize={14} color="#566674">
@@ -12,15 +12,20 @@ const HelpCenter = () => {
 
 			<Stack gap={8}>
 				<ForwardLink
+					link="https://discord.gg/uG2JEmTZXZ"
+					title="Feedback to us"
+					icon={<Message />}
+				/>
+				<ForwardLink
 					link="https://discord.gg/3v7jwG45pe"
 					title="Contact Support"
-					icon={<Message />}
+					icon={<Book />}
 				/>
 
 				<ForwardLink
 					link="https://walless.io/privacy-policy"
 					title="Privacy Policy"
-					icon={<Book />}
+					icon={<Shield size={16} />}
 				/>
 
 				<ForwardLink

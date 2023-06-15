3ecdde5c94d5237b4619c8b78f0f38015883682c
ltminhthu <ltminhthu.209@gmail.com>
2023-06-15 14:54:07 +0700

    navigate to latest screen after logging in

--- apps/web/src/state/app/authentication.ts
+++ apps/web/src/state/app/authentication.ts
@@ -46,7 +46,10 @@ export const setProfile = async (profile: UserProfile) => {
 		doc.type = 'Setting';
 		doc.version = '0.0.1';
 		doc.profile = profile;
-		doc.config = { hideBalance: true };
+		doc.config = {
+			hideBalance: true,
+			latestLocation: '/',
+		};
 
 		return doc;
 	});

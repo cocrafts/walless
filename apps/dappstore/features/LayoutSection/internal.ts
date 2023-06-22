7d009b12bffed9b278efac71f2428b14989fdd9d
ltminhthu <ltminhthu.209@gmail.com>
2023-06-22 21:51:28 +0700

    add mock data

---
+++ apps/dappstore/features/LayoutSection/internal.ts
@@ -0,0 +1,114 @@
+export interface LayoutCardProps {
+	id: string;
+	coverImage: string;
+	logoImage: string;
+	title: string;
+	description: string;
+	loveCount: number;
+	activeCount: number;
+	onDetails: () => void;
+	activeLayoutId?: string;
+	setIsActiveId?: (id: string) => void;
+}
+
+export const defiList: LayoutCardProps[] = [
+	{
+		id: 'defi-1',
+		coverImage: '/img/layouts/defi/cover/valorant.png',
+		logoImage: '/img/layouts/defi/logo/valorant.png',
+		title: 'Valorant',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		loveCount: 0,
+		activeCount: 0,
+		onDetails: () => {
+			console.log('the details are shown');
+		},
+	},
+	{
+		id: 'defi-2',
+		coverImage: '/img/layouts/defi/cover/sui.png',
+		logoImage: '/img/layouts/defi/logo/sui.png',
+		title: 'Sui',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		loveCount: 0,
+		activeCount: 0,
+		onDetails: () => {
+			console.log('the details are shown');
+		},
+	},
+	{
+		id: 'defi-3',
+		coverImage: '/img/layouts/defi/cover/solana.png',
+		logoImage: '/img/layouts/defi/logo/solana.png',
+		title: 'Solana',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		loveCount: 0,
+		activeCount: 0,
+		onDetails: () => {
+			console.log('the details are shown');
+		},
+	},
+	{
+		id: 'defi-4',
+		coverImage: '/img/layouts/defi/cover/fortnite.png',
+		logoImage: '/img/layouts/defi/logo/sui.png',
+		title: 'Fortnite',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		loveCount: 0,
+		activeCount: 0,
+		onDetails: () => {
+			console.log('the details are shown');
+		},
+	},
+];
+
+export const gamesList: LayoutCardProps[] = [
+	{
+		id: 'game-1',
+		coverImage: '/img/layouts/games/cover/valorant.png',
+		logoImage: '/img/layouts/games/logo/valorant.png',
+		title: 'Valorant',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		loveCount: 0,
+		activeCount: 0,
+		onDetails: () => {
+			console.log('the details are shown');
+		},
+	},
+	{
+		id: 'game-2',
+		coverImage: '/img/layouts/games/cover/magic-eden.png',
+		logoImage: '/img/layouts/games/logo/magic-eden.png',
+		title: 'Magic Eden',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		loveCount: 0,
+		activeCount: 0,
+		onDetails: () => {
+			console.log('the details are shown');
+		},
+	},
+	{
+		id: 'game-3',
+		coverImage: '/img/layouts/games/cover/nike.png',
+		logoImage: '/img/layouts/games/logo/nike.png',
+		title: 'Nike',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		loveCount: 0,
+		activeCount: 0,
+		onDetails: () => {
+			console.log('the details are shown');
+		},
+	},
+	{
+		id: 'game-4',
+		coverImage: '/img/layouts/games/cover/adidas.png',
+		logoImage: '/img/layouts/games/logo/adidas.png',
+		title: 'Adidas',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		loveCount: 0,
+		activeCount: 0,
+		onDetails: () => {
+			console.log('the details are shown');
+		},
+	},
+];

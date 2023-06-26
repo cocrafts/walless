12592c56aac36af987b3127f7fea43f0fbcb798d
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-06-27 01:15:26 +0700

    bump extension version

--- apps/landing/utils/connection.ts
+++ apps/landing/utils/connection.ts
@@ -2,6 +2,6 @@ import { clusterApiUrl, Connection } from '@solana/web3.js';
 
 const endpoint = __DEV__
 	? clusterApiUrl('devnet')
-	: 'https://broken-billowing-sanctuary.solana-mainnet.quiknode.pro/269a66566cb8bd89906698287232cf38c5cae13a/';
+	: 'https://nd-863-074-236.p2pify.com/0e6f78c82b1fcf2cd179a29a1dd0302e';
 
 export const connection = new Connection(endpoint);

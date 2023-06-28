69033314aec54bb7a6404518a02de095d0cbf409
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-06-28 10:55:34 +0700

    refactor home layout

--- apps/dappstore/pages/index.tsx
+++ apps/dappstore/pages/index.tsx
@@ -1,29 +1,14 @@
 import { type FC } from 'react';
-import { ScrollView } from 'react-native';
-import { View } from '@walless/gui';
-import { Footer, Header } from 'components/layouts';
+import HomeLayout from 'components/layouts/HomeLayout';
 import LayoutSection from 'features/LayoutSection';
 import WalletCustomizeSection from 'features/WalletCustomizeSection';
-import { sharedStyles } from 'utils/style';
 
 const IndexPage: FC = () => {
 	return (
-		<View style={sharedStyles.minScreen}>
-			<View style={sharedStyles.container}>
-				<Header />
-			</View>
-			<ScrollView
-				contentContainerStyle={sharedStyles.container}
-				showsVerticalScrollIndicator={false}
-			>
-				<View style={sharedStyles.contentContainer}>
-					<LayoutSection />
-
-					<WalletCustomizeSection />
-				</View>
-			</ScrollView>
-			<Footer />
-		</View>
+		<HomeLayout>
+			<LayoutSection />
+			<WalletCustomizeSection />
+		</HomeLayout>
 	);
 };
 

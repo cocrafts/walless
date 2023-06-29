08e22938da2303f979d3fafb22f41c020472cbd4
ltminhthu <ltminhthu.209@gmail.com>
2023-06-30 00:29:04 +0700

    update layout to routing

--- apps/widgets/src/pages/layouts/[id].tsx
+++ apps/widgets/src/pages/layouts/[id].tsx
@@ -1,31 +1,12 @@
-import { type FC, type ReactNode, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
-import { Button, Text, View } from '@walless/gui';
-import { Heart } from '@walless/icons';
+import { Text } from '@walless/gui';
 import { HomeLayout } from 'components/layouts';
+import { type LayoutProps, defiList, gamesList } from 'features/internal';
 import LayoutDetails from 'features/LayoutDetails';
-import {
-	type LayoutProps,
-	defiList,
-	gamesList,
-} from 'features/LayoutSection/internal';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 
-interface Props {
-	id: string;
-	coverImage: string;
-	logoImage: string;
-	title: string;
-	description: string;
-	loveCount: number;
-	activeCount: number;
-	Screenshots?: ReactNode;
-	Information?: ReactNode;
-	Comments?: ReactNode;
-}
-
-const Layout: FC<Props> = () => {
+const Layout = () => {
 	const [layout, setLayout] = useState<LayoutProps | null>(null);
 	const router = useRouter();
 	const { id } = router.query;
@@ -44,26 +25,7 @@ const Layout: FC<Props> = () => {
 			{!layout ? (
 				<Text>loading</Text>
 			) : (
-				<View style={styles.layoutContainer}>
-					<View style={styles.coverContainer}>
-						<Image src={layout?.coverImage} alt={layout?.title} fill={true} />
-					</View>
-
-					<View style={styles.bodyContainer}>
-						<View style={styles.blockContainer}>
-							<LayoutDetails
-								style={styles.informationContainer}
-								layout={layout}
-								logoSize={80}
-							/>
-						</View>
-
-						<View style={styles.blockContainer}>
-							<Button style={styles.button} title="Get early access" />
-							<Button style={styles.button} title="Share layout" />
-						</View>
-					</View>
-				</View>
+				<LayoutDetails style={styles.layoutContainer} layout={layout} />
 			)}
 		</HomeLayout>
 	);
@@ -74,7 +36,6 @@ export default Layout;
 const styles = StyleSheet.create({
 	container: {},
 	layoutContainer: {
-		width: 'fit-content',
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
@@ -83,20 +44,9 @@ const styles = StyleSheet.create({
 		height: 600,
 	},
 	bodyContainer: {
-		flexDirection: 'row',
+		flex: 1,
 		justifyContent: 'space-between',
 		width: '100%',
 		paddingHorizontal: 40,
-		gap: 200,
-	},
-	informationContainer: {
-		alignItems: 'flex-start',
-		marginTop: -40,
-	},
-	blockContainer: {
-		flex: 1,
-	},
-	button: {
-		marginLeft: 'auto',
 	},
 });

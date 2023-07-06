71220dd07ab05ca786d3bac39368287b8e564299
FME849 <1414439@gmail.com>
2023-07-06 10:38:32 +0700

    initialize `walless-adapter-solana-react` package

---
+++ packages/walless-adapter-solana-react/src/useWalless.ts
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { installLayout } from '@walless/walless-adapter-solana-base';
+
+export function useWalless() {
+	const { wallet } = useWallet();
+	const extendMethods = { installLayout };
+	const handleIsNotWalless = () => {
+		throw new Error('only available to Walless wallet');
+	};
+
+	useEffect(() => {
+		if (wallet?.adapter.name !== 'Walless') {
+			for (const method in extendMethods) {
+				extendMethods[method as keyof typeof extendMethods] =
+					handleIsNotWalless;
+			}
+		}
+	}, [wallet]);
+
+	return {
+		...wallet,
+		...extendMethods,
+	};
+}

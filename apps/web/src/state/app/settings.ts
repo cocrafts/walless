b2e452f4eb2387f6070038df11d4672d0f8379ab
fluctlight-kayaba <kayaba.fluctlight@gmail.com>
2023-07-11 06:43:34 +0700

    feat: sync stored endpoints (by engine) in live state

--- apps/web/src/state/app/settings.ts
+++ apps/web/src/state/app/settings.ts
@@ -1,15 +1,7 @@
 import { modules } from '@walless/ioc';
-import { type SettingDocument } from '@walless/store';
-import { appState } from 'state/app';
+import type { SettingDocument } from '@walless/store';
 
 const id = 'settings';
-
-export const sync = (settings?: SettingDocument) => {
-	if (settings?._id) {
-		appState.config = settings.config;
-		appState.profile = settings.profile;
-	}
-};
 
 export const setPrivacy = async (hideBalance: boolean) => {
 	await modules.storage.upsert<SettingDocument>(id, async (doc) => {

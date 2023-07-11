6bc0b39bd5dd44b18f8188b5b0911ae094980bfd
FME849 <1414439@gmail.com>
2023-07-11 12:24:36 +0700

    add `isWallessConnected` to `useWalless` hook

--- packages/walless-adapter-solana-react/src/useWalless.ts
+++ packages/walless-adapter-solana-react/src/useWalless.ts
@@ -3,6 +3,7 @@ import { useWallet } from '@solana/wallet-adapter-react';
 import { installLayout } from '@walless/walless-adapter-solana-base';
 
 export function useWalless() {
+	const [isWallessConnected, setIsWallessConnected] = useState(false);
 	const walletContext = useWallet();
 	const extendMethods = { installLayout };
 	const handleIsNotWalless = () => {
@@ -10,16 +11,23 @@ export function useWalless() {
 	};
 
 	useEffect(() => {
-		if (wallet?.adapter.name !== 'Walless') {
+		setIsWallessConnected(
+			walletContext.wallet?.adapter.name.toLocaleLowerCase() === 'walless',
+		);
+	}, [walletContext]);
+
+	useEffect(() => {
+		if (!isWallessConnected) {
 			for (const method in extendMethods) {
 				extendMethods[method as keyof typeof extendMethods] =
 					handleIsNotWalless;
 			}
 		}
-	}, [wallet]);
+	}, [isWallessConnected]);
 
 	return {
 		...walletContext,
 		...extendMethods,
+		isWallessConnected,
 	};
 }

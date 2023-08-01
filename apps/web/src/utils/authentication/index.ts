a92b94fb574b66ba0ed190f51bc0b060c478f6bd
Tan Le <tanlethanh8266@gmail.com>
2023-08-01 11:29:20 +0700

    refactor method to validate invitation code only

---
+++ apps/web/src/utils/authentication/index.ts
@@ -0,0 +1,18 @@
+import type { InvitationCode } from '@walless/graphql';
+import { queries } from '@walless/graphql';
+import { qlClient } from '@walless/graphql';
+
+export const validateInvitationCode = async (code: string) => {
+	const { invitationCode } = await qlClient.request<{
+		invitationCode: InvitationCode;
+	}>(queries.invitationCode, { code });
+
+	if (!invitationCode) {
+		throw Error('This invitation code is invalid!');
+	} else if (invitationCode?.email) {
+		throw Error('This invitation code is used by another account!');
+	} else {
+		return code;
+	}
+};
+

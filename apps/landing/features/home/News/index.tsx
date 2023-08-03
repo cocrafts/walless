b0cef48fb720dacefca6faf813377f4a8dfe7c20
FME849 <1414439@gmail.com>
2023-08-03 15:11:12 +0700

    update: separate big component into smaller ones

--- apps/landing/features/home/News/index.tsx
+++ apps/landing/features/home/News/index.tsx
@@ -1,8 +1,8 @@
-import { Image, Linking, StyleSheet } from 'react-native';
-import { Hoverable, Text, View } from '@walless/gui';
-import { ArrowRight } from '@walless/icons';
+import { StyleSheet } from 'react-native';
+import { Text, View } from '@walless/gui';
 import SectionContainer from 'components/SectionContainer';
 
+import CardNews from './CardNews';
 import { mockData } from './internal';
 
 export const News = () => {
@@ -14,30 +14,11 @@ export const News = () => {
 					const isLastItem = idx === mockData.length - 1;
 
 					return (
-						<View
+						<CardNews
 							key={idx}
-							style={[styles.cardContainer, isLastItem && { marginRight: 0 }]}
-						>
-							<View style={styles.cardContent}>
-								<Text style={styles.cardTitle} numberOfLines={2}>
-									{data.title}
-								</Text>
-								<Text style={styles.cardDescription} numberOfLines={3}>
-									{data.description}
-								</Text>
-								<View style={styles.cardLink}>
-									<Hoverable
-										horizontal
-										style={{ alignItems: 'center' }}
-										onPress={() => Linking.openURL(data.link)}
-									>
-										<Text style={styles.cardLinkText}>Read</Text>
-										<ArrowRight size={20} />
-									</Hoverable>
-								</View>
-							</View>
-							<Image source={data.image} style={styles.image} />
-						</View>
+							data={data}
+							style={isLastItem ? { marginRight: 0 } : {}}
+						/>
 					);
 				})}
 			</View>
@@ -61,44 +42,5 @@ const styles = StyleSheet.create({
 		marginTop: 30,
 		flexDirection: 'row',
 		justifyContent: 'space-between',
-	},
-	cardContainer: {
-		flex: 1,
-		marginRight: 20,
-		backgroundColor: '#202D38',
-		padding: 20,
-		borderRadius: 15,
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'stretch',
-	},
-	cardContent: {
-		flex: 1,
-		marginRight: 30,
-	},
-	cardTitle: {
-		fontSize: 18,
-		lineHeight: 26,
-		fontWeight: '500',
-		color: '#ffffff',
-	},
-	cardDescription: {
-		marginTop: 10,
-		lineHeight: 20,
-		color: '#566674',
-	},
-	cardLink: {
-		flex: 1,
-		justifyContent: 'flex-end',
-	},
-	cardLinkText: {
-		fontSize: 16,
-		color: '#ffffff',
-		marginRight: 5,
-	},
-	image: {
-		aspectRatio: 283 / 196,
-		width: 250,
-		borderRadius: 10,
 	},
 });
--- apps/landing/features/home/Partners/index.tsx
+++ apps/landing/features/home/Partners/index.tsx
@@ -1,49 +1,32 @@
 import { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import { Hoverable, Text, View } from '@walless/gui';
-import { ArrowRight } from '@walless/icons';
 import SectionContainer from 'components/SectionContainer';
 
 import Carousel from './Carousel';
+import TitleAndControl from './TitleAndControl';
 
 export const Partners = () => {
 	const [activeIndex, setActiveIndex] = useState(0);
 	const [isPrevDisable, setIsPrevDisable] = useState(false);
 	const [isNextDisable, setIsNextDisable] = useState(false);
+
 	const handleReachBounder = (isRightReach: boolean, isLeftReach: boolean) => {
 		setIsPrevDisable(isLeftReach);
 		setIsNextDisable(isRightReach);
 	};
 
+	const handleActiveIndexChange = (next: number) => {
+		setActiveIndex(next);
+	};
+
 	return (
 		<SectionContainer horizontal>
-			<View>
-				<Text style={styles.title}>Supporting partners</Text>
-				<View style={styles.carouselControlContainer}>
-					<View style={isPrevDisable && styles.disabledButton}>
-						<Hoverable
-							style={{
-								...styles.outlineButton,
-								...styles.leftArrow,
-							}}
-							onPress={() => setActiveIndex(activeIndex - 1)}
-							disabled={isPrevDisable}
-						>
-							<ArrowRight />
-						</Hoverable>
-					</View>
-					<View style={styles.buttonSeparator} />
-					<View style={isNextDisable && styles.disabledButton}>
-						<Hoverable
-							style={styles.outlineButton}
-							onPress={() => setActiveIndex(activeIndex + 1)}
-							disabled={isNextDisable}
-						>
-							<ArrowRight />
-						</Hoverable>
-					</View>
-				</View>
-			</View>
+			<TitleAndControl
+				activeIndex={activeIndex}
+				isPrevDisable={isPrevDisable}
+				isNextDisable={isNextDisable}
+				handleActiveIndexChange={handleActiveIndexChange}
+			/>
 			<Carousel
 				style={styles.carouselContainer}
 				activeIndex={activeIndex}
@@ -56,33 +39,6 @@ export const Partners = () => {
 export default Partners;
 
 const styles = StyleSheet.create({
-	title: {
-		fontSize: 32,
-		fontWeight: '500',
-		color: '#ffffff',
-	},
-	carouselControlContainer: {
-		flexDirection: 'row',
-		marginTop: 30,
-	},
-	outlineButton: {
-		width: 36,
-		height: 36,
-		borderRadius: 20,
-		borderColor: '#ffffff',
-		borderWidth: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	disabledButton: {
-		opacity: 0.5,
-	},
-	leftArrow: {
-		transform: [{ rotate: '180deg' }],
-	},
-	buttonSeparator: {
-		width: 15,
-	},
 	carouselContainer: {
 		marginLeft: 30,
 		flex: 1,

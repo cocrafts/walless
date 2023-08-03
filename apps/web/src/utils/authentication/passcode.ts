5f9f974d28043d248cd799f4c829e48c5e20659b
Tan Le <tanlethanh8266@gmail.com>
2023-08-04 02:18:45 +0700

    deprecate passcode flow

--- apps/web/src/utils/authentication/passcode.ts
+++ apps/web/src/utils/authentication/passcode.ts
@@ -3,10 +3,18 @@ import {
 	recoverDeviceShareFromPasscode,
 } from '../w3a';
 
+/**
+ * @deprecated
+ * This method is deprecated by using recovery flow
+ * */
 export const setupRemotePasscode = async (passcode: string) => {
 	await configureSecurityQuestionShare(passcode);
 };
 
+/**
+ * @deprecated
+ * This method is deprecated by using recovery flow
+ * */
 export const validateAndRecoverWithPasscode = async (passcode: string) => {
 	const unlockSuccess = await recoverDeviceShareFromPasscode(passcode);
 	return unlockSuccess;

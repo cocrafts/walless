bf782511c41afa03d63c3d9f557989232ae665ce
FME849 <1414439@gmail.com>
2023-08-09 11:06:34 +0700

    chore: fix style, group similar styles under shared.ts

--- apps/landing/features/home/News/index.tsx
+++ apps/landing/features/home/News/index.tsx
@@ -2,13 +2,15 @@ import { StyleSheet } from 'react-native';
 import { Text, View } from '@walless/gui';
 import SectionContainer from 'components/SectionContainer';
 
+import { sharedStyles } from '../shared';
+
 import CardNews from './CardNews';
 import { mockData } from './internal';
 
 export const News = () => {
 	return (
 		<SectionContainer style={styles.container}>
-			<Text style={styles.title}>What&rsquo;s in the corner :)</Text>
+			<Text style={sharedStyles.title}>What&rsquo;s in the corner :)</Text>
 			<View style={styles.contentContainer}>
 				{mockData.map((data, idx) => {
 					const isLastItem = idx === mockData.length - 1;
@@ -31,11 +33,6 @@ export default News;
 const styles = StyleSheet.create({
 	container: {
 		alignItems: 'center',
-	},
-	title: {
-		fontSize: 32,
-		fontWeight: '500',
-		color: '#ffffff',
 	},
 	contentContainer: {
 		width: '100%',

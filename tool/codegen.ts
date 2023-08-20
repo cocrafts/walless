915cc6a38fdaab938cc9831e81d1352c187c4c8c
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-08-21 04:35:17 +0700

    feat: codegen upgrade, use gotenks interface

--- tool/codegen.ts
+++ tool/codegen.ts
@@ -2,10 +2,13 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
 	overwrite: true,
-	schema: 'https://api-dev.walless.io/light',
+	schema: 'https://nerve.walless.io/graphql',
 	generates: {
 		'./packages/graphql/types.ts': {
 			plugins: ['typescript', 'typescript-resolvers'],
+		},
+		'./schema.graphql': {
+			plugins: ['schema-ast'],
 		},
 	},
 };

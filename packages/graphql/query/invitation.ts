ab159ae9f438ee1cf5f6da2688f1e168fa5bbdad
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-08-21 04:58:01 +0700

    feat: migrate invitation flow

--- packages/graphql/query/invitation.ts
+++ packages/graphql/query/invitation.ts
@@ -1,19 +1,10 @@
 import { gql } from 'graphql-request';
 
-export const invitationCode = gql`
-	query invitationCode($code: String!) {
-		invitationCode(code: $code) {
-			pk
+export const walletInvitation = gql`
+	query walletInvitation($code: String, $email: String) {
+		walletInvitation(code: $code, email: $email) {
+			code
 			email
-			timestamp
-		}
-	}
-`;
-
-export const invitationAccount = gql`
-	query invitationAccount($email: String!) {
-		invitationAccount(email: $email) {
-			pk
 		}
 	}
 `;

d81048f02c634a9a3a9ca87163245d92eb9a3206
Tuan Tran <1414439@gmail.com>
2023-08-22 16:16:53 +0700

    update: bring back previous version of index.tsx file

--- apps/landing/pages/index.tsx
+++ apps/landing/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import HomeLayout from 'components/layouts/Home';
 // import Developer from 'features/home/Developer';
 import EasyOnboarding from 'features/home/EasyOnboarding';
@@ -8,42 +7,19 @@ import HeadingSection from 'features/home/HeadingSection';
 // TODO: Refactor Partners section
 import Partners from 'features/home/LegacyPartners';
 import News from 'features/home/News';
-import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import Head from 'next/head';
 
-export const IndexPage = ({
-	imgUrl,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-	return (
-		<Fragment>
-			{imgUrl && (
-				<Head>
-					<meta property="og:image" content={imgUrl} key="ogImage" />
-					<meta property="twitter:image" content={imgUrl} key="twitterImage" />
-				</Head>
-			)}
-			<HomeLayout>
-				<HeadingSection />
-				<ExtensionDownload />
-				<EasyOnboarding />
-				<EditTool />
-				{/* <Developer /> */}
-				<News />
-				<Partners />
-			</HomeLayout>
-		</Fragment>
-	);
+export const IndexPage = () => {
+    return (
+        <HomeLayout>
+            <HeadingSection />
+            <ExtensionDownload />
+            <EasyOnboarding />
+            <EditTool />
+            {/* <Developer /> */}
+            <News />
+            <Partners />
+        </HomeLayout>
+    );
 };
 
 export default IndexPage;
-
-export const getServerSideProps: GetServerSideProps<{
-	imgUrl: string;
-}> = async ({ query }) => {
-	const { params } = query;
-	const imgUrl: string = params?.[0]
-		? `https://cdn.stormgate.io/walless/waitlist-cards/${params[0]}.png`
-		: '';
-
-	return { props: { imgUrl } };
-};

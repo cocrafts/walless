005503bd919ac20e4accadbe6a66bf8c22189d9d
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-09-01 00:38:22 +0700

    remove: local endpoint

--- apps/widgets/src/utils/graphql.ts
+++ apps/widgets/src/utils/graphql.ts
@@ -1,10 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
-const localEndpoint = 'http://localhost:8080/graphql';
-
 export const qlClient = new GraphQLClient(
-	// process.env.GRAPHQL_ENDPOINT as string,
-	localEndpoint,
+	process.env.GRAPHQL_ENDPOINT as string,
 	{
 		errorPolicy: 'all',
 	},

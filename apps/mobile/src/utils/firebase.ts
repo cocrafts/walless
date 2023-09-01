706a142ee59b03614ff54bfaa8143bec1777482d
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-09-01 16:17:31 +0700

    feat: replicate firebase module in Mobile

--- apps/mobile/src/utils/firebase.ts
+++ apps/mobile/src/utils/firebase.ts
@@ -1,4 +1,23 @@
 import auth from '@react-native-firebase/auth';
+import remoteConfig from '@react-native-firebase/remote-config';
+import { defaultConfig } from '@walless/app';
+import type { RemoteConfig } from '@walless/core';
+
+const minimumFetchIntervalMillis = __DEV__
+	? 10000 // 10 seconds for development
+	: 3600000; // 1 hour for prod
+remoteConfig().setDefaults(defaultConfig as never);
+remoteConfig().setConfigSettings({ minimumFetchIntervalMillis });
+
+export const loadRemoteConfig = (): RemoteConfig => {
+	remoteConfig().activate();
+	remoteConfig().fetch(); // fetch for next launch
+	const allConfig = remoteConfig().getAll();
+
+	return {
+		experimentalEnabled: allConfig.experimentalEnabled?.asBoolean(),
+	};
+};
 
 export interface FireCache {
 	idToken?: string;

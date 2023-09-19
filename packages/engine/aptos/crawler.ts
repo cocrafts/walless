404c95dd57c560d159411a31aabf458168578d91
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-09-19 18:22:38 +0700

    sync

--- packages/engine/aptos/crawler.ts
+++ packages/engine/aptos/crawler.ts
@@ -1,23 +1,33 @@
-import type { PublicKeyDocument } from '@walless/store';
+import type { PublicKeyDocument, TokenDocument } from '@walless/store';
 import { selectors } from '@walless/store';
-import type { Provider } from 'aptos';
+import { type Provider, FaucetClient, HexString } from 'aptos';
 
+import { tokenActions } from '../state/tokens';
 import type { EngineRunner } from '../utils/type';
+
+import { APTOS_DEVNET, APTOS_FAUCET_DEVNET } from './shared';
 
 export const aptosEngineRunner: EngineRunner<Provider> = {
 	start: async (context) => {
-		const { endpoint, connection, storage, qlClient } = context;
-
-		console.log('aptosEngineRunner start');
-		console.log('--> endpoint', endpoint);
-		console.log('--> connection aka provider', connection);
-		console.log('--> storage', storage);
-		console.log('--> qlClient', qlClient);
+		const { connection, storage } = context;
 
 		const keyResult = await storage.find(selectors.aptosKeys);
 		const keys = keyResult.docs as PublicKeyDocument[];
 
-		console.log('--> keys', keys);
+		const pubkey = new HexString(keys[0]._id);
+
+		try {
+			const data = await connection.getAccountResources(pubkey);
+			console.log('--> data', data);
+
+			const tokenDocuments: TokenDocument[] = [];
+
+			tokenActions.setItems(tokenDocuments);
+		} catch (error) {
+			// Create account in devnet for __DEV__ mode
+			const faucet = new FaucetClient(APTOS_DEVNET, APTOS_FAUCET_DEVNET);
+			await faucet.fundAccount(pubkey, 1000000000);
+		}
 	},
 	stop: async () => {
 		// MIGHT UPDATE THIS IN THE FUTURE

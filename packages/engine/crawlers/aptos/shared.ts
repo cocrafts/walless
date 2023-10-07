6a564573ffcdb6eb081a9a78d39069ce69267a2b
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-10-07 09:28:34 +0700

    chore: remove redundant file

--- packages/engine/crawlers/aptos/shared.ts
+++
@@ -1,9 +0,0 @@
-// https://aptos.dev/guides/system-integrators-guide/#production-network-access
-export const APTOS_MAINNET = 'https://fullnode.mainnet.aptoslabs.com';
-export const APTOS_TESTNET = 'https://fullnode.testnet.aptoslabs.com';
-export const APTOS_DEVNET = 'https://fullnode.devnet.aptoslabs.com';
-
-export const APTOS_FAUCET_DEVNET = 'https://faucet.devnet.aptoslabs.com';
-
-export const APTOS_COIN_DEVNET =
-	'0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>';

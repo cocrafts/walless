b9c3ddd7a0ab1b4b05a11d35160141a72bcc8bd7
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-10-24 12:22:00 +0700

    ui: aptos layout

---
+++ apps/web/src/screens/Dashboard/Aptos.tsx
@@ -0,0 +1,142 @@
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import type { CardSkin, TabAble } from '@walless/app';
+import {
+	MainFeatureButtons,
+	SlideHandler,
+	TabsHeader,
+	WalletCard,
+} from '@walless/app';
+import { Networks } from '@walless/core';
+import type { SlideOption } from '@walless/gui';
+import { Slider, View } from '@walless/gui';
+import { Copy } from '@walless/icons';
+import { appActions } from 'state/app';
+import { showReceiveModal } from 'state/app/modal';
+import { onrampWithGateFi } from 'utils/gatefi';
+import { usePublicKeys, useSettings, useTokens } from 'utils/hooks';
+
+import ActivityTab from './components/ActivityTab';
+import AptosTokensTab from './components/AptosTokensTab';
+import { TokenTab } from './components';
+import { layoutTabs } from './shared';
+
+const AptosDashboard = () => {
+	const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+	const { setting, setPrivacy } = useSettings();
+
+	const publicKeys = usePublicKeys(Networks.aptos);
+	const { tokens, valuation } = useTokens(Networks.aptos);
+
+	const bottomSliderItems: SlideOption[] = [
+		{
+			id: 'tokens',
+			component: () => <TokenTab tokens={tokens} />,
+		},
+		{
+			id: 'collectibles',
+			component: () => <AptosTokensTab pubkey={publicKeys[0]._id} />,
+		},
+		{
+			id: 'activities',
+			component: () => <ActivityTab network={Networks.aptos} />,
+		},
+	];
+
+	const handleCopyAddress = async (value: string) => {
+		await appActions.copy(value, () => <Copy size={18} color="#FFFFFF" />);
+	};
+
+	const handleChangePrivateSetting = (next: boolean) => {
+		setPrivacy(next);
+	};
+
+	const handleTabPress = (item: TabAble) => {
+		const idx = layoutTabs.indexOf(item);
+		setActiveTabIndex(idx);
+	};
+
+	const handleSend = () => {
+		appActions.showSendModal({ layoutNetwork: Networks.aptos });
+	};
+
+	const handleBuy = () => {
+		onrampWithGateFi({ wallet: publicKeys[0]._id });
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.walletsContainer}>
+				{publicKeys.map((item, index) => (
+					<WalletCard
+						key={index}
+						index={index}
+						item={item}
+						valuation={valuation}
+						skin={aptosCardSkin}
+						hideBalance={setting.hideBalance}
+						onCopyAddress={handleCopyAddress}
+						onChangePrivateSetting={handleChangePrivateSetting}
+						width={publicKeys.length == 1 ? 328 : 312}
+					/>
+				))}
+			</View>
+
+			<View style={styles.mainFeaturesContainer}>
+				<MainFeatureButtons
+					onReceivePress={() => showReceiveModal(Networks.aptos)}
+					onSendPress={handleSend}
+					onBuyPress={handleBuy}
+				/>
+				{publicKeys.length > 1 && (
+					<SlideHandler items={publicKeys} activeItem={publicKeys[0]} />
+				)}
+			</View>
+
+			<View>
+				<TabsHeader
+					items={layoutTabs}
+					activeItem={layoutTabs[activeTabIndex]}
+					onTabPress={handleTabPress}
+				/>
+
+				<Slider
+					style={styles.sliderContainer}
+					items={bottomSliderItems}
+					activeItem={bottomSliderItems[activeTabIndex]}
+				/>
+			</View>
+		</View>
+	);
+};
+
+export default AptosDashboard;
+
+const aptosCardSkin: CardSkin = {
+	backgroundSrc: { uri: '/img/network/sky-card-bg.png' },
+	largeIconSrc: { uri: '/img/explore/logo-trans-aptos.svg' },
+	iconSrc: { uri: '/img/explore/logo-trans-aptos.svg' },
+	iconColor: 'transparent',
+	iconSize: 16,
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 12,
+		gap: 18,
+	},
+	mainFeaturesContainer: {
+		alignItems: 'center',
+		gap: 18,
+	},
+	walletsContainer: {
+		flexDirection: 'row',
+		gap: 12,
+	},
+	sliderContainer: {
+		flex: 1,
+		height: '100%',
+	},
+});
---
+++ apps/web/src/screens/Dashboard/components/AptosTokensTab/index.tsx
@@ -0,0 +1,107 @@
+import { type FC, useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import type { TabAble } from '@walless/app';
+import { TabsHeader } from '@walless/app';
+import { aptosState } from '@walless/engine';
+import { getAptosConnection } from '@walless/engine/crawlers/aptos/utils';
+import type { SlideOption } from '@walless/gui';
+import { Slider, View } from '@walless/gui';
+import { useSnapshot } from 'valtio';
+
+import DirectTransfer from './DirectTransfer';
+import OwnedTokens from './OwnedTokens';
+import PendingTokens from './PendingTokens';
+
+interface Props {
+	pubkey: string;
+}
+
+const AptosTokensTab: FC<Props> = ({ pubkey }) => {
+	const aptosSnap = useSnapshot(aptosState);
+	const [fee, setFee] = useState(0);
+
+	useEffect(() => {
+		const getFee = async () => {
+			const conn = await getAptosConnection();
+			const fee = await conn.estimateGasPrice();
+			return fee.gas_estimate / 10 ** 8;
+		};
+		getFee().then(setFee).catch(console.error);
+	}, [aptosSnap]);
+
+	const ownedTokens = Array.from(aptosSnap.ownedTokens.values());
+	const pendingTokens = Array.from(aptosSnap.pendingTokens.values());
+	const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+	const layoutTabs: TabAble[] = [
+		{
+			id: 'owned',
+			title: `Owned Tokens (${ownedTokens.length})`,
+		},
+		{
+			id: 'pending',
+			title: `Pending Tokens (${pendingTokens.length})`,
+		},
+	];
+
+	const bottomSliderItems: SlideOption[] = [
+		{
+			id: 'owned',
+			component: () => <OwnedTokens />,
+		},
+		{
+			id: 'pending',
+			component: () => <PendingTokens fee={fee} />,
+		},
+	];
+
+	const handleTabPress = (item: TabAble) => {
+		const idx = layoutTabs.indexOf(item);
+		setActiveTabIndex(idx);
+	};
+
+	return (
+		<View style={styles.container}>
+			<DirectTransfer
+				pubkey={pubkey}
+				directTransfer={aptosSnap.directTransfer}
+				fee={fee}
+			/>
+
+			<View style={styles.tabContainer}>
+				<TabsHeader
+					items={layoutTabs}
+					activeItem={layoutTabs[activeTabIndex]}
+					onTabPress={handleTabPress}
+				/>
+
+				<Slider
+					style={styles.sliderContainer}
+					items={bottomSliderItems}
+					activeItem={bottomSliderItems[activeTabIndex]}
+				/>
+			</View>
+		</View>
+	);
+};
+
+export default AptosTokensTab;
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		justifyContent: 'space-between',
+		rowGap: 10,
+		paddingTop: 16,
+		paddingBottom: 60,
+	},
+	tabContainer: {
+		width: '100%',
+	},
+	sliderContainer: {
+		flex: 1,
+		marginTop: 16,
+		height: '100%',
+	},
+});

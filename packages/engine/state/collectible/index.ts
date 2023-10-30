45c2b5854b0cfa010ce8b69022ef56e63a0887df
FME849 <1414439@gmail.com>
2023-10-30 15:50:11 +0700

    update: add and update collectibleActions methods

--- packages/engine/state/collectible/index.ts
+++ packages/engine/state/collectible/index.ts
@@ -21,6 +21,10 @@ export const collectibleActions = {
 		await modules.storage.upsert<CollectionDocument>(id, async () => item);
 	},
 	updateCollectibleAmount: async (id: string, amount: number) => {
+		const collectible = collectibleState.map.get(id);
+
+		if (!collectible) return false;
+
 		const result = await modules.storage.upsert<CollectibleDocument>(
 			id,
 			async (prevDoc) => {
@@ -31,6 +35,40 @@ export const collectibleActions = {
 		);
 
 		return result.ok;
+	},
+	updateCollectionAmount: async (id: string, count: number) => {
+		const result = await modules.storage.upsert<CollectionDocument>(
+			id,
+			async (prevDoc) => {
+				prevDoc.count = count;
+
+				return prevDoc;
+			},
+		);
+
+		return result.ok;
+	},
+	removeCollectibleDoc: async (id: string) => {
+		const collectible = await modules.storage.safeGet<CollectibleDocument>(id);
+		const collection = await modules.storage.safeGet<CollectionDocument>(
+			collectible!.collectionId,
+		);
+		const collectionCount = collection!.count - 1;
+
+		if (collectionCount === 0) {
+			await modules.storage.removeDoc(collection!._id);
+		} else {
+			await modules.storage.upsert<CollectionDocument>(
+				collection!._id,
+				async (prevDoc) => {
+					prevDoc.count -= 1;
+
+					return prevDoc;
+				},
+			);
+		}
+
+		await modules.storage.removeDoc(collectible!._id);
 	},
 };
 

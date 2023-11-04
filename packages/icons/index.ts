840df13ec0c9c32c41a4860a5e05553b4c791cd1
ltminhthu <ltminhthu.209@gmail.com>
2023-11-04 21:16:15 +0700

    add assets and icons

--- packages/icons/index.ts
+++ packages/icons/index.ts
@@ -14,6 +14,7 @@ export { ChevronDown } from './components/ChevronDown';
 export { ChevronLeft } from './components/ChevronLeft';
 export { ChevronRight } from './components/ChevronRight';
 export { ChevronUp } from './components/ChevronUp';
+export { Clock } from './components/Clock';
 export { Compass } from './components/Compass';
 export { Copy } from './components/Copy';
 export { Discord } from './components/Discord';

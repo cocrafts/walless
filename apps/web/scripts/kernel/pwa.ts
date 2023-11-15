29a06291d8c484b60adefa4e958abe616367852f
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-11-16 06:51:06 +0700

    chore: don't use pwa fetch yet, will work on offline support later

--- apps/web/scripts/kernel/pwa.ts
+++ apps/web/scripts/kernel/pwa.ts
@@ -1,7 +1,6 @@
 export const configurePWA = () => {
 	self.addEventListener('install', onInstall);
 	self.addEventListener('activate', onActivate);
-	self.addEventListener('fetch', onFetch);
 };
 
 const onInstall = () => {
@@ -10,8 +9,4 @@ const onInstall = () => {
 
 const onActivate = () => {
 	console.log('service worker activated');
-};
-
-const onFetch = (event: Event) => {
-	console.log(event);
 };

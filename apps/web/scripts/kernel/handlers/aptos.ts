f565417028444c1b4cbf8310995dd23453f357d8
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-11-17 14:30:41 +0700

    refactor: remove duplicate code

--- apps/web/scripts/kernel/handlers/aptos.ts
+++ apps/web/scripts/kernel/handlers/aptos.ts
@@ -9,56 +9,36 @@ type Payload = {
 	transaction?: string;
 };
 
-export const transferCoin: HandleMethod<Payload> = async ({ payload }) => {
-	if (!payload.privateKey || !payload.transaction) {
-		throw Error('Missing privateKey or transaction');
-	}
+type AptosHandle = (
+	privateKey: Uint8Array,
+	transaction: string,
+) => Promise<string>;
 
-	const signatureString = await aptosHandler.handleTransferCoin(
-		payload.privateKey,
-		payload.transaction,
-	);
+export const constructAptosHandle =
+	(handle: AptosHandle): HandleMethod<Payload> =>
+	async ({ payload }) => {
+		if (!payload.privateKey || !payload.transaction) {
+			throw Error('Missing privateKey or transaction');
+		}
 
-	respond(payload.requestId, ResponseCode.SUCCESS, { signatureString });
-};
+		const signatureString = await handle(
+			payload.privateKey,
+			payload.transaction,
+		);
 
-export const transferToken: HandleMethod<Payload> = async ({ payload }) => {
-	if (!payload.privateKey || !payload.transaction) {
-		throw Error('Missing privateKey or transaction');
-	}
+		respond(payload.requestId, ResponseCode.SUCCESS, { signatureString });
+	};
 
-	const signatureString = await aptosHandler.handleTransferToken(
-		payload.privateKey,
-		payload.transaction,
-	);
+export const transferCoin = constructAptosHandle(
+	aptosHandler.handleTransferCoin,
+);
 
-	respond(payload.requestId, ResponseCode.SUCCESS, { signatureString });
-};
+export const transferToken = constructAptosHandle(
+	aptosHandler.handleTransferToken,
+);
 
-export const claimToken: HandleMethod<Payload> = async ({ payload }) => {
-	if (!payload.privateKey || !payload.transaction) {
-		throw Error('Missing privateKey or transaction');
-	}
+export const claimToken = constructAptosHandle(aptosHandler.handleClaimToken);
 
-	const signatureString = await aptosHandler.handleClaimToken(
-		payload.privateKey,
-		payload.transaction,
-	);
-
-	respond(payload.requestId, ResponseCode.SUCCESS, { signatureString });
-};
-
-export const updateDirectTransfer: HandleMethod<Payload> = async ({
-	payload,
-}) => {
-	if (!payload.privateKey || !payload.transaction) {
-		throw Error('Missing privateKey or transaction');
-	}
-
-	const signatureString = await aptosHandler.handleUpdateDirectTransfer(
-		payload.privateKey,
-		payload.transaction,
-	);
-
-	respond(payload.requestId, ResponseCode.SUCCESS, { signatureString });
-};
+export const updateDirectTransfer = constructAptosHandle(
+	aptosHandler.handleUpdateDirectTransfer,
+);

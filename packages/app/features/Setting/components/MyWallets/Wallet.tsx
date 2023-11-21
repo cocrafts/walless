fd223f1d6bbc6ceb1a7d32fc3f10a9799f0ed7fa
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-11-21 11:55:59 +0700

    fix: lose wallet icons on web profile

--- packages/app/features/Setting/components/MyWallets/Wallet.tsx
+++ packages/app/features/Setting/components/MyWallets/Wallet.tsx
@@ -4,6 +4,7 @@ import { copy, getNetworkInfo } from '@walless/app/utils';
 import { shortenAddress } from '@walless/core';
 import { Hoverable, Text, View } from '@walless/gui';
 import { Copy } from '@walless/icons';
+import { modules } from '@walless/ioc';
 import type { PublicKeyDocument } from '@walless/store';
 
 interface Props {
@@ -13,14 +14,22 @@ interface Props {
 
 export const Wallet: FC<Props> = ({ item, index }) => {
 	const network = getNetworkInfo(item.network);
+
 	const onCopy = async () => {
 		await copy(item._id, () => <Copy size={18} color="#FFFFFF" />);
 	};
 
+	if (!network) {
+		return null;
+	}
+
 	return (
 		<Hoverable horizontal style={styles.container} onPress={onCopy}>
 			<View horizontal style={{ gap: 10 }}>
-				<Image source={network?.icon as never} style={styles.icon} />
+				<Image
+					source={network.icon || modules.asset.setting[network.network].icon}
+					style={styles.icon}
+				/>
 
 				<View>
 					<Text>

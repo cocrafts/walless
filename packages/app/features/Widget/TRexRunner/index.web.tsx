a482c0794d92b1b63c385b541a6006eb70c2423e
Tan Le <tanlethanh8266@gmail.com>
2023-11-22 16:58:35 +0700

    fix: auto focus game when open game widget

--- packages/app/features/Widget/TRexRunner/index.web.tsx
+++ packages/app/features/Widget/TRexRunner/index.web.tsx
@@ -1,14 +1,23 @@
+import { useRef } from 'react';
 import { StyleSheet } from 'react-native';
 import { View } from '@walless/gui';
 
 export const TRexRunner = () => {
+	const iframeRef = useRef<HTMLIFrameElement>(null);
+
+	const handleOnLoad = () => {
+		iframeRef.current?.contentWindow?.focus();
+	};
+
 	return (
 		<View style={styles.container}>
 			<iframe
+				ref={iframeRef}
 				src="https://cdn.stormgate.io/documents/runner/index.html"
 				width={352}
 				height={300}
 				style={{ borderColor: 'transparent' }}
+				onLoad={handleOnLoad}
 			/>
 		</View>
 	);

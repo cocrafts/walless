c6676588918bae5089688160271d4d9dfd37115b
FME849 <1414439@gmail.com>
2023-12-01 15:55:11 +0700

    fix: make Swipe down gesture a component

--- packages/app/modals/SendToken.tsx
+++ packages/app/modals/SendToken.tsx
@@ -1,19 +1,9 @@
 import type { FC } from 'react';
 import { StyleSheet } from 'react-native';
-import {
-	Gesture,
-	GestureDetector,
-	GestureHandlerRootView,
-} from 'react-native-gesture-handler';
-import {
-	runOnJS,
-	useAnimatedStyle,
-	useSharedValue,
-	withTiming,
-} from 'react-native-reanimated';
 import type { Networks } from '@walless/core';
+import { runtime } from '@walless/core';
 import type { ModalConfigs } from '@walless/gui';
-import { AnimatedView, modalActions } from '@walless/gui';
+import { modalActions, SwipeDownGesture } from '@walless/gui';
 import { utils } from '@walless/ioc';
 import type { CollectibleDocument } from '@walless/store';
 
@@ -40,63 +30,28 @@ export const SendModal: FC<{ config: ModalConfigs }> = ({ config }) => {
 		modalActions.hide(config.id as string);
 	};
 
-	const offset = useSharedValue(0);
-	const context = useSharedValue(0);
-	const modalHeight = useSharedValue(0);
-
-	const pan = Gesture.Pan()
-		.onStart(() => {
-			context.value = offset.value;
-		})
-		.onUpdate((event) => {
-			const newPosition = event.translationY + context.value;
-			if (newPosition > 0) {
-				offset.value = newPosition;
-			}
-		})
-		.onEnd((event) => {
-			const newPosition = event.translationY + context.value;
-			if (newPosition > 100) {
-				offset.value = withTiming(modalHeight.value);
-				runOnJS(handleClose)();
-			} else {
-				offset.value = withTiming(0);
-			}
-		});
-
-	const animatedStyle = useAnimatedStyle(() => {
-		return {
-			transform: [{ translateY: offset.value }],
-		};
-	});
-
 	return (
-		<GestureHandlerRootView>
-			<GestureDetector gesture={pan}>
-				<AnimatedView
-					style={[styles.container, animatedStyle]}
-					onLayout={({ nativeEvent }) => {
-						modalHeight.value = nativeEvent.layout.height;
-					}}
-				>
-					<SendFeature
-						initCollectible={collectible}
-						tokens={tokens}
-						tokenForFee={tokens[0]}
-						nftCollections={collections}
-						nftCollectibles={collectibles}
-						publicKeys={addressList}
-						// TODO: resolve this with gasilon
-						getTransactionFee={getTransactionFee}
-						getTransactionAbstractFee={getTransactionAbstractFee}
-						onClose={handleClose}
-						checkValidAddress={checkValidAddress}
-						createAndSendTransaction={utils.createAndSend}
-						network={layoutNetwork}
-					/>
-				</AnimatedView>
-			</GestureDetector>
-		</GestureHandlerRootView>
+		<SwipeDownGesture
+			style={styles.container}
+			callbackOnClose={handleClose}
+			gestureEnable={runtime.isMobile}
+		>
+			<SendFeature
+				initCollectible={collectible}
+				tokens={tokens}
+				tokenForFee={tokens[0]}
+				nftCollections={collections}
+				nftCollectibles={collectibles}
+				publicKeys={addressList}
+				// TODO: resolve this with gasilon
+				getTransactionFee={getTransactionFee}
+				getTransactionAbstractFee={getTransactionAbstractFee}
+				onClose={handleClose}
+				checkValidAddress={checkValidAddress}
+				createAndSendTransaction={utils.createAndSend}
+				network={layoutNetwork}
+			/>
+		</SwipeDownGesture>
 	);
 };
 
---
+++ packages/gui/components/SwipeDownGesture.tsx
@@ -0,0 +1,94 @@
+import type { FC, ReactNode } from 'react';
+import type { LayoutChangeEvent, ViewStyle } from 'react-native';
+import type {
+	GestureUpdateEvent,
+	PanGestureHandlerEventPayload,
+} from 'react-native-gesture-handler';
+import {
+	Gesture,
+	GestureDetector,
+	GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+import {
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming,
+} from 'react-native-reanimated';
+
+import { AnimatedView } from './aliased';
+
+interface Props {
+	children: ReactNode;
+	style?: ViewStyle;
+	gestureEnable?: boolean;
+	callbackOnClose?: () => void;
+}
+
+type GestureFunc = (
+	event: GestureUpdateEvent<PanGestureHandlerEventPayload>,
+) => void;
+
+export const SwipeDownGesture: FC<Props> = ({
+	children,
+	style,
+	gestureEnable = false,
+	callbackOnClose,
+}) => {
+	const offset = useSharedValue(0);
+	const context = useSharedValue(0);
+	const modalHeight = useSharedValue(0);
+
+	const onContainerLayout: (event: LayoutChangeEvent) => void = ({
+		nativeEvent,
+	}) => {
+		modalHeight.value = nativeEvent.layout.height;
+	};
+
+	const onGestureStart = () => {
+		context.value = offset.value;
+	};
+
+	const onGestureUpdate: GestureFunc = (event) => {
+		const newPosition = event.translationY + context.value;
+		if (newPosition > 0) {
+			offset.value = newPosition;
+		}
+	};
+
+	const onGestureEnd: GestureFunc = (event) => {
+		const newPosition = event.translationY + context.value;
+		if (newPosition > 100) {
+			offset.value = withTiming(modalHeight.value);
+			callbackOnClose?.();
+		} else {
+			offset.value = withTiming(0);
+		}
+	};
+
+	const pan = Gesture.Pan()
+		.onStart(onGestureStart)
+		.onUpdate(onGestureUpdate)
+		.onEnd(onGestureEnd)
+		.enabled(gestureEnable);
+
+	const animatedStyle = useAnimatedStyle(() => {
+		return {
+			transform: [{ translateY: offset.value }],
+		};
+	}, [offset]);
+
+	return (
+		<GestureHandlerRootView>
+			<GestureDetector gesture={pan}>
+				<AnimatedView
+					style={[style, animatedStyle]}
+					onLayout={onContainerLayout}
+				>
+					{children}
+				</AnimatedView>
+			</GestureDetector>
+		</GestureHandlerRootView>
+	);
+};
+
+export default SwipeDownGesture;

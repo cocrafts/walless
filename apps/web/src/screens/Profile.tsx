10ef30baab438c75b2c58749508efb3ebc480f16
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-12-02 00:43:51 +0700

    ui: bring back setting util on extension

--- apps/web/src/screens/Profile.tsx
+++ apps/web/src/screens/Profile.tsx
@@ -1,4 +1,7 @@
+import { StyleSheet } from 'react-native';
 import { ProfileFeature } from '@walless/app';
+import { Hoverable, View } from '@walless/gui';
+import { Setting } from '@walless/icons';
 import { router } from 'utils/routing';
 
 export const ProfileScreen = () => {
@@ -11,11 +14,24 @@ export const ProfileScreen = () => {
 	};
 
 	return (
-		<ProfileFeature
-			onNavigateToHistory={handleNavigateToHistory}
-			onSettingPress={handleSettingPress}
-		/>
+		<View>
+			<Hoverable style={styles.settingContainer} onPress={handleSettingPress}>
+				<Setting size={16} />
+			</Hoverable>
+			<ProfileFeature onNavigateToHistory={handleNavigateToHistory} />
+		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	settingContainer: {
+		marginLeft: 'auto',
+		marginRight: 14,
+		marginTop: 14,
+		backgroundColor: '#25313D',
+		padding: 8,
+		borderRadius: 100,
+	},
+});
 
 export default ProfileScreen;

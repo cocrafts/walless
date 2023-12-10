93851927ec1255fb10a22814a5daa1c59fce75dd
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-12-10 16:30:35 +0700

    ui: re-skin recovery screen

--- packages/app/features/Recovery.tsx
+++ packages/app/features/Recovery.tsx
@@ -1,52 +1,52 @@
 import type { FC } from 'react';
-import { useRef } from 'react';
-import { Image, Linking, StyleSheet, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import type { ViewStyle } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import { Button, Input, Text, View } from '@walless/gui';
+import { modules } from '@walless/ioc';
 
 interface Props {
+	style?: ViewStyle;
 	onPressContinue: (key?: string) => void;
 }
 
-export const Recovery: FC<Props> = ({ onPressContinue }) => {
-	const recoveryKey = useRef<string>();
+export const Recovery: FC<Props> = ({ style, onPressContinue }) => {
+	const [recoveryKey, setRecoveryKey] = useState('');
 
-	const handleChangeText = (text: string) => {
-		recoveryKey.current = text;
-	};
-
-	const onLinkPress = async () => {
-		await Linking.openURL('https://discord.gg/3v7jwG45pe');
+	const handlePressContinue = () => {
+		onPressContinue(recoveryKey.trim());
 	};
 
 	return (
-		<View style={styles.container}>
-			<Image source={{ uri: '/img/bare-icon.png' }} style={styles.logo} />
+		<View style={[styles.container, style]}>
+			<Image source={modules.asset.misc.walless} style={styles.logo} />
 
 			<View style={styles.titleContainer}>
-				<Text style={styles.title}>Recovery account</Text>
+				<Text style={styles.title}>Recovery your account</Text>
 				<Text style={styles.subText}>
-					Please input your recovery key to sign-in
+					Enter your Secret key to get going again
 				</Text>
 			</View>
 
-			<View>
-				<Input style={styles.recoveryInput} onChangeText={handleChangeText} />
+			<View style={styles.lowerContainer}>
+				<Input
+					style={styles.inputText}
+					placeholder="Enter Secret key"
+					textAlign="center"
+					onChangeText={setRecoveryKey}
+				/>
 				<Button
+					style={[styles.continueButton]}
 					titleStyle={styles.continueButtonTitle}
 					title="Continue"
-					onPress={() => {
-						onPressContinue(recoveryKey.current?.trim());
-					}}
+					onPress={handlePressContinue}
+					disabled={!recoveryKey.trim()}
 				/>
-			</View>
-
-			<View style={styles.footerContainer}>
-				<Text>Having issue with passcode? </Text>
-				<TouchableOpacity onPress={onLinkPress}>
-					<View cursorPointer noSelect>
-						<Text style={styles.link}>Contact us</Text>
-					</View>
-				</TouchableOpacity>
+				<Text style={styles.reminderText}>
+					Upon sign-up, your Secret Key is sent in the Walless Emergency Kit to
+					your registered email. If forgotten, contact us at{' '}
+					<Text style={styles.email}>hello@walless.io</Text>
+				</Text>
 			</View>
 		</View>
 	);
@@ -57,39 +57,47 @@ export default Recovery;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		paddingHorizontal: 40,
+		alignItems: 'center',
+		paddingHorizontal: 36,
 		paddingTop: 50,
-		paddingBottom: 20,
+		gap: 40,
 	},
 	logo: {
 		width: 83,
 		height: 43,
-		marginHorizontal: 'auto',
 	},
 	titleContainer: {
-		paddingVertical: 40,
+		gap: 8,
 	},
 	title: {
-		paddingBottom: 10,
 		fontSize: 20,
+		fontWeight: '400',
+		color: '#fff',
 		textAlign: 'center',
 	},
 	subText: {
 		color: '#566674',
 		textAlign: 'center',
 	},
-	recoveryInput: {
-		marginBottom: 10,
+	lowerContainer: {
+		width: '100%',
+	},
+	inputText: {
+		textAlign: 'center',
 	},
 	continueButtonTitle: {
 		fontWeight: '600',
 	},
-	footerContainer: {
-		flexDirection: 'row',
-		marginTop: 'auto',
-		justifyContent: 'center',
+	continueButton: {
+		marginTop: 18,
+		marginBottom: 14,
 	},
-	link: {
+	reminderText: {
+		fontSize: 10,
+		color: '#566674',
+		textAlign: 'center',
+	},
+	email: {
 		color: '#19A3E1',
 	},
 });

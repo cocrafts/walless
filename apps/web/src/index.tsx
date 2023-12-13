72c2fec77a9374d11a16b27cc75c0d772293d47c
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-12-14 01:13:04 +0700

    chore: deprecate App component, move it to root level

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,12 +1,67 @@
+import type { FC } from 'react';
+import { useEffect, useRef } from 'react';
+import type { ViewStyle } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import App from 'components/App';
+import { RouterProvider } from 'react-router-dom';
+import { useResponsive } from '@walless/app';
+import { appState } from '@walless/engine';
+import { modalActions, ModalManager } from '@walless/gui';
+import SplashWrapper from 'components/Splash';
+import { router } from 'utils/routing';
+import { useSnapshot } from 'valtio';
 
-export const AppContainer = () => {
+interface Props {
+	width?: number;
+	height?: number;
+}
+
+const App: FC<Props> = ({ width = 410, height = 600 }) => {
+	const app = useSnapshot(appState);
+	const { isMobileResponsive } = useResponsive();
+	const containerRef = useRef<View>(null);
+	const containerStyle: ViewStyle = isMobileResponsive
+		? styles.container
+		: styles.wrappedContainer;
+	const wrappedAppStyle: ViewStyle = {
+		width,
+		maxHeight: height,
+		borderRadius: 8,
+		overflow: 'hidden',
+	};
+
+	useEffect(() => {
+		modalActions.setContainerRef(containerRef);
+	}, []);
+
 	return (
 		<SafeAreaProvider>
-			<App />
+			<View style={containerStyle}>
+				<View
+					ref={containerRef}
+					style={[styles.appContainer, !isMobileResponsive && wrappedAppStyle]}
+				>
+					{app.loading ? <SplashWrapper /> : <RouterProvider router={router} />}
+					<ModalManager />
+				</View>
+			</View>
 		</SafeAreaProvider>
 	);
 };
 
-export default AppContainer;
+export default App;
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	wrappedContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	appContainer: {
+		flex: 1,
+		backgroundColor: '#19232c',
+	},
+});

78094cb487c3f62a57148ddee1fd22e43ea7d689
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-12-14 00:29:49 +0700

    feat: useLazyGridLayout effect for reponsive grid layout

--- packages/app/utils/hooks/app.ts
+++ packages/app/utils/hooks/app.ts
@@ -1,15 +1,17 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
+import type { LayoutChangeEvent } from 'react-native';
 import { Platform } from 'react-native';
 import type { EdgeInsets } from 'react-native-safe-area-context';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { dimensionState } from '@walless/gui';
+import { useDebouncedCallback } from 'use-debounce';
 import { useSnapshot } from 'valtio';
 
 export const useResponsive = () => {
 	const { isMobile: isMobileDevice, windowSize } = useSnapshot(dimensionState);
 
 	const isMobileResponsive = useMemo(() => {
-		return windowSize.width < 600;
+		return windowSize.width < 1600;
 	}, [isMobileDevice, windowSize.width]);
 
 	return {
@@ -36,4 +38,38 @@ export const useUniversalInsets = (): EdgeInsets => {
 	}
 
 	return insets;
+};
+
+export interface GridLayoutOptions {
+	referenceWidth: number;
+	heightRatio?: number;
+	gap: number;
+}
+
+export const useLazyGridLayout = ({
+	referenceWidth = 160,
+	heightRatio = 1,
+	gap,
+}: GridLayoutOptions) => {
+	const [width, setWidth] = useState<number>(0);
+	const onGridContainerLayout = useDebouncedCallback(
+		({ nativeEvent }: LayoutChangeEvent) => {
+			const extendedContainerWidth = nativeEvent.layout.width + gap;
+			const extendedItemWidth = referenceWidth + gap;
+			const itemCount = Math.floor(extendedContainerWidth / extendedItemWidth);
+			const extraWidth = extendedContainerWidth % extendedItemWidth;
+			const nextWidth = referenceWidth + Math.floor(extraWidth / itemCount);
+
+			if (nextWidth !== width) {
+				setWidth(nextWidth);
+			}
+		},
+		200,
+	);
+
+	return {
+		onGridContainerLayout,
+		width,
+		height: width * heightRatio,
+	};
 };

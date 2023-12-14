40d910674b5bbdb8ba4f451c1fc8ee74ca7665fc
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-12-14 21:00:31 +0700

    chore: prevent selection in DashboardNavigator

--- packages/app/components/DashboardNavigator/index.tsx
+++ packages/app/components/DashboardNavigator/index.tsx
@@ -111,9 +111,10 @@ export default DashboardNavigator;
 
 const styles = StyleSheet.create({
 	container: {
+		flex: 1,
 		paddingVertical: 12,
 		backgroundColor: '#131C24',
-		height: '100%',
+		userSelect: 'none',
 	},
 	orbContainer: {
 		flex: 1,

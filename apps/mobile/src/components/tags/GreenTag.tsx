d8794543bfc247b3e24bd2dc03dfdbda0b0ab215
Tan Le <tanlethanh8266@gmail.com>
2024-01-04 19:34:12 +0700

    chore: move transaction result screen to app

---
+++ apps/mobile/src/components/tags/GreenTag.tsx
@@ -0,0 +1,31 @@
+import type { FC, ReactNode } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text, View } from '@walless/gui';
+
+interface Props {
+	title: string;
+	prefix?: ReactNode;
+}
+
+export const GreenTag: FC<Props> = ({ title, prefix }) => {
+	return (
+		<View style={styles.container}>
+			{prefix}
+			<Text style={styles.innerText}>{title}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		borderRadius: 10,
+		paddingHorizontal: 10,
+		paddingVertical: 5,
+		alignItems: 'center',
+		gap: 6,
+		backgroundColor: '#13241D',
+	},
+	innerText: {
+		color: '#60C591',
+	},
+});

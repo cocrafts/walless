a44e9fb963cafc4a183ff1d1cd4c01d5efa8b85a
Tan Le <tanlethanh8266@gmail.com>
2024-01-17 19:44:20 +0700

    feat: query solana tokens when starting engine

--- apps/wallet/src/engine/runners/solana/index.ts
+++ apps/wallet/src/engine/runners/solana/index.ts
@@ -1,8 +1,11 @@
-import { clusterApiUrl, Connection } from '@solana/web3.js';
+import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
 import { Networks } from '@walless/core';
+import { type PublicKeyDocument, selectors } from '@walless/store';
+import { addTokensToStorage, storage } from 'utils/storage';
 
-import type { EngineConfig, Runner } from '../../types';
+import type { CreateFunction } from '../../types';
 
+import { getTokenDocumentsOnChain } from './tokens';
 import type { SolanaContext } from './types';
 
 const endpointUrl: Record<string, string> = {
@@ -11,18 +14,34 @@ const endpointUrl: Record<string, string> = {
 	mainnet: SOLANA_CLUSTER_URL,
 };
 
-export const createSolanaRunner = (config: EngineConfig): Runner => {
+export const createSolanaRunner: CreateFunction = async (config) => {
 	const { endpoints } = config;
-
-	const connection = new Connection(endpointUrl[endpoints[Networks.solana]]);
+	const endpoint = endpoints[Networks.solana];
+	const connection = new Connection(endpointUrl[endpoint]);
+	const keys = (await storage.find<PublicKeyDocument>(selectors.solanaKeys))
+		.docs;
 
 	return {
-		start() {},
-		stop() {},
+		start: async () => {
+			const promises = keys.map((key) => {
+				const address = key._id;
+				const walletPublicKey = new PublicKey(address);
+				return [
+					getTokenDocumentsOnChain(connection, endpoint, walletPublicKey).then(
+						(tokens) => {
+							addTokensToStorage(tokens);
+						},
+					),
+				];
+			});
+
+			await Promise.all(promises);
+		},
+		stop: async () => {},
+		restart: async () => {},
 		getContext: (): SolanaContext => {
 			return { connection };
 		},
-		restart: () => {},
 	};
 };
 

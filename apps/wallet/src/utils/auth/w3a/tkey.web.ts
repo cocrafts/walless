4654068f875562b2f279b077895dca465e0ac4e0
Tan Le <tanlethanh8266@gmail.com>
2024-01-17 15:01:07 +0700

    fix: construct threshold key on web

--- apps/wallet/src/utils/auth/w3a/tkey.web.ts
+++ apps/wallet/src/utils/auth/w3a/tkey.web.ts
@@ -1,10 +1,11 @@
 import type { ShareStore } from '@tkey/common-types';
-import type { WebStorageModule } from '@tkey/web-storage';
+import ThresholdKey from '@tkey/default';
+import { WebStorageModule } from '@tkey/web-storage';
 import type { CustomAuthArgs } from '@toruslabs/customauth';
 import CustomAuthCore from '@toruslabs/customauth';
 import { w3aBaseUrl } from 'utils/config/index.web';
 
-import type { CoreModules, CoreThresholdKey } from './core';
+import { type CoreModules, coreModules, type CoreThresholdKey } from './core';
 
 type WebTkeyModules = CoreModules & {
 	webStorage: WebStorageModule;
@@ -27,7 +28,15 @@ export const customAuthArgs: CustomAuthArgs = {
 export const CustomAuth = new CustomAuthCore(customAuthArgs);
 
 export const initTkey = (): WebTkey => {
-	return {} as WebTkey;
+	return new ThresholdKey({
+		customAuthArgs,
+		manualSync: true,
+		enableLogging: true,
+		modules: {
+			...coreModules,
+			webStorage: new WebStorageModule(),
+		},
+	}) as WebTkey;
 };
 
 export const storeDeviceShare = async (tkey: WebTkey, share: ShareStore) => {

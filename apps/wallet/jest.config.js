377fc5018b586882849242a1df06212fb8599834
Tan Le <tanlethanh8266@gmail.com>
2024-01-19 15:05:40 +0700

    fix: resolve custom exported packages in jest config

--- apps/wallet/jest.config.js
+++ apps/wallet/jest.config.js
@@ -5,5 +5,12 @@ module.exports = {
 	moduleNameMapper: {
 		// Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
 		uuid: require.resolve('uuid'),
+		'@solana/codecs-data-structures': require.resolve(
+			'@solana/codecs-data-structures',
+		),
+		'@solana/codecs-core': require.resolve('@solana/codecs-core'),
+		'@solana/codecs-numbers': require.resolve('@solana/codecs-numbers'),
+		'@solana/codecs-strings': require.resolve('@solana/codecs-strings'),
+		'@solana/options': require.resolve('@solana/options'),
 	},
 };

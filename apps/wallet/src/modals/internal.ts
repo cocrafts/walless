cbfa02ead4052200abdb1a2671b1986f2f664e52
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-01-24 12:19:18 +0700

    refactor: remove unused style

--- apps/wallet/src/modals/internal.ts
+++ apps/wallet/src/modals/internal.ts
@@ -1,5 +1,3 @@
-import { StyleSheet } from 'react-native';
-
 export enum ModalId {
 	Error = 'Error',
 	Logout = 'Logout',
@@ -10,16 +8,3 @@ export enum ModalId {
 	Send = 'Send',
 	TransactionDetails = 'TransactionDetails',
 }
-
-export const modalStyles = StyleSheet.create({
-	wrapper: {
-		backgroundColor: '#141B21',
-		justifyContent: 'space-between',
-		gap: 60,
-		borderTopRightRadius: 20,
-		borderTopLeftRadius: 20,
-		paddingTop: 10,
-		paddingBottom: 40,
-		paddingHorizontal: 28,
-	},
-});

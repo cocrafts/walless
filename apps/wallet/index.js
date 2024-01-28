59d11e5ce71b4d27e6cb590e14473d2dfad7717b
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-01-29 06:16:04 +0700

    feat: use messaging module in Mobile build

--- apps/wallet/index.js
+++ apps/wallet/index.js
@@ -2,7 +2,9 @@ require('utils/shim');
 
 import { AppRegistry } from 'react-native';
 
+import { initializeMessaging } from './browser/kernel/messaging';
 import { name as appName } from './app.json';
 import App from './src';
 
+initializeMessaging();
 AppRegistry.registerComponent(appName, () => App);

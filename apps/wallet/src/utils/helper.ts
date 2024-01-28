8e26427f65c49168bd4d00ba57030248cd54c10a
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-01-29 06:16:32 +0700

    feat: url helper

--- apps/wallet/src/utils/helper.ts
+++ apps/wallet/src/utils/helper.ts
@@ -37,3 +37,31 @@ export const getNetworkInfo = (network: Networks): NetworkInfo | undefined => {
 			};
 	}
 };
+
+export const isValidUrl = (str: string) => {
+	const pattern = new RegExp(
+		'^([a-zA-Z]+:\\/\\/)?' + // protocol
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+			'(\\#[-a-z\\d_]*)?$', // fragment locator
+		'i',
+	);
+
+	return pattern.test(str);
+};
+
+export const isValidHttpUrl = (str: string) => {
+	const pattern = new RegExp(
+		'^(https?:\\/\\/)' + // protocol
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+			'(\\#[-a-z\\d_]*)?$', // fragment locator
+		'i',
+	);
+
+	return pattern.test(str);
+};

8904097c115a4c05ef607423f60acc34f143c4df
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-01-30 14:16:52 +0700

    chore: fix import

--- apps/wallet/src/stacks/Explorer/WidgetNavigator/index.tsx
+++ apps/wallet/src/stacks/Explorer/WidgetNavigator/index.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import type { FC } from 'react';
 import type { StyleProp, ViewStyle } from 'react-native';
 import { StyleSheet, View } from 'react-native';
 import type { UserProfile } from '@walless/core';

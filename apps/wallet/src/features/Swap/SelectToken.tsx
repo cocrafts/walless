dffa26639f44f3161068618bb4f4a85fc1f6230f
Tan Le <tanlethanh8266@gmail.com>
2024-02-01 00:12:50 +0700

    feat: select token in swap

---
+++ apps/wallet/src/features/Swap/SelectToken.tsx
@@ -0,0 +1,58 @@
+import { type FC, Fragment } from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { Text, View } from '@walless/gui';
+import { ChevronDown } from '@walless/icons';
+import type { TokenDocument } from '@walless/store';
+
+type Props = {
+	token?: TokenDocument;
+};
+
+const SelectToken: FC<Props> = ({ token }) => {
+	return (
+		<View style={styles.container}>
+			{token ? (
+				<Fragment>
+					<Image
+						style={styles.icon}
+						source={{ uri: token.metadata?.imageUri }}
+					/>
+					<Text style={styles.text}>{token.metadata?.name}</Text>
+				</Fragment>
+			) : (
+				<View style={styles.emptyContainer}>
+					<Text style={styles.text}>Select token</Text>
+				</View>
+			)}
+			<ChevronDown size={16} color="#566674" />
+		</View>
+	);
+};
+
+export default SelectToken;
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		backgroundColor: '#313F4A',
+		alignItems: 'center',
+		gap: 8,
+		padding: 6,
+		borderRadius: 20,
+		alignSelf: 'flex-start',
+	},
+	icon: {
+		height: 26,
+		width: 26,
+		borderRadius: 13,
+	},
+	text: {
+		fontSize: 16,
+		color: '#FFFFFF',
+	},
+	emptyContainer: {
+		height: 26,
+		paddingLeft: 10,
+		justifyContent: 'center',
+	},
+});

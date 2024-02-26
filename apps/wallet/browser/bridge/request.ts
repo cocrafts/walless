559939d9c4483b6db5328f02f1d641a4b121f8d4
Tan Le <tanlethanh8266@gmail.com>
2024-02-26 16:27:46 +0700

    feat: add popup request handles utils

--- apps/wallet/browser/bridge/request.ts
+++ apps/wallet/browser/bridge/request.ts
@@ -1,16 +1,75 @@
-import { RequestType, Timeout } from '@walless/core';
+import { PopupType, RequestType, ResponseCode, Timeout } from '@walless/core';
 import type { PureMessagePayload } from '@walless/messaging';
 import { Channels } from '@walless/messaging';
 import * as bs58 from 'bs58';
 
-import type { PopupPayload } from './utils';
+import type { PayloadOptions, PopupPayload } from './utils';
 import { encryptedMessenger } from './utils';
 
 export const sendRequest = async (payload: PureMessagePayload) => {
 	return await encryptedMessenger.request('kernel', payload);
 };
 
-export const findRequest = async (requestId: string, from: string) => {
+export const handleRequestConnect = async (
+	requestId: string,
+	isApproved: boolean,
+) => {
+	const payload: PopupPayload = {
+		from: PopupType.REQUEST_CONNECT_POPUP,
+		type: RequestType.REQUEST_CONNECT,
+		sourceRequestId: requestId,
+		isApproved,
+	};
+
+	try {
+		const res = await encryptedMessenger.request(Channels.kernel, payload);
+		if (res.responseCode === ResponseCode.SUCCESS) {
+			window.close();
+		}
+	} catch (error) {
+		throw Error('Unable to handle connect request');
+	}
+};
+
+export const handleRequestInstallLayout = async (
+	requestId: string,
+	isApproved: boolean,
+) => {
+	const payload: PopupPayload = {
+		from: PopupType.REQUEST_INSTALL_LAYOUT_POPUP,
+		type: RequestType.INSTALL_LAYOUT,
+		sourceRequestId: requestId,
+		isApproved,
+	};
+
+	try {
+		return await encryptedMessenger.request(Channels.kernel, payload, 10000);
+	} catch (error) {
+		throw Error('Not successfully install layout');
+	}
+};
+
+export const handleRequestSignature = async (
+	options: PayloadOptions,
+	type: RequestType,
+) => {
+	const payload: PopupPayload = {
+		from: PopupType.SIGNATURE_POPUP,
+		type,
+		...options,
+	};
+
+	try {
+		return await encryptedMessenger.request(Channels.kernel, payload, 10000);
+	} catch (error) {
+		throw Error('Unable to handle sign message request');
+	}
+};
+
+export const getDataFromSourceRequest = async (
+	requestId: string,
+	from: string,
+) => {
 	const payload: PopupPayload = {
 		from,
 		type: RequestType.REQUEST_PAYLOAD,
@@ -25,7 +84,13 @@ export const findRequest = async (requestId: string, from: string) => {
 		);
 
 		if ('message' in res) {
-			const displayMessage = new TextDecoder().decode(bs58.decode(res.message));
+			let displayMessage;
+			try {
+				displayMessage = new TextDecoder().decode(bs58.decode(res.message));
+			} catch {
+				displayMessage = res.message;
+			}
+
 			return {
 				...res,
 				message: displayMessage,

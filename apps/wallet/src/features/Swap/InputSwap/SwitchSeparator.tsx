ca182f635e80accd44834a1525569a6f4a3cd59a
Tan Le <tanlethanh8266@gmail.com>
2024-02-28 18:41:56 +0700

    feat: implement switch button

--- apps/wallet/src/features/Swap/InputSwap/SwitchSeparator.tsx
+++ apps/wallet/src/features/Swap/InputSwap/SwitchSeparator.tsx
@@ -2,14 +2,28 @@ import { StyleSheet } from 'react-native';
 import { Hoverable, View } from '@walless/gui';
 import { Switch } from '@walless/icons';
 import { showError } from 'modals/Error';
+import { useJupiterContext, useSnapshot, useTokens } from 'utils/hooks';
 
-import { swapContext } from '../context';
+import { swapActions, swapContext } from '../context';
 
 const SwitchSeparator = () => {
+	const { network } = useSnapshot(swapContext).swap;
+	const { tokens } = useTokens(network);
+	const { tokens: JupTokens } = useJupiterContext();
+
 	const handleSwitch = () => {
 		if (!swapContext.swap.fromToken || !swapContext.swap.toToken) {
 			showError({ errorText: 'Please select tokens to swap' });
 		}
+		const newFromToken = tokens.find(
+			(t) => t.account.mint === swapContext.swap.toToken?.address,
+		);
+		const newToToken = JupTokens.find(
+			(t) => t.address === swapContext.swap.fromToken?.account.mint,
+		);
+		if (!newFromToken || !newFromToken) return;
+
+		swapActions.update({ fromToken: newFromToken, toToken: newToToken });
 	};
 
 	return (

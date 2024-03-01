c37de9482b47df1415e1710da0f2f88a7f5d862a
ltminhthu <ltminhthu.209@gmail.com>
2024-03-01 17:46:52 +0700

    add new ui of referral screen

--- apps/wallet/src/screens/Dashboard/Referral/index.tsx
+++ apps/wallet/src/screens/Dashboard/Referral/index.tsx
@@ -1,166 +1,83 @@
-import { type FC, useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import type { FC } from 'react';
+import { ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import type { StackScreenProps } from '@react-navigation/stack';
-import type { ReferralRankingRecord, WalletInvitation } from '@walless/graphql';
-import { queries } from '@walless/graphql';
-import type { SlideOption } from '@walless/gui';
-import { Slider, SliderTabs, Text, View } from '@walless/gui';
-import type {
-	TabAble,
-	TabItemStyle,
-} from '@walless/gui/components/SliderTabs/TabItem';
-import { qlClient } from 'utils/graphql';
+import { Text, View } from '@walless/gui';
+import { ArrowTopRight, Chart, Star } from '@walless/icons';
 import type { SettingParamList } from 'utils/navigation';
 
 import DetailsContainer from './DetailsContainer';
-import InviteFriendsTab from './InviteFriendsTab';
-import LeaderboardTab from './Leaderboard';
+import { mockNewInvitation } from './internal';
+import InvitationCard from './InvitationCard';
 import SuccessfulReferral from './SuccessfulReferral';
 
 type Props = StackScreenProps<SettingParamList, 'Referral'>;
 
 export const ReferralScreen: FC<Props> = () => {
-	const [activeIndex, setActiveIndex] = useState(0);
-	const [claimedReferrals, setClaimedReferrals] = useState<WalletInvitation[]>(
-		[],
+	const totalPoints = 200;
+	const rankingPercent = 10;
+	const currentPoints = 20;
+
+	const chartIcon = (
+		<View style={[styles.chartIcon, styles.icon]}>
+			<Chart size={20} color="#F6D570" />
+		</View>
 	);
-	const [unclaimedReferrals, setUnclaimedReferrals] = useState<
-		WalletInvitation[]
-	>([]);
-	const [leaderboard, setLeaderboard] = useState<ReferralRankingRecord[]>([]);
 
-	useEffect(() => {
-		const fetchClaimedReferrals = async () => {
-			const { claimedWalletInvitations } = await qlClient.request<{
-				claimedWalletInvitations: WalletInvitation[];
-			}>(queries.claimedInvitations);
+	const starIcon = (
+		<View style={[styles.starIcon, styles.icon]}>
+			<Star size={20} color="#44C5FF" />
+		</View>
+	);
 
-			setClaimedReferrals(claimedWalletInvitations);
-		};
-
-		const fetchUnclaimedReferrals = async () => {
-			const { unclaimedWalletInvitations } = await qlClient.request<{
-				unclaimedWalletInvitations: WalletInvitation[];
-			}>(queries.unclaimedInvitations);
-
-			setUnclaimedReferrals(unclaimedWalletInvitations);
-		};
-
-		const fetchLeaderboard = async () => {
-			const { referralLeaderboard } = await qlClient.request<{
-				referralLeaderboard: ReferralRankingRecord[];
-			}>(queries.referralLeaderboard);
-			setLeaderboard(referralLeaderboard);
-		};
-
-		Promise.all([
-			fetchClaimedReferrals(),
-			fetchUnclaimedReferrals(),
-			fetchLeaderboard(),
-		]);
-	}, []);
-
-	const accountId = unclaimedReferrals?.[0]?.referrerId;
-
-	const myRank = leaderboard.find((record) => record.id === accountId)?.rank;
-
-	const currentLevelReferralCount = 3;
-
-	const sliderItems: SlideOption[] = [
-		{
-			id: 'inviteFriends',
-			component: () => (
-				<InviteFriendsTab
-					claimedReferrals={claimedReferrals.toReversed()}
-					unclaimedReferrals={unclaimedReferrals}
-				/>
-			),
-		},
-		{
-			id: 'leaderboard',
-			component: () => (
-				<LeaderboardTab accountId={accountId} leaderboard={leaderboard} />
-			),
-		},
-	];
-
-	const tabItems: TabAble[] = [
-		{
-			id: 'inviteFriends',
-			title: 'Invite Friends',
-		},
-		{
-			id: 'leaderboard',
-			title: 'Leaderboard',
-		},
-	];
-
-	const activatedStyle: TabItemStyle = {
-		containerStyle: {
-			borderBottomWidth: 1,
-			borderBottomColor: '#ffffff',
-			borderRadius: 0,
-		},
-		textStyle: {
-			color: '#ffffff',
-			fontWeight: '500',
-		},
-	};
-
-	const deactivatedStyle: TabItemStyle = {
-		containerStyle: {
-			backgroundColor: 'transparent',
-		},
-		textStyle: {
-			color: '#566674',
-			fontWeight: '500',
-		},
-	};
-
-	const handleTabPress = (item: TabAble) => {
-		const idx = tabItems.indexOf(item);
-		setActiveIndex(idx);
-	};
+	const arrowIcon = (
+		<TouchableOpacity style={styles.arrowIcon}>
+			<ArrowTopRight size={20} color="#FFFFFF" />
+		</TouchableOpacity>
+	);
 
 	return (
-		<View style={[styles.container]}>
-			<Text style={styles.title}>Be an Influencer</Text>
-			<Text style={styles.subtext}>
-				Lorem ipsum dolor sit amet consectetur.
-			</Text>
-
-			<View style={styles.headerContainer}>
-				<View style={styles.subpartContainer}>
-					<DetailsContainer
-						title="Rank"
-						value={myRank ? `#${myRank}` : 'N/A'}
-					/>
-
-					<DetailsContainer title="Points from friends" value="coming soon" />
-				</View>
-
+		<ScrollView showsVerticalScrollIndicator={false} style={[styles.container]}>
+			<View style={styles.summaryContainer}>
 				<SuccessfulReferral
 					{...{
-						numberOfActivatedReferrals: claimedReferrals.length,
-						numberOfReferrals: currentLevelReferralCount,
+						currentPoints: currentPoints,
+						goalPoint: 60,
 					}}
 				/>
+				<View style={styles.infoDetailsContainer}>
+					<DetailsContainer
+						LeftIcon={chartIcon}
+						title="You are in"
+						value={`Top ${rankingPercent}%`}
+						RightIcon={arrowIcon}
+					/>
+					<DetailsContainer
+						LeftIcon={starIcon}
+						title="Your Point"
+						value={totalPoints.toString()}
+					/>
+				</View>
 			</View>
 
-			<SliderTabs
-				items={tabItems}
-				activeItem={tabItems[activeIndex]}
-				activatedStyle={activatedStyle}
-				deactivatedStyle={deactivatedStyle}
-				onTabPress={handleTabPress}
-			/>
-
-			<Slider
-				style={styles.slider}
-				items={sliderItems}
-				activeItem={sliderItems[activeIndex]}
-			/>
-		</View>
+			<View style={styles.referralCodeContainer}>
+				<View style={styles.titleContainer}>
+					<Text style={styles.title}>Spread the fun!</Text>
+					<Text style={styles.subtext}>
+						Let&apos;s ride this wave together with your pals{' '}
+					</Text>
+				</View>
+				<View style={styles.referralCodeList}>
+					{mockNewInvitation.map((invitation) => (
+						<InvitationCard
+							key={invitation}
+							invitation={invitation}
+							isReadyToCollect={true}
+							points={20}
+						/>
+					))}
+				</View>
+			</View>
+		</ScrollView>
 	);
 };
 
@@ -169,9 +86,6 @@ export default ReferralScreen;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		paddingHorizontal: 14,
-		paddingTop: 16,
-		paddingBottom: 36,
 		gap: 12,
 	},
 	title: {
@@ -181,18 +95,48 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 	},
 	subtext: {
-		color: '#ffffff',
+		color: '#566674',
 		textAlign: 'center',
 	},
-	headerContainer: {
+	summaryContainer: {
+		backgroundColor: '#131C24',
+		borderRadius: 16,
+		padding: 16,
 		gap: 12,
+		marginHorizontal: 12,
+		marginBottom: 12,
 	},
-	subpartContainer: {
+	infoDetailsContainer: {
 		flexDirection: 'row',
 		gap: 12,
 	},
-	slider: {
+	chartIcon: {
+		borderColor: '#F6D57033',
+	},
+	starIcon: {
+		borderColor: '#44C5FF33',
+	},
+	arrowIcon: {
+		backgroundColor: '#1F2A34',
+		padding: 8,
+		borderRadius: 50,
+	},
+	icon: {
+		padding: 8,
+		borderRadius: 50,
+		borderWidth: 1,
+	},
+	referralCodeContainer: {
+		backgroundColor: '#131C24',
+		borderRadius: 16,
+		padding: 16,
+	},
+	referralCodeList: {
 		flex: 1,
-		padding: 20,
+		gap: 12,
+	},
+	titleContainer: {
+		gap: 12,
+		marginBottom: 12,
 	},
 });

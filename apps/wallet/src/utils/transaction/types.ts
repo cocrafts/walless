d5a5733277e5eb70a214af79c3a231274bac3678
Tan Le <tanlethanh8266@gmail.com>
2024-03-08 17:25:50 +0700

    feat: define unversal transaction with gasilon

---
+++ apps/wallet/src/utils/transaction/types.ts
@@ -0,0 +1,39 @@
+import type {
+	Networks,
+	NftV2,
+	SolanaCollectible,
+	SolanaToken,
+	TokenV2,
+} from '@walless/core';
+import type { NftDocumentV2, TokenDocumentV2 } from '@walless/store';
+
+export type SendTransaction = {
+	type: 'token' | 'nft';
+	network: Networks;
+	sender: string;
+	receiver: string;
+	amount: number;
+};
+
+export type SendTokenTransaction<T extends TokenV2 = TokenV2> =
+	SendTransaction & {
+		token: TokenDocumentV2<T>;
+	};
+
+export type SendNftTransaction<N extends NftV2 = NftV2> = SendTransaction & {
+	nft: NftDocumentV2<N>;
+};
+
+export type GasilonSupportedTransaction<T extends TokenV2 = TokenV2> = {
+	fee?: number;
+	tokenForFee: TokenDocumentV2<T> | null;
+};
+
+export type SolanaSendTransaction = SendTransaction &
+	GasilonSupportedTransaction<SolanaToken>;
+
+export type SolanaSendTokenTransaction = SendTokenTransaction<SolanaToken> &
+	GasilonSupportedTransaction<SolanaToken>;
+
+export type SolanaSendNftTransaction = SendNftTransaction<SolanaCollectible> &
+	GasilonSupportedTransaction<SolanaToken>;

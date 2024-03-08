4a9fae82502ff9d68bea8bfd100c0114e51fcd0a
FME849 <1414439@gmail.com>
2024-03-08 17:30:59 +0700

    update: separate history item UI

--- apps/wallet/src/features/History/HistoryItem/index.tsx
+++ apps/wallet/src/features/History/HistoryItem/index.tsx
@@ -1,87 +1,50 @@
 import type { FC } from 'react';
-import type { ImageURISource } from 'react-native';
-import { StyleSheet, View } from 'react-native';
-import type { TransactionHistory } from '@walless/core';
-import { Hoverable } from '@walless/gui';
-import { showTransactionDetailsModal } from 'modals/TransactionDetailsModal';
-import assets from 'utils/assets';
-import { getNetworkInfo } from 'utils/helper';
+import { View } from 'react-native';
+import type {
+	SolanaSwapHistoryV2,
+	SolanaTransferHistoryV2,
+	SolanaUnknownHistory,
+} from '@walless/core';
+import { Networks } from '@walless/core';
+import type { TransactionHistoryDocument } from '@walless/store';
 
-import ItemAddress from './ItemAddress';
-import ItemAmount from './ItemAmount';
-import ItemTokenIcon from './ItemTokenIcon';
+import SolanaSwapHistoryItem from './SolanaSwap';
+import SolanaTransferHistoryItem from './SolanaTransfer';
 
-const HistoryItem: FC<TransactionHistory> = (transaction) => {
-	const { transactionType, amount, network, sender, receiver, status, token } =
-		transaction;
-	const networkInfo = getNetworkInfo(network);
-	const address = transactionType === 'Sent' ? receiver : sender;
+interface Props {
+	transaction: TransactionHistoryDocument;
+}
 
-	const icon = token.metadata?.image
-		? { uri: token.metadata.image }
-		: assets.misc.unknownToken;
+export const HistoryItem: FC<Props> = ({ transaction }) => {
+	const isSolana = transaction.network === Networks.solana;
+	if (isSolana) {
+		const { transactionType } = transaction as TransactionHistoryDocument<
+			SolanaTransferHistoryV2 | SolanaSwapHistoryV2 | SolanaUnknownHistory
+		>;
 
-	return (
-		<Hoverable
-			style={styles.container}
-			onPress={() => showTransactionDetailsModal({ transaction })}
-		>
-			<View style={styles.contentContainer}>
-				<View style={styles.leftPartContainer}>
-					<ItemTokenIcon
-						type={transactionType}
-						status={status}
-						icon={icon}
-						isCollectible={!!token.metadata?.mpl}
-					/>
-					<ItemAddress
-						type={transactionType}
-						address={address}
-						imageUri={networkInfo?.icon as ImageURISource}
-					/>
-				</View>
-
-				<ItemAmount
-					amount={amount}
-					type={transactionType}
-					status={status}
-					tokenSymbol={token.metadata?.symbol}
+		if (transactionType === 'Unknown') {
+			return <View style={{ height: 50, backgroundColor: 'blue' }}></View>;
+		} else if (transactionType === 'Swap') {
+			console.log(JSON.stringify(transaction, null, 2));
+			return (
+				<SolanaSwapHistoryItem
+					transaction={
+						transaction as TransactionHistoryDocument<SolanaSwapHistoryV2>
+					}
 				/>
-			</View>
-		</Hoverable>
-	);
+			);
+		} else {
+			return (
+				<SolanaTransferHistoryItem
+					transaction={
+						transaction as TransactionHistoryDocument<SolanaTransferHistoryV2>
+					}
+				/>
+			);
+		}
+	} else {
+		return <View></View>;
+	}
 };
 
 export default HistoryItem;
-
-const styles = StyleSheet.create({
-	container: {
-		flexGrow: 1,
-		flexDirection: 'row',
-		overflow: 'hidden',
-	},
-	contentContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		width: '100%',
-		backgroundColor: '#131C24',
-		paddingVertical: 10,
-		paddingHorizontal: 12,
-		borderRadius: 8,
-	},
-	leftPartContainer: {
-		width: '70%',
-		flexDirection: 'row',
-		alignItems: 'center',
-		gap: 8,
-	},
-	balance: {
-		textAlign: 'right',
-	},
-	networkIcon: {
-		width: 16,
-		height: 16,
-		borderRadius: 4,
-	},
-});

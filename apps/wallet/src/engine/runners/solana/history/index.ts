a65730621b2c32c0be5bfd8385d066c376332076
FME849 <1414439@gmail.com>
2024-03-12 18:22:29 +0700

    refactor: refactor history with new type

---
+++ apps/wallet/src/engine/runners/solana/history/index.ts
@@ -0,0 +1,428 @@
+import type {
+	Connection,
+	ParsedInstruction,
+	ParsedTransaction,
+	ParsedTransactionMeta,
+	ParsedTransactionWithMeta,
+	TokenBalance,
+} from '@solana/web3.js';
+import type { PublicKey } from '@solana/web3.js';
+import type {
+	SolanaSwapHistory,
+	SolanaSwapHistoryV2,
+	SolanaToken,
+	SolanaTransferHistory,
+	SolanaTransferHistoryV2,
+	SolanaUnknownHistory,
+} from '@walless/core';
+import { Networks } from '@walless/core';
+import type { TokenDocument, TransactionHistoryDocument } from '@walless/store';
+import { selectors } from '@walless/store';
+import { solMint } from 'utils/constants';
+import { storage } from 'utils/storage';
+
+import { throttle } from '../internal';
+import { getTokenMetadata, solMetadata } from '../metadata';
+import type {
+	AccountBalanceMap,
+	ParsedTokenAccountWithAddress,
+	TransactionBalances,
+} from '../types';
+
+import { extractSwapBalance, JUPITER_V6_PROGRAM_ID } from './swapHistory';
+
+const historyLimit = 20;
+
+export const getTransactionsHistory = async (
+	connection: Connection,
+	wallet: PublicKey,
+	accounts: ParsedTokenAccountWithAddress[],
+) => {
+	const tokenAccounts = accounts
+		.filter((account) => account.tokenAmount.decimals > 0)
+		.map((account) => account.publicKey);
+
+	tokenAccounts.unshift(wallet);
+
+	const signatures = await getTransactionSignatures(connection, tokenAccounts);
+	signatures.splice(historyLimit, signatures.length - historyLimit);
+
+	const txPromises = signatures.map(async (s) => {
+		return throttle(async () => {
+			// don't use getParsedTransactions because
+			// it uses batch rpc request failed by rpc limit without retrying
+			// we need to use getParsedTransaction for separately retry
+			const tx = await connection.getParsedTransaction(s, {
+				maxSupportedTransactionVersion: 0,
+				commitment: 'finalized',
+			});
+
+			const txDoc = await constructTransactionHistoryDocument(
+				connection,
+				tx as ParsedTransactionWithMeta,
+				wallet,
+			);
+
+			if (!txDoc) return;
+			await storage.upsert<TransactionHistoryDocument>(txDoc._id, async () => {
+				return txDoc;
+			});
+		})();
+	});
+
+	await Promise.all(txPromises);
+
+	removeOldHistories();
+};
+
+const getTransactionSignatures = async (
+	connection: Connection,
+	accounts: PublicKey[],
+) => {
+	const signatures = (
+		await Promise.all(
+			accounts.map(async (account) => {
+				const sigs = await throttle(() => {
+					return connection.getSignaturesForAddress(account, {
+						limit: historyLimit,
+					});
+				})();
+
+				return sigs;
+			}),
+		)
+	)
+		.flat()
+		.sort((sig1, sig2) =>
+			(sig1?.blockTime || 0) < (sig2?.blockTime || 0) ? 1 : -1,
+		)
+		.map((s) => s.signature);
+
+	return [...new Set(signatures)];
+};
+
+const constructTransactionHistoryDocument = async (
+	connection: Connection,
+	parsedTransaction: ParsedTransactionWithMeta,
+	wallet: PublicKey,
+): Promise<
+	TransactionHistoryDocument<
+		SolanaTransferHistoryV2 | SolanaSwapHistoryV2 | SolanaUnknownHistory
+	>
+> => {
+	const { meta, transaction, blockTime } = parsedTransaction;
+	const walletAddress = wallet.toString();
+
+	const _id = `${walletAddress}/history/${transaction.signatures[0]}`;
+	const signature = transaction.signatures[0];
+	const id = signature;
+	const date = blockTime ? new Date(blockTime * 1000) : new Date();
+	const status = !meta?.err ? 'Success' : 'Failed';
+	const tokenForFee = getTransactionTokenFee();
+	const fee = (meta?.fee || 0) / 10 ** 9;
+	const balances = await getTransactionBalances(
+		connection,
+		parsedTransaction,
+		walletAddress,
+	);
+
+	if (!tokenForFee || balances.transactionType === 'Unknown') {
+		return {
+			_id,
+			id,
+			type: 'History',
+			signature,
+			network: Networks.solana,
+			transactionType: 'Unknown',
+			status,
+			date,
+		};
+	}
+
+	return {
+		_id,
+		type: 'History',
+		id,
+		signature,
+		network: Networks.solana,
+		tokenForFee,
+		fee,
+		date,
+		status,
+		...balances,
+	};
+};
+
+const getTransferTxType = (
+	walletAddress: string,
+	sender?: string,
+): 'Sent' | 'Received' => {
+	return sender === walletAddress ? 'Sent' : 'Received';
+};
+
+const getTransactionBalances = async (
+	connection: Connection,
+	parsedTransaction: ParsedTransactionWithMeta,
+	ownerAddress: string,
+): Promise<TransactionBalances> => {
+	const { meta, transaction } = parsedTransaction;
+
+	if (!meta || !meta?.preTokenBalances || !meta?.postTokenBalances)
+		return { transactionType: 'Unknown' };
+
+	const isJupiterSwap = meta?.logMessages?.find((message) =>
+		message.includes(JUPITER_V6_PROGRAM_ID),
+	);
+
+	if (isJupiterSwap) {
+		return await parseSwapTransaction({ transaction, meta, ownerAddress });
+	}
+
+	const isSplTokenTransaction =
+		meta.preTokenBalances?.length > 0 || meta.postTokenBalances?.length > 0;
+
+	if (isSplTokenTransaction) {
+		return await getSplTransactionBalances(
+			connection,
+			meta.preTokenBalances,
+			meta.postTokenBalances,
+			ownerAddress,
+		);
+	} else {
+		return await getNativeTransactionBalances(transaction, ownerAddress);
+	}
+};
+
+const parseSwapTransaction = async ({
+	transaction,
+	meta,
+	ownerAddress,
+}: {
+	transaction: ParsedTransaction;
+	meta: ParsedTransactionMeta;
+	ownerAddress: string;
+}): Promise<SolanaSwapHistory | { transactionType: 'Unknown' }> => {
+	const {
+		receivedTokenMint,
+		receivedTokenAmount,
+		sentTokenMint,
+		sentTokenAmount,
+	} = extractSwapBalance(transaction, meta);
+
+	if (!receivedTokenMint || !sentTokenMint)
+		return { transactionType: 'Unknown' };
+
+	const isWrappedSol =
+		receivedTokenMint === 'So11111111111111111111111111111111111111112';
+
+	const receivedTokenId = `${ownerAddress}/token/${
+		isWrappedSol ? solMint : receivedTokenMint
+	}`;
+	const sentTokenId = `${ownerAddress}/token/${sentTokenMint}`;
+
+	const [receivedTokenDoc, sentTokenDoc] = await Promise.all([
+		storage.safeGet<TokenDocument<SolanaToken>>(receivedTokenId),
+		storage.safeGet<TokenDocument<SolanaToken>>(sentTokenId),
+	]);
+
+	const receivedToken = {
+		amount: receivedTokenAmount / 10 ** (receivedTokenDoc?.decimals || 9),
+		metadata: {
+			mint: receivedTokenMint,
+			name: receivedTokenDoc?.name || '',
+			symbol: receivedTokenDoc?.symbol || '',
+			image: receivedTokenDoc?.image || '',
+		},
+	};
+
+	const sentToken = {
+		amount: sentTokenAmount / 10 ** (sentTokenDoc?.decimals || 9),
+		metadata: {
+			mint: sentTokenMint,
+			name: sentTokenDoc?.name || '',
+			symbol: sentTokenDoc?.symbol || '',
+			image: sentTokenDoc?.image || '',
+		},
+	};
+
+	return {
+		transactionType: 'Swap',
+		receivedToken,
+		sentToken,
+	};
+};
+
+const getNativeTransactionBalances = async (
+	transaction: ParsedTransaction,
+	ownerAddress: string,
+): Promise<TransactionBalances> => {
+	const bubblegumProgram = 'BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY';
+	const programId = transaction.message.instructions[0].programId.toString();
+
+	if (programId === bubblegumProgram) {
+		return {
+			transactionType: 'Unknown',
+		};
+	}
+
+	const token: SolanaTransferHistoryV2['token'] = {
+		mint: solMint,
+		name: solMetadata.name,
+		symbol: solMetadata.symbol,
+		image: solMetadata.image,
+	};
+	const parsedInstruction = transaction.message.instructions.find(
+		(instruction) =>
+			'parsed' in instruction && instruction.parsed !== 'undefined',
+	) as ParsedInstruction;
+	const amount =
+		((parsedInstruction?.parsed?.info?.lamports as number) || 0) / 10 ** 9;
+
+	if (!amount) {
+		return {
+			transactionType: 'Unknown',
+		};
+	}
+
+	const sender = (parsedInstruction?.parsed?.info?.source as string) || '';
+	const receiver =
+		(parsedInstruction?.parsed?.info?.destination as string) || '';
+	const transactionType = getTransferTxType(ownerAddress, sender);
+
+	return {
+		sender,
+		receiver,
+		amount,
+		token,
+		transactionType,
+	};
+};
+
+const getSplTransactionBalances = async (
+	connection: Connection,
+	preTokenBalances: TokenBalance[],
+	postTokenBalances: TokenBalance[],
+	ownerAddress: string,
+): Promise<SolanaTransferHistory | SolanaSwapHistory> => {
+	const tokenBalanceMap = mappingTokenBalance(
+		preTokenBalances,
+		postTokenBalances,
+	);
+	const txRelatedToOwner = Object.keys(tokenBalanceMap).filter(
+		(mint) => tokenBalanceMap[mint][ownerAddress],
+	);
+
+	return await parseSplTransferTransaction({
+		connection,
+		txRelatedToOwner,
+		tokenBalanceMap,
+		ownerAddress,
+	});
+};
+
+const mappingTokenBalance = (
+	preTokenBalances: TokenBalance[],
+	postTokenBalances: TokenBalance[],
+): Record<string, AccountBalanceMap> => {
+	const tokenBalanceMap: Record<string, AccountBalanceMap> = {};
+	postTokenBalances.forEach((balance) => {
+		if (balance.owner) {
+			const tokenBalance = tokenBalanceMap[balance.mint];
+			tokenBalanceMap[balance.mint] = {
+				...tokenBalance,
+				[balance.owner]: {
+					postBalance: balance.uiTokenAmount,
+				},
+			};
+		}
+	});
+
+	preTokenBalances.forEach((balance) => {
+		if (balance.owner) {
+			const tokenBalance = tokenBalanceMap[balance.mint];
+			tokenBalanceMap[balance.mint] = {
+				...tokenBalance,
+				[balance.owner]: {
+					...tokenBalance[balance.owner],
+					preBalance: balance.uiTokenAmount,
+				},
+			};
+		}
+	});
+
+	return tokenBalanceMap;
+};
+
+const parseSplTransferTransaction = async ({
+	connection,
+	txRelatedToOwner,
+	tokenBalanceMap,
+	ownerAddress,
+}: {
+	connection: Connection;
+	txRelatedToOwner: string[];
+	tokenBalanceMap: Record<string, AccountBalanceMap>;
+	ownerAddress: string;
+}): Promise<SolanaTransferHistory> => {
+	const [mint] = txRelatedToOwner;
+	const tokenMetadata = await getTokenMetadata(connection, mint);
+
+	const token: SolanaTransferHistoryV2['token'] = {
+		mint,
+		name: tokenMetadata?.name,
+		symbol: tokenMetadata?.symbol,
+		image: tokenMetadata?.image,
+	};
+
+	const amount =
+		(tokenBalanceMap[mint][ownerAddress].postBalance?.uiAmount || 0) -
+		(tokenBalanceMap[mint][ownerAddress].preBalance?.uiAmount || 0);
+	const [otherOwner] = Object.keys(tokenBalanceMap[mint]).filter(
+		(owner) => owner !== ownerAddress,
+	);
+
+	let sender = '';
+	let receiver = '';
+
+	if (amount > 0) {
+		sender = otherOwner;
+		receiver = ownerAddress;
+	} else {
+		sender = ownerAddress;
+		receiver = otherOwner;
+	}
+	const transactionType = getTransferTxType(ownerAddress, sender);
+
+	return {
+		sender,
+		receiver,
+		token,
+		amount: Math.abs(amount),
+		transactionType,
+	};
+};
+
+const getTransactionTokenFee = () => {
+	return {
+		mint: solMint,
+		name: solMetadata.name,
+		symbol: solMetadata.symbol,
+		image: solMetadata.image,
+	};
+};
+
+const removeOldHistories = async () => {
+	const { docs } = await storage.find(selectors.allHistories);
+
+	if (docs.length === 0) return;
+
+	const historyDocs = (docs as TransactionHistoryDocument[]).sort(
+		(doc1, doc2) => (doc1.date > doc2.date ? -1 : 1),
+	);
+
+	const historyDocsToRemove = historyDocs.slice(historyLimit);
+
+	historyDocsToRemove.forEach((doc) => {
+		storage.removeDoc(doc._id);
+	});
+};
--- apps/wallet/src/engine/runners/solana/types.ts
+++ apps/wallet/src/engine/runners/solana/types.ts
@@ -1,5 +1,12 @@
+import type { IdlEvents, IdlTypes } from '@coral-xyz/anchor';
 import type { Connection, PublicKey, TokenAmount } from '@solana/web3.js';
-import type { NetworkCluster } from '@walless/core';
+import type {
+	NetworkCluster,
+	SolanaSwapHistory,
+	SolanaTransferHistory,
+} from '@walless/core';
+
+import type { Jupiter } from './history/jupiter';
 
 export type SolanaContext = {
 	connection: Connection;
@@ -16,3 +23,28 @@ export type ParsedTokenAccount = {
 	state: string;
 	tokenAmount: TokenAmount;
 };
+
+export interface AccountBalance {
+	postBalance?: TokenAmount;
+	preBalance?: TokenAmount;
+}
+
+export type AccountBalanceMap = Record<string, AccountBalance>;
+
+export type TransactionBalances =
+	| {
+			transactionType: 'Unknown';
+	  }
+	| SolanaTransferHistory
+	| SolanaSwapHistory;
+
+export type SwapEvent = IdlEvents<Jupiter>['SwapEvent'];
+
+export interface PartialInstruction {
+	programId: PublicKey;
+	data: string;
+	accounts: PublicKey[];
+}
+
+type RoutePlanStep = IdlTypes<Jupiter>['RoutePlanStep'];
+export type RoutePlan = RoutePlanStep[];

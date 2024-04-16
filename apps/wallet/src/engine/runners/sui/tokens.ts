fdb248418211a61965e37a4cf34fc05d1b871aaf
FME849 <1414439@gmail.com>
2024-04-16 14:03:24 +0700

    update: group Sui coins before construct TokenDocument

--- apps/wallet/src/engine/runners/sui/tokens.ts
+++ apps/wallet/src/engine/runners/sui/tokens.ts
@@ -1,4 +1,8 @@
-import type { CoinStruct, SuiClient } from '@mysten/sui.js/client';
+import type {
+	CoinStruct,
+	PaginatedCoins,
+	SuiClient,
+} from '@mysten/sui.js/client';
 import { SUI_DECIMALS } from '@mysten/sui.js/utils';
 import type { NetworkCluster, SuiToken } from '@walless/core';
 import { Networks } from '@walless/core';
@@ -10,22 +14,49 @@ export const constructSuiTokenDocument = async (
 	client: SuiClient,
 	cluster: NetworkCluster,
 	owner: string,
-	coin: CoinStruct,
+	coinObjects: CoinStruct[],
 ): Promise<TokenDocument<SuiToken> | undefined> => {
+	const [coin] = coinObjects;
 	const metadata = await getSUITokenMetadata(client, coin);
 	if (!metadata) return;
+	return coinObjects.reduce((tokenDoc, coinObject) => {
+		if (Object.keys(tokenDoc).length === 0) {
+			tokenDoc = {
+				_id: `${owner}/token/${coinObject.coinType}`,
+				type: 'Token',
+				network: Networks.sui,
+				cluster,
+				name: metadata.name,
+				symbol: metadata.symbol,
+				image: metadata.image,
+				balance: Number(coinObject.balance) / 10 ** SUI_DECIMALS,
+				coinObjectIds: [coinObject.coinObjectId],
+				coinType: coinObject.coinType,
+				owner,
+				lockedUntilEpoch: null,
+				previousTransaction: coinObject.previousTransaction,
+			};
+		} else {
+			tokenDoc.coinObjectIds.push(coinObject.coinObjectId);
+			tokenDoc.balance += Number(coinObject.balance) / 10 ** SUI_DECIMALS;
+		}
 
-	return {
-		_id: `${owner}/token/${coin.coinType}`,
-		type: 'Token',
-		network: Networks.sui,
-		cluster,
-		name: metadata.name,
-		symbol: metadata.symbol,
-		image: metadata.image,
-		balance: Number(coin.balance) / 10 ** SUI_DECIMALS,
-		coinObjectId: coin.coinObjectId,
-		coinType: coin.coinType,
-		owner,
-	};
+		return tokenDoc;
+	}, {} as TokenDocument<SuiToken>);
+};
+
+export const groupCoinByType = (coins: PaginatedCoins) => {
+	const { data: coinObjects } = coins;
+	return coinObjects.reduce(
+		(coinList, coinObject) => {
+			if (!coinList[coinObject.coinType]) {
+				coinList[coinObject.coinType] = [coinObject];
+			} else {
+				coinList[coinObject.coinType].push(coinObject);
+			}
+
+			return coinList;
+		},
+		{} as Record<string, CoinStruct[]>,
+	);
 };

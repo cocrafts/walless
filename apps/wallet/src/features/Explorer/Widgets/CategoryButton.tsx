b8d3351727300d02cb175a90afed4f3bf1788f55
ltminhthu <ltminhthu.209@gmail.com>
2024-08-06 12:02:39 +0700

    update type after changing the way to classify widgets

--- apps/wallet/src/features/Explorer/Widgets/CategoryButton.tsx
+++ apps/wallet/src/features/Explorer/Widgets/CategoryButton.tsx
@@ -5,14 +5,14 @@ import Animated, {
 	interpolateColor,
 	useAnimatedStyle,
 } from 'react-native-reanimated';
-import type { WidgetCategory } from '@walless/core';
+import type { WidgetCategories } from '@walless/core';
 
 const AnimatedHoverable = Animated.createAnimatedComponent(TouchableOpacity);
 
 interface CategoryButtonProps {
 	index: number;
-	title: string;
-	onPress: (index: number, category: WidgetCategory | string) => void;
+	title: WidgetCategories;
+	onPress: (index: number, category: WidgetCategories) => void;
 	animatedValue: SharedValue<number>;
 	data: number[];
 }
